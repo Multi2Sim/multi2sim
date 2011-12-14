972054d438738016a23940d3188ebaacf6148b9a
ubal <rafael.ubal.83@gmail.com>
2011-12-14 22:01:41 +0000

    * TEX/ALU engine: fixed condition to prevent pipeline functions to be executed when engines are idle.

--- src/libgpuarch/alu-engine.c
+++ src/libgpuarch/alu-engine.c
@@ -425,7 +425,9 @@ void gpu_alu_engine_write(struct gpu_compute_unit_t *compute_unit)
 void gpu_alu_engine_run(struct gpu_compute_unit_t *compute_unit)
 {
 	/* If no wavefront to run, avoid entering loop */
-	/* FIXME - add support for this condition */
+	if (!lnlist_count(compute_unit->alu_engine.pending_queue) &&
+		!lnlist_count(compute_unit->alu_engine.finished_queue))
+		return;
 
 	/* ALU Engine stages */
 	gpu_alu_engine_write(compute_unit);
