75df667f1f8be9c172dbbe8dd44e369e01eb36b2
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2012-02-26 20:48:47 +0000

    * 'libsdram' removed. * New library 'libmemsystem'. This will be the new centralized library for the   entire memory system, CPU and GPU. A first approach has been implemented so   far in 'libgpuarch'. First step is moving it here, and then integrating it   with the functionality in current 'libcachesystem'. Finally, 'libcachesystem'   will be removed.

--- trunk/src/libmemsystem/mem-system.h
+++ trunk/src/libmemsystem/mem-system.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2012  Rafael Ubal (ubal@ece.neu.edu)
+ *  Copyright (C) 2011  Rafael Ubal (ubal@ece.neu.edu)
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,6 +16,9 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <sdram.h>
+#ifndef MEM_SYSTEM_H
+#define MEM_SYSTEM_H
 
+
+#endif
 
