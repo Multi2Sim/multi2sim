2179787d5a0c1e6074319cc06100af1afdd1b952
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2012-02-29 15:10:56 +0000

    * MMU and directories integrated in 'mem-system' library.

--- trunk/src/libmemsystem/mem-system.h
+++ trunk/src/libmemsystem/mem-system.h
@@ -92,6 +92,83 @@ void cache_set_transient_tag(struct cache_t *cache, uint32_t set, uint32_t way, 
 
 
 
+/*
+ * Directory
+ */
+
+
+struct dir_lock_t
+{
+	int lock;
+	struct moesi_stack_t *lock_queue;
+};
+
+#define DIR_ENTRY_OWNER_NONE  (-1)
+#define DIR_ENTRY_VALID_OWNER(dir_entry)  ((dir_entry)->owner >= 0)
+
+struct dir_entry_t
+{
+	int owner;  /* Node owning the block (-1 = No owner)*/
+	int num_sharers;  /* Number of 1s in next field */
+	unsigned char sharer[0];  /* Bitmap of sharers (must be last field) */
+};
+
+struct dir_t
+{
+	/* Number of possible sharers for a block. This determines
+	 * the size of the directory entry bitmap. */
+	int num_nodes;
+
+	/* Width, height and depth of the directory. For caches, it is
+	 * useful to have a 3-dim directory. XSize is the number of
+	 * sets, YSize is the number of ways of the cache, and ZSize
+	 * is the number of sub-blocks of size 'cache_min_block_size'
+	 * that fit within a block. */
+	int xsize, ysize, zsize;
+
+	/* Array of xsize * ysize locks. Each lock corresponds to a
+	 * block, i.e. a set of zsize directory entries */
+	struct dir_lock_t *dir_lock;
+
+	/* Last field. This is an array of xsize*ysize*zsize elements of type
+	 * dir_entry_t, which have likewise variable size. */
+	unsigned char data[0];
+};
+
+struct dir_t *dir_create(int xsize, int ysize, int zsize, int num_nodes);
+void dir_free(struct dir_t *dir);
+
+struct dir_entry_t *dir_entry_get(struct dir_t *dir, int x, int y, int z);
+void dir_entry_set_sharer(struct dir_t *dir, struct dir_entry_t *dir_entry, int node);
+void dir_entry_clear_sharer(struct dir_t *dir, struct dir_entry_t *dir_entry, int node);
+void dir_entry_clear_all_sharers(struct dir_t *dir, volatile struct dir_entry_t *dir_entry);
+int dir_entry_is_sharer(struct dir_t *dir, struct dir_entry_t *dir_entry, int node);
+void dir_entry_dump_sharers(struct dir_t *dir, struct dir_entry_t *dir_entry);
+int dir_entry_group_shared_or_owned(struct dir_t *dir, int x, int y);
+
+struct dir_lock_t *dir_lock_get(struct dir_t *dir, int x, int y);
+int dir_lock_lock(struct dir_lock_t *dir_lock, int event, struct moesi_stack_t *stack);
+void dir_lock_unlock(struct dir_lock_t *dir_lock);
+void dir_unlock(struct dir_t *dir, int x, int y);
+
+
+
+
+/*
+ * Memory Management Unit
+ */
+
+extern uint32_t mmu_page_size;
+extern uint32_t mmu_page_mask;
+extern uint32_t mmu_log_page_size;
+
+void mmu_init(void);
+void mmu_done(void);
+uint32_t mmu_translate(int mid, uint32_t vtladdr);
+int mmu_valid_phaddr(uint32_t phaddr);
+
+
+
 
 /*
  * Memory Module
@@ -341,6 +418,52 @@ void gpu_mod_handler_write(int event, void *data);
 /*
  * CPU Event-Driven Simulation
  */
+
+enum {
+	moesi_state_invalid = 0,
+	moesi_state_modified,
+	moesi_state_owned,
+	moesi_state_exclusive,
+	moesi_state_shared
+};
+
+extern long long moesi_stack_id;
+
+struct moesi_stack_t
+{
+	long long id;
+	struct mod_t *mod, *target, *except;
+	uint32_t addr, set, way, tag;
+	uint32_t src_set, src_way, src_tag;
+	struct dir_lock_t *dir_lock;
+	int status, response, pending;
+	int hit;
+
+	/* Message sent to the network */
+	struct net_msg_t *msg;
+
+	/* Flags */
+	int err : 1;
+	int shared : 1;
+	int read : 1;
+	int blocking : 1;
+	int writeback : 1;
+	int eviction : 1;
+	int retry : 1;
+
+	/* Cache block lock */
+	int lock_event;
+	struct moesi_stack_t *lock_next;
+
+	/* Return event */
+	int retevent;
+	void *retstack;
+};
+
+struct moesi_stack_t *moesi_stack_create(uint64_t id, struct mod_t *mod,
+	uint32_t addr, int retevent, void *retstack);
+void moesi_stack_return(struct moesi_stack_t *stack);
+
 
 extern int EV_MOESI_FIND_AND_LOCK;
 extern int EV_MOESI_FIND_AND_LOCK_ACTION;
