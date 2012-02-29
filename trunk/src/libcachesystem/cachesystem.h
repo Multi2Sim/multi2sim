c249db7a986cf66c606c43b8fd83dc8cc69966b3
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2012-02-29 17:07:34 +0000

    * TLBs removed.

--- trunk/src/libcachesystem/cachesystem.h
+++ trunk/src/libcachesystem/cachesystem.h
@@ -60,87 +60,6 @@ struct ccache_access_t
 	struct ccache_access_t *next;  /* Alias (same address/access, but different eventq_item */
 };
 
-#if 0
-struct ccache_t
-{
-	enum mod_kind_t kind;
-	char *name;
-	uint32_t block_size;
-	int log_block_size;
-	int latency;
-
-	/* Directory */
-	struct dir_t *dir;
-	uint32_t dir_size;
-	uint32_t dir_num_sets;
-	uint32_t dir_assoc;
-
-	/* Address range */
-	enum mod_range_kind_t range_kind;
-	union {
-		/* For range_kind = mod_range_bounds */
-		struct {
-			uint32_t low;
-			uint32_t high;
-		} bounds;
-
-		/* For range_kind = mod_range_interleaved */
-		struct {
-			uint32_t mod;
-			uint32_t div;
-			uint32_t eq;
-		} interleaved;
-	} range;
-
-	/* Banks and ports */
-	struct mod_bank_t *banks;
-	int bank_count;
-	int read_port_count;  /* Number of read ports (per bank) */
-	int write_port_count;  /* Number of write ports (per bank) */
-
-	/* Low and high memory modules */
-	struct linked_list_t *high_mod_list;
-	struct linked_list_t *low_mod_list;
-
-	/* Associated structures */
-	struct cache_t *cache;  /* Cache holding data */
-
-	/* Networks */
-	struct net_t *high_net;
-	struct net_t *low_net;
-	struct net_node_t *high_net_node;  /* Node in upper network */
-	struct net_node_t *low_net_node;  /* Node in lower network */
-
-	/* List of in-flight accesses */
-	struct linked_list_t *access_list;  /* Elements of type ccache_access_t */
-	int pending_reads;  /* Non-aliasing reads in access_list */
-	int pending_writes;  /* Writes in access_list */
-
-	/* Stats */
-	uint64_t accesses;
-	uint64_t hits;
-	uint64_t evictions;
-	uint64_t reads;
-	uint64_t blocking_reads;
-	uint64_t non_blocking_reads;
-	uint64_t read_hits;
-	uint64_t writes;
-	uint64_t blocking_writes;
-	uint64_t non_blocking_writes;
-	uint64_t write_hits;
-
-	uint64_t read_retries;
-	uint64_t write_retries;
-
-	uint64_t no_retry_accesses;
-	uint64_t no_retry_hits;
-	uint64_t no_retry_reads;
-	uint64_t no_retry_read_hits;
-	uint64_t no_retry_writes;
-	uint64_t no_retry_write_hits;
-};
-#endif
-
 struct mod_t *__mod_create(char *name, enum mod_kind_t kind);
 void __mod_free(struct mod_t *mod);
 
@@ -148,35 +67,6 @@ int __mod_find_block(struct mod_t *mod, uint32_t addr,
 	uint32_t *pset, uint32_t *pway, uint32_t *ptag, int *pstatus);
 void __mod_dump(struct mod_t *mod, FILE *f);
 struct mod_t *__mod_get_low_mod(struct mod_t *mod);
-
-
-/*
- * CPU Event-Driven Simulation
- */
-
-#define cache_debug(...) debug(cache_debug_category, __VA_ARGS__)
-extern int cache_debug_category;
-
-/*
- * TLB
- */
-
-struct tlb_t
-{
-	/* Parameters */
-	char name[100];
-	int hitlat;
-	int misslat;
-	struct cache_t *cache;  /* Cache holding data */
-
-	/* Stats */
-	long long accesses;
-	long long hits;
-	long long evictions;
-};
-
-struct tlb_t *tlb_create();
-void tlb_free(struct tlb_t *tlb);
 
 
 
@@ -200,19 +90,16 @@ enum cache_kind_t
 	cache_kind_data
 };
 
-enum tlb_kind_t
-{
-	tlb_kind_data = 0,
-	tlb_kind_inst
-};
-
 struct cache_system_stack_t
 {
-	int core, thread;
+	int core;
+	int thread;
+
 	enum cache_kind_t cache_kind;
 	enum mod_access_kind_t access_kind;
+
 	uint32_t addr;
-	int pending;
+
 	struct linked_list_t *eventq;
 	void *eventq_item;
 
@@ -227,7 +114,6 @@ void cache_system_handler(int event, void *data);
 
 extern int EV_CACHE_SYSTEM_ACCESS;
 extern int EV_CACHE_SYSTEM_ACCESS_CACHE;
-extern int EV_CACHE_SYSTEM_ACCESS_TLB;
 extern int EV_CACHE_SYSTEM_ACCESS_FINISH;
 
 void cache_system_init(int def_cores, int def_threads);
