b511cc37e00d97df3b6ce642975980ecc9296ace
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2012-02-29 02:34:45 +0000

    * Variables 'ccache' replaced by 'mod'. * Type 'ccache_t' discarded, now we use type 'mod_t' defined in 'mem-system.h' * Previous 'ccache_xxx' functions used in 'moesi.c' now renamed temporarily   to '__mod_xxx' functions. The objective is getting rid of all of them, and   use 'mod_xxx' functions in 'mem-system.h' instead.
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@431 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- src/libstruct/repos.c
+++ src/libstruct/repos.c
@@ -21,9 +21,11 @@
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
-#include "repos.h"
+#include <debug.h>
+#include <repos.h>
 
-struct objtail_t {
+struct objtail_t
+{
 	int id;
 	int status;  /* 0=free, 1=allocated */
 	void *prev;
@@ -31,26 +33,35 @@ struct objtail_t {
 };
 
 
-struct repos_t {
+struct repos_t
+{
 	char *name;
 	int id;
-	int objsize;
+	int object_size;
 	void *alloc_head;
-	void *unalloc_head;
+	void *dealloc_head;
 };
 
 
 struct repos_t *repos_create(int objsize, char *name)
 {
 	struct repos_t *repos;
+
+	/* Check */
 	if (objsize <= 0)
-		return NULL;
+		panic("%s: invalid object size", __FUNCTION__);
+
+	/* Allocate repository */
 	repos = calloc(1, sizeof(struct repos_t));
 	if (!repos)
-		return NULL;
+		fatal("%s: out of memory", __FUNCTION__);
+
+	/* Initialize */
 	repos->id = random();
 	repos->name = name;
-	repos->objsize = objsize;
+	repos->object_size = objsize;
+
+	/* Return */
 	return repos;
 }
 
@@ -62,8 +73,8 @@ void repos_free_dump(struct repos_t *repos, void(*dump)(void *, FILE *))
 	int count = 0;
 
 	/* Free objects in unallocated list */
-	for (obj = repos->unalloc_head; obj; obj = next_obj) {
-		objtail = obj + repos->objsize;
+	for (obj = repos->dealloc_head; obj; obj = next_obj) {
+		objtail = obj + repos->object_size;
 		next_obj = objtail->next;
 		free(obj);
 	}
@@ -71,7 +82,7 @@ void repos_free_dump(struct repos_t *repos, void(*dump)(void *, FILE *))
 	/* Free objects in allocated list */
 	count = 0;
 	for (obj = repos->alloc_head; obj; obj = next_obj) {
-		objtail = obj + repos->objsize;
+		objtail = obj + repos->object_size;
 		next_obj = objtail->next;
 		count++;
 		if (dump) {
@@ -102,30 +113,30 @@ void *repos_create_object(struct repos_t *repos)
 	
 	/* No unallocated object available. Create a new object.
 	 * Insert it into the unallocated list head. */
-	if (!repos->unalloc_head) {
-		obj = calloc(1, repos->objsize + sizeof(struct objtail_t));
+	if (!repos->dealloc_head) {
+		obj = calloc(1, repos->object_size + sizeof(struct objtail_t));
 		if (!obj)
 			return NULL;
-		objtail = obj + repos->objsize;
+		objtail = obj + repos->object_size;
 		objtail->id = repos->id;
-		repos->unalloc_head = obj;
+		repos->dealloc_head = obj;
 	}
 
 	/* Remove the first unallocated object from the list */
-	obj = repos->unalloc_head;
-	objtail = obj + repos->objsize;
+	obj = repos->dealloc_head;
+	objtail = obj + repos->object_size;
 	assert(!objtail->prev);
 	assert(!objtail->status);
 	next_obj = objtail->next;
-	next_objtail = next_obj + repos->objsize;
+	next_objtail = next_obj + repos->object_size;
 	if (next_obj)
 		next_objtail->prev = NULL;
-	repos->unalloc_head = next_obj;
+	repos->dealloc_head = next_obj;
 
 	/* Clear object and insert it into the allocated list head */
-	memset(obj, 0, repos->objsize);
+	memset(obj, 0, repos->object_size);
 	next_obj = repos->alloc_head;
-	next_objtail = next_obj + repos->objsize;
+	next_objtail = next_obj + repos->object_size;
 	if (next_obj)
 		next_objtail->prev = obj;
 	objtail->next = next_obj;
@@ -152,7 +163,7 @@ void repos_free_object(struct repos_t *repos, void *obj)
 	}
 
 	/* Check that object was allocated */
-	objtail = obj + repos->objsize;
+	objtail = obj + repos->object_size;
 	if (!objtail->status) {
 		fprintf(stderr, "panic: %s.repos_free_object: object was not allocated\n",
 			repos->name);
@@ -161,9 +172,9 @@ void repos_free_object(struct repos_t *repos, void *obj)
 
 	/* Remove object from allocated list */
 	prev_obj = objtail->prev;
-	prev_objtail = prev_obj + repos->objsize;
+	prev_objtail = prev_obj + repos->object_size;
 	next_obj = objtail->next;
-	next_objtail = next_obj + repos->objsize;
+	next_objtail = next_obj + repos->object_size;
 	if (prev_obj)
 		prev_objtail->next = next_obj;
 	if (next_obj)
@@ -172,14 +183,14 @@ void repos_free_object(struct repos_t *repos, void *obj)
 		repos->alloc_head = next_obj;
 	
 	/* Insert object into unallocated list head */
-	next_obj = repos->unalloc_head;
-	next_objtail = obj + repos->objsize;
+	next_obj = repos->dealloc_head;
+	next_objtail = obj + repos->object_size;
 	if (next_obj)
 		next_objtail->prev = obj;
 	objtail->prev = NULL;
 	objtail->next = next_obj;
 	objtail->status = 0;
-	repos->unalloc_head = obj;
+	repos->dealloc_head = obj;
 }
 
 
@@ -188,7 +199,7 @@ int repos_allocated_object(struct repos_t *repos, void *obj)
 	struct objtail_t *objtail;
 	if (!obj)
 		return 0;
-	objtail = obj + repos->objsize;
+	objtail = obj + repos->object_size;
 	return objtail->id == repos->id && objtail->status;
 }
 
