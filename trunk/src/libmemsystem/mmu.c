42270da70928131ce1001649c0efef1e5278f0b0
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2012-03-07 22:09:36 +0000

    * Report for accesses to physical pages (option --report-mem-access)

--- trunk/src/libmemsystem/mmu.c
+++ trunk/src/libmemsystem/mmu.c
@@ -25,6 +25,8 @@
  * Global variables
  */
 
+char *mmu_report_file_name = "";
+
 uint32_t mmu_page_size = 1 << 12;  /* 4KB default page size */
 uint32_t mmu_log_page_size;
 uint32_t mmu_page_mask;
@@ -40,66 +42,42 @@ uint32_t mmu_page_mask;
 #define MMU_PAGE_HASH_SIZE  (1 << 10)
 #define MMU_PAGE_LIST_SIZE  (1 << 10)
 
-
 /* Physical memory page */
 struct mmu_page_t
 {
 	struct mmu_page_t *next;
 
 	int mid;  /* Memory map ID */
-	uint32_t vtladdr;  /* Virtual address of page */
-	uint32_t phaddr;  /* Physical address */
+	uint32_t vtl_addr;  /* Virtual address of page */
+	uint32_t phy_addr;  /* Physical address */
+
+	/* Statistics */
+	long long num_read_accesses;
+	long long num_write_accesses;
+	long long num_execute_accesses;
 };
 
-
+/* Memory management unit */
 struct mmu_t
 {
+	/* List of pages */
+	struct list_t *page_list;
+
 	/* Hash table of pages */
-	struct mmu_page_t *page_hash_table[MMU_PAGE_HASH_SIZE];  /* Hash table of pages */
-	struct list_t *page_list;  /* List of pages */
+	struct mmu_page_t *page_hash_table[MMU_PAGE_HASH_SIZE];
+
+	/* Report file */
+	FILE *report_file;
 };
 
-
-/* Global memory management unit */
 static struct mmu_t *mmu;
 
 
 
 
-
 /*
- * Public Functions
+ * Private Functions
  */
-
-void mmu_init()
-{
-	/* Variables derived from page size */
-	mmu_log_page_size = log_base2(mmu_page_size);
-	mmu_page_mask = mmu_page_size - 1;
-
-	/* Create MMU */
-	mmu = calloc(1, sizeof(struct mmu_t));
-	if (!mmu)
-		fatal("%s: out of memory", __FUNCTION__);
-
-	/* Initialize */
-	mmu->page_list = list_create_with_size(MMU_PAGE_LIST_SIZE);
-}
-
-
-void mmu_done()
-{
-	int i;
-
-	/* Free pages */
-	for (i = 0; i < list_count(mmu->page_list); i++)
-		free(list_get(mmu->page_list, i));
-	list_free(mmu->page_list);
-
-	/* Free MMU */
-	free(mmu);
-}
-
 
 static struct mmu_page_t *mmu_get_page(int mid, uint32_t vtladdr)
 {
@@ -114,7 +92,7 @@ static struct mmu_page_t *mmu_get_page(int mid, uint32_t vtladdr)
 	page = mmu->page_hash_table[index];
 	while (page)
 	{
-		if (page->vtladdr == tag && page->mid == mid)
+		if (page->vtl_addr == tag && page->mid == mid)
 			break;
 		prev = page;
 		page = page->next;
@@ -129,9 +107,9 @@ static struct mmu_page_t *mmu_get_page(int mid, uint32_t vtladdr)
 			fatal("%s: out of memory", __FUNCTION__);
 
 		/* Initialize */
-		page->vtladdr = tag;
+		page->vtl_addr = tag;
 		page->mid = mid;
-		page->phaddr = list_count(mmu->page_list) << mmu_log_page_size;
+		page->phy_addr = list_count(mmu->page_list) << mmu_log_page_size;
 
 		/* Insert in page list */
 		list_add(mmu->page_list, page);
@@ -155,23 +133,165 @@ static struct mmu_page_t *mmu_get_page(int mid, uint32_t vtladdr)
 }
 
 
-uint32_t mmu_translate(int mid, uint32_t vtladdr)
+/* Compare two pages */
+static int mmu_page_compare(const void *ptr1, const void *ptr2)
 {
-	struct mmu_page_t *page;
-	uint32_t offs, phaddr;
+	struct mmu_page_t *page1 = (struct mmu_page_t *) ptr1;
+	struct mmu_page_t *page2 = (struct mmu_page_t *) ptr2;
 
-	offs = vtladdr & mmu_page_mask;
-	page = mmu_get_page(mid, vtladdr);
-	assert(page);
-	phaddr = page->phaddr | offs;
-	return phaddr;
+	long long num_accesses1;
+	long long num_accesses2;
+
+	num_accesses1 = page1->num_read_accesses + page1->num_write_accesses
+		+ page1->num_execute_accesses;
+	num_accesses2 = page2->num_read_accesses + page2->num_write_accesses
+		+ page2->num_execute_accesses;
+	if (num_accesses1 < num_accesses2)
+		return 1;
+	else if (num_accesses1 == num_accesses2)
+		return 0;
+	else
+		return -1;
 }
 
 
-int mmu_valid_phaddr(uint32_t phaddr)
-{
-	uint32_t index;
 
-	index = phaddr >> mmu_log_page_size;
+
+/*
+ * Public Functions
+ */
+
+void mmu_init()
+{
+	/* Variables derived from page size */
+	mmu_log_page_size = log_base2(mmu_page_size);
+	mmu_page_mask = mmu_page_size - 1;
+
+	/* Create Memory Management Unit (MMU) */
+	mmu = calloc(1, sizeof(struct mmu_t));
+	if (!mmu)
+		fatal("%s: out of memory", __FUNCTION__);
+
+	/* Initialize */
+	mmu->page_list = list_create_with_size(MMU_PAGE_LIST_SIZE);
+
+	/* Open report file */
+	if (*mmu_report_file_name)
+	{
+		mmu->report_file = open_write(mmu_report_file_name);
+		if (!mmu->report_file)
+			fatal("%s: cannot open report file for MMU", mmu_report_file_name);
+	}
+}
+
+
+void mmu_done()
+{
+	int i;
+
+	/* Dump report */
+	mmu_dump_report();
+
+	/* Free pages */
+	for (i = 0; i < list_count(mmu->page_list); i++)
+		free(list_get(mmu->page_list, i));
+	list_free(mmu->page_list);
+
+	/* Free MMU */
+	free(mmu);
+}
+
+
+void mmu_dump_report(void)
+{
+	struct mmu_page_t *page;
+
+	FILE *f;
+	int i;
+
+	long long num_accesses;
+
+	/* Report file */
+	f = mmu->report_file;
+	if (!f)
+		return;
+
+	/* Sort list of pages it as per access count */
+	list_sort(mmu->page_list, mmu_page_compare);
+
+	/* Header */
+	fprintf(f, "%5s %5s %9s %9s %10s %10s %10s %10s\n", "Idx", "MemID", "VtlAddr",
+		"PhyAddr", "Accesses", "Read", "Write", "Exec");
+	for (i = 0; i < 77; i++)
+		fprintf(f, "-");
+	fprintf(f, "\n");
+
+	/* Dump */
+	for (i = 0; i < list_count(mmu->page_list); i++)
+	{
+		page = list_get(mmu->page_list, i);
+		num_accesses = page->num_read_accesses + page->num_write_accesses
+			+ page->num_execute_accesses;
+		fprintf(f, "%5d %5d %9x %9x %10lld %10lld %10lld %10lld\n",
+			i + 1, page->mid, page->vtl_addr, page->phy_addr, num_accesses,
+			page->num_read_accesses, page->num_write_accesses,
+			page->num_execute_accesses);
+	}
+	fclose(f);
+}
+
+
+uint32_t mmu_translate(int mid, uint32_t vtl_addr)
+{
+	struct mmu_page_t *page;
+
+	uint32_t offset;
+	uint32_t phy_addr;
+
+	offset = vtl_addr & mmu_page_mask;
+	page = mmu_get_page(mid, vtl_addr);
+	assert(page);
+	phy_addr = page->phy_addr | offset;
+	return phy_addr;
+}
+
+
+int mmu_valid_phy_addr(uint32_t phy_addr)
+{
+	int index;
+
+	index = phy_addr >> mmu_log_page_size;
 	return index < mmu->page_list->count;
+}
+
+
+void mmu_access_page(uint32_t phy_addr, enum mmu_access_t access)
+{
+	struct mmu_page_t *page;
+	int index;
+
+	/* Get page */
+	index = phy_addr >> mmu_log_page_size;
+	page = list_get(mmu->page_list, index);
+	if (!page)
+		return;
+
+	/* Record access */
+	switch (access)
+	{
+	case mmu_access_read:
+		page->num_read_accesses++;
+		break;
+
+	case mmu_access_write:
+		page->num_write_accesses++;
+		break;
+
+	case mmu_access_execute:
+		page->num_execute_accesses++;
+		break;
+
+	default:
+		panic("%s: invalid access", __FUNCTION__);
+	}
 }
