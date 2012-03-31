cf4840a85dd0f9fc6b8c09f32c84be13475addf6
ubal <rafael.ubal.83@gmail.com>
2012-03-31 22:54:01 +0000

    * Access list

--- src/libmemvisual/vmod-panel.c
+++ src/libmemvisual/vmod-panel.c
@@ -194,8 +194,12 @@ static void vmod_panel_create_widget(struct vmod_panel_t *panel)
 		list_add(level->vmod_list, vmod);
 	}
 
-	/* Insert levels */
+	/* Insert access list */
 	vbox = gtk_vbox_new(FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(vbox), vlist_get_widget(panel->vmod_access_list), FALSE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(vbox), gtk_hseparator_new(), FALSE, FALSE, 0);
+
+	/* Insert levels */
 	LIST_FOR_EACH(panel->vmod_level_list, level_id)
 	{
 		level = list_get(panel->vmod_level_list, level_id);
@@ -241,6 +245,9 @@ struct vmod_panel_t *vmod_panel_create(void)
 	panel->vmod_table = hash_table_create(0, FALSE);
 	panel->vnet_table = hash_table_create(0, FALSE);
 	panel->vmod_level_list = list_create();
+	panel->vmod_access_list = vlist_create("List of accesses", 50, 50,
+		(vlist_get_elem_name_func_t) vmod_access_get_name_str,
+		(vlist_get_elem_desc_func_t) vmod_access_get_desc_str);
 
 	/* Read and add components to the panel */
 	vmod_panel_read_config(panel);
@@ -276,6 +283,11 @@ void vmod_panel_free(struct vmod_panel_t *panel)
 		vmod_level_free(list_get(panel->vmod_level_list, i));
 	list_free(panel->vmod_level_list);
 
+	/* Free accesses */
+	VLIST_FOR_EACH(panel->vmod_access_list, i)
+		vmod_access_free(vlist_get(panel->vmod_access_list, i));
+	vlist_free(panel->vmod_access_list);
+
 	/* Free panel */
 	free(panel);
 }
@@ -299,6 +311,83 @@ struct vmod_t *vmod_panel_get_vmod(struct vmod_panel_t *panel, char *name)
 }
 
 
+void vmod_panel_read_checkpoint(struct vmod_panel_t *panel, FILE *f)
+{
+	char vmod_name[MAX_STRING_SIZE];
+	char access_name[MAX_STRING_SIZE];
+
+	struct vmod_t *vmod;
+	struct vmod_access_t *access;
+
+	int num_accesses;
+	int count;
+	int i;
+
+	/* Empty access list */
+	while (vlist_count(panel->vmod_access_list))
+	{
+		access = vlist_remove_at(panel->vmod_access_list, 0);
+		vmod_access_free(access);
+	}
+
+	/* Read number of accesses */
+	count = fread(&num_accesses, 1, 4, f);
+	if (count != 4)
+		fatal("%s: error reading from checkpoint", __FUNCTION__);
+
+	/* Read accesses */
+	for (i = 0; i < num_accesses; i++)
+	{
+		str_read_from_file(f, access_name, sizeof access_name);
+		access = vmod_access_create(access_name);
+		vlist_add(panel->vmod_access_list, access);
+	}
+
+	/* Read caches */
+	for (i = 0; i < hash_table_count(panel->vmod_table); i++)
+	{
+		/* Get module */
+		str_read_from_file(f, vmod_name, sizeof vmod_name);
+		vmod = hash_table_get(panel->vmod_table, vmod_name);
+		if (!vmod)
+			panic("%s: invalid module name", __FUNCTION__);
+
+		/* Read cache checkpoint */
+		vcache_read_checkpoint(vmod->vcache, f);
+	}
+}
+
+
+void vmod_panel_write_checkpoint(struct vmod_panel_t *panel, FILE *f)
+{
+	struct vmod_t *vmod;
+	struct vmod_access_t *access;
+
+	char *vmod_name;
+
+	int num_accesses;
+	int count;
+	int i;
+
+	/* Dump access list */
+	num_accesses = vlist_count(panel->vmod_access_list);
+	count = fwrite(&num_accesses, 1, 4, f);
+	VLIST_FOR_EACH(panel->vmod_access_list, i)
+	{
+		access = vlist_get(panel->vmod_access_list, i);
+		str_write_to_file(f, vmod_access_get_name(access));
+	}
+	if (count != 4)
+		fatal("%s: cannot write to checkpoint file", __FUNCTION__);
+
+	/* Dump caches */
+	HASH_TABLE_FOR_EACH(panel->vmod_table, vmod_name, vmod)
+	{
+		str_write_to_file(f, vmod->name);
+		vmod_write_checkpoint(vmod, f);
+	}
+}
+
 
 void vmod_panel_refresh(struct vmod_panel_t *panel)
 {
@@ -306,6 +395,7 @@ void vmod_panel_refresh(struct vmod_panel_t *panel)
 
 	char *vmod_name;
 
+	vlist_refresh(panel->vmod_access_list);
 	HASH_TABLE_FOR_EACH(panel->vmod_table, vmod_name, vmod)
 		vmod_refresh(vmod);
 }
