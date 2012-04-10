30d67fa107776f291cf43e25d90d25d728e91cb6
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2012-04-10 19:33:54 +0000

    * x86-timing.h
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@590 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- src/arch/x86/timing/bpred.c
+++ src/arch/x86/timing/bpred.c
@@ -17,7 +17,9 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <cpuarch.h>
+
+#include <x86-timing.h>
+
 
 #define BTB_ENTRY(SET, WAY) (&bpred->btb[(SET) * bpred_btb_assoc + (WAY)])
 
--- src/arch/x86/timing/stg-issue.c
+++ src/arch/x86/timing/stg-issue.c
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <cpuarch.h>
+#include <x86-timing.h>
 
 
 static int issue_sq(int core, int thread, int quant)
