a45345ad8ca58e92bf945dbfbf89908cae53fd43
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2012-04-10 19:33:54 +0000

    * x86-timing.h

--- trunk/src/arch/x86/timing/stg-fetch.c
+++ trunk/src/arch/x86/timing/stg-fetch.c
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <cpuarch.h>
+#include <x86-timing.h>
 
 
 static int can_fetch(int core, int thread)
--- trunk/src/m2s.c
+++ trunk/src/m2s.c
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <cpuarch.h>
+#include <x86-timing.h>
 #include <gpuarch.h>
 #include <gpuvisual.h>
 #include <visual.h>
