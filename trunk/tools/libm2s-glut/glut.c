c541601f2603ff1476fe631f9f2ed78a3b4ab665
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2012-05-04 22:52:57 +0000

    * Added 'glut' library to dependences. * Clarified error messages for missing 'zlib'.

--- trunk/tools/libm2s-glut/glut.c
+++ trunk/tools/libm2s-glut/glut.c
@@ -25,7 +25,10 @@
 	__FUNCTION__, err_m2s_glut_not_impl);
 
 
-static char *err_m2s_glut_not_impl = "";
+static char *err_m2s_glut_not_impl =
+	"\tMulti2Sim provides partial support for the OpenGL Utility Toolkit (GLUT)\n"
+	"\tlibrary. To request the implementation of a certain functionality, please\n"
+	"\temail development@multi2sim.org.\n";
 
 
 void glutInit(int *argcp, char **argv)
@@ -34,142 +37,699 @@ void glutInit(int *argcp, char **argv)
 }
 
 
-void glutInitDisplayMode(unsigned int mode);
-void glutInitDisplayString(const char *string);
-void glutInitWindowPosition(int x, int y);
-void glutInitWindowSize(int width, int height);
-void glutMainLoop(void);
+void glutInitDisplayMode(unsigned int mode)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
 
-/* GLUT window sub-API. */
-int glutCreateWindow(const char *title);
-int glutCreateSubWindow(int win, int x, int y, int width, int height);
-void glutDestroyWindow(int win);
-void glutPostRedisplay(void);
-void glutPostWindowRedisplay(int win);
-void glutSwapBuffers(void);
-int glutGetWindow(void);
-void glutSetWindow(int win);
-void glutSetWindowTitle(const char *title);
-void glutSetIconTitle(const char *title);
-void glutPositionWindow(int x, int y);
-void glutReshapeWindow(int width, int height);
-void glutPopWindow(void);
-void glutPushWindow(void);
-void glutIconifyWindow(void);
-void glutShowWindow(void);
-void glutHideWindow(void);
-void glutFullScreen(void);
-void glutSetCursor(int cursor);
-void glutWarpPointer(int x, int y);
 
-/* GLUT overlay sub-API. */
-void glutEstablishOverlay(void);
-void glutRemoveOverlay(void);
-void glutUseLayer(GLenum layer);
-void glutPostOverlayRedisplay(void);
-void glutPostWindowOverlayRedisplay(int win);
-void glutShowOverlay(void);
-void glutHideOverlay(void);
+void glutInitDisplayString(const char *string)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
 
-/* GLUT menu sub-API. */
-int glutCreateMenu(void (*)(int));
-void glutDestroyMenu(int menu);
-int glutGetMenu(void);
-void glutSetMenu(int menu);
-void glutAddMenuEntry(const char *label, int value);
-void glutAddSubMenu(const char *label, int submenu);
-void glutChangeToMenuEntry(int item, const char *label, int value);
-void glutChangeToSubMenu(int item, const char *label, int submenu);
-void glutRemoveMenuItem(int item);
-void glutAttachMenu(int button);
-void glutDetachMenu(int button);
 
-/* GLUT window callback sub-API. */
-void glutDisplayFunc(void (*func)(void));
-void glutReshapeFunc(void (*func)(int width, int height));
-void glutKeyboardFunc(void (*func)(unsigned char key, int x, int y));
-void glutMouseFunc(void (*func)(int button, int state, int x, int y));
-void glutMotionFunc(void (*func)(int x, int y));
-void glutPassiveMotionFunc(void (*func)(int x, int y));
-void glutEntryFunc(void (*func)(int state));
-void glutVisibilityFunc(void (*func)(int state));
-void glutIdleFunc(void (*func)(void));
-void glutTimerFunc(unsigned int millis, void (*func)(int value), int value);
-void glutMenuStateFunc(void (*func)(int state));
-void glutSpecialFunc(void (*func)(int key, int x, int y));
-void glutSpaceballMotionFunc(void (*func)(int x, int y, int z));
-void glutSpaceballRotateFunc(void (*func)(int x, int y, int z));
-void glutSpaceballButtonFunc(void (*func)(int button, int state));
-void glutButtonBoxFunc(void (*func)(int button, int state));
-void glutDialsFunc(void (*func)(int dial, int value));
-void glutTabletMotionFunc(void (*func)(int x, int y));
-void glutTabletButtonFunc(void (*func)(int button, int state, int x, int y));
-void glutMenuStatusFunc(void (*func)(int status, int x, int y));
-void glutOverlayDisplayFunc(void (*func)(void));
-void glutWindowStatusFunc(void (*func)(int state));
-void glutKeyboardUpFunc(void (*func)(unsigned char key, int x, int y));
-void glutSpecialUpFunc(void (*func)(int key, int x, int y));
-void glutJoystickFunc(void (*func)(unsigned int buttonMask, int x, int y, int z), int pollInterval);
+void glutInitWindowPosition(int x, int y)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
 
-/* GLUT color index sub-API. */
-void glutSetColor(int, GLfloat red, GLfloat green, GLfloat blue);
-GLfloat glutGetColor(int ndx, int component);
-void glutCopyColormap(int win);
 
-/* GLUT state retrieval sub-API. */
-int glutGet(GLenum type);
-int glutDeviceGet(GLenum type);
+void glutInitWindowSize(int width, int height)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
 
-/* GLUT extension support sub-API */
-int glutExtensionSupported(const char *name);
-int glutGetModifiers(void);
-int glutLayerGet(GLenum type);
 
-/* GLUT font sub-API */
-void glutBitmapCharacter(void *font, int character);
-int glutBitmapWidth(void *font, int character);
-void glutStrokeCharacter(void *font, int character);
-int glutStrokeWidth(void *font, int character);
-int glutBitmapLength(void *font, const unsigned char *string);
-int glutStrokeLength(void *font, const unsigned char *string);
+void glutMainLoop(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
 
-/* GLUT pre-built models sub-API */
-void glutWireSphere(GLdouble radius, GLint slices, GLint stacks);
-void glutSolidSphere(GLdouble radius, GLint slices, GLint stacks);
-void glutWireCone(GLdouble base, GLdouble height, GLint slices, GLint stacks);
-void glutSolidCone(GLdouble base, GLdouble height, GLint slices, GLint stacks);
-void glutWireCube(GLdouble size);
-void glutSolidCube(GLdouble size);
-void glutWireTorus(GLdouble innerRadius, GLdouble outerRadius, GLint sides, GLint rings);
-void glutSolidTorus(GLdouble innerRadius, GLdouble outerRadius, GLint sides, GLint rings);
-void glutWireDodecahedron(void);
-void glutSolidDodecahedron(void);
-void glutWireTeapot(GLdouble size);
-void glutSolidTeapot(GLdouble size);
-void glutWireOctahedron(void);
-void glutSolidOctahedron(void);
-void glutWireTetrahedron(void);
-void glutSolidTetrahedron(void);
-void glutWireIcosahedron(void);
-void glutSolidIcosahedron(void);
 
-/* GLUT video resize sub-API. */
-int glutVideoResizeGet(GLenum param);
-void glutSetupVideoResizing(void);
-void glutStopVideoResizing(void);
-void glutVideoResize(int x, int y, int width, int height);
-void glutVideoPan(int x, int y, int width, int height);
+int glutCreateWindow(const char *title)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
 
-/* GLUT debugging sub-API. */
-void glutReportErrors(void);
-void glutIgnoreKeyRepeat(int ignore);
-void glutSetKeyRepeat(int repeatMode);
-void glutForceJoystickFunc(void);
 
-/* GLUT game mode sub-API. */
-void glutGameModeString(const char *string);
-int glutEnterGameMode(void);
-void glutLeaveGameMode(void);
-int glutGameModeGet(GLenum mode);
+int glutCreateSubWindow(int win, int x, int y, int width, int height)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+void glutDestroyWindow(int win)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutPostRedisplay(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutPostWindowRedisplay(int win)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSwapBuffers(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+int glutGetWindow(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+void glutSetWindow(int win)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSetWindowTitle(const char *title)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSetIconTitle(const char *title)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutPositionWindow(int x, int y)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutReshapeWindow(int width, int height)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutPopWindow(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutPushWindow(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutIconifyWindow(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutShowWindow(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutHideWindow(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutFullScreen(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSetCursor(int cursor)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutWarpPointer(int x, int y)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutEstablishOverlay(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutRemoveOverlay(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutUseLayer(GLenum layer)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutPostOverlayRedisplay(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutPostWindowOverlayRedisplay(int win)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutShowOverlay(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutHideOverlay(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+int glutCreateMenu(void (*func)(int))
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+void glutDestroyMenu(int menu)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+int glutGetMenu(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+void glutSetMenu(int menu)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutAddMenuEntry(const char *label, int value)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutAddSubMenu(const char *label, int submenu)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutChangeToMenuEntry(int item, const char *label, int value)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutChangeToSubMenu(int item, const char *label, int submenu)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutRemoveMenuItem(int item)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutAttachMenu(int button)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutDetachMenu(int button)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutDisplayFunc(void (*func)(void))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutReshapeFunc(void (*func)(int width, int height))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutKeyboardFunc(void (*func)(unsigned char key, int x, int y))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutMouseFunc(void (*func)(int button, int state, int x, int y))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutMotionFunc(void (*func)(int x, int y))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutPassiveMotionFunc(void (*func)(int x, int y))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutEntryFunc(void (*func)(int state))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutVisibilityFunc(void (*func)(int state))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutIdleFunc(void (*func)(void))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutTimerFunc(unsigned int millis, void (*func)(int value), int value)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutMenuStateFunc(void (*func)(int state))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSpecialFunc(void (*func)(int key, int x, int y))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSpaceballMotionFunc(void (*func)(int x, int y, int z))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSpaceballRotateFunc(void (*func)(int x, int y, int z))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSpaceballButtonFunc(void (*func)(int button, int state))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutButtonBoxFunc(void (*func)(int button, int state))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutDialsFunc(void (*func)(int dial, int value))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutTabletMotionFunc(void (*func)(int x, int y))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutTabletButtonFunc(void (*func)(int button, int state, int x, int y))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutMenuStatusFunc(void (*func)(int status, int x, int y))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutOverlayDisplayFunc(void (*func)(void))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutWindowStatusFunc(void (*func)(int state))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutKeyboardUpFunc(void (*func)(unsigned char key, int x, int y))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSpecialUpFunc(void (*func)(int key, int x, int y))
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutJoystickFunc(void (*func)(unsigned int buttonMask, int x, int y, int z), int pollInterval)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSetColor(int cell, GLfloat red, GLfloat green, GLfloat blue)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+GLfloat glutGetColor(int ndx, int component)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0.0;
+}
+
+
+void glutCopyColormap(int win)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+int glutGet(GLenum type)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+int glutDeviceGet(GLenum type)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+int glutExtensionSupported(const char *name)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+int glutGetModifiers(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+int glutLayerGet(GLenum type)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+void glutBitmapCharacter(void *font, int character)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+int glutBitmapWidth(void *font, int character)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+void glutStrokeCharacter(void *font, int character)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+int glutStrokeWidth(void *font, int character)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+int glutBitmapLength(void *font, const unsigned char *string)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+int glutStrokeLength(void *font, const unsigned char *string)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+void glutWireSphere(GLdouble radius, GLint slices, GLint stacks)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSolidSphere(GLdouble radius, GLint slices, GLint stacks)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutWireCone(GLdouble base, GLdouble height, GLint slices, GLint stacks)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSolidCone(GLdouble base, GLdouble height, GLint slices, GLint stacks)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutWireCube(GLdouble size)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSolidCube(GLdouble size)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutWireTorus(GLdouble innerRadius, GLdouble outerRadius, GLint sides, GLint rings)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSolidTorus(GLdouble innerRadius, GLdouble outerRadius, GLint sides, GLint rings)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutWireDodecahedron(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSolidDodecahedron(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutWireTeapot(GLdouble size)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSolidTeapot(GLdouble size)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutWireOctahedron(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSolidOctahedron(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutWireTetrahedron(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSolidTetrahedron(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutWireIcosahedron(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSolidIcosahedron(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+int glutVideoResizeGet(GLenum param)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+void glutSetupVideoResizing(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutStopVideoResizing(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutVideoResize(int x, int y, int width, int height)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutVideoPan(int x, int y, int width, int height)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutReportErrors(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutIgnoreKeyRepeat(int ignore)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutSetKeyRepeat(int repeatMode)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutForceJoystickFunc(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+void glutGameModeString(const char *string)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+int glutEnterGameMode(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
+
+
+void glutLeaveGameMode(void)
+{
+	__M2S_GLUT_NOT_IMPL__
+}
+
+
+int glutGameModeGet(GLenum mode)
+{
+	__M2S_GLUT_NOT_IMPL__
+	return 0;
+}
 
 
