e61c4bbfdd1ac007372630d64b4487734b6d12c2
yash <yash@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2012-07-12 17:44:28 +0000

    Update to ARM disassembler instructions

--- trunk/src/arch/arm/emu/emu.c
+++ trunk/src/arch/arm/emu/emu.c
@@ -19,15 +19,55 @@
 
 #include <arm-emu.h>
 
+
+void arm_elf_function_symbol(struct elf_file_t *elf_file, unsigned int inst_addr)
+{
+	unsigned int i;
+	struct elf_symbol_t *symbol;
+	for ( i = 0; i < list_count(elf_file->symbol_table); i++)
+	{
+		symbol = (struct elf_symbol_t* )list_get(elf_file->symbol_table, i);
+		if(symbol->value == inst_addr)
+		{
+			if((!strncmp(symbol->name, "$",1)))
+			{
+				continue;
+			}
+			else
+			{
+				printf ("\n%08x <%s>\n", symbol->value, symbol->name);
+				break;
+			}
+		}
+	}
+}
+
+unsigned int arm_dump_word_symbol(struct elf_file_t *elf_file, unsigned int inst_addr, void *inst_ptr)
+{
+	struct elf_symbol_t *symbol;
+	unsigned int word_flag;
+ 	symbol = elf_symbol_get_by_address(elf_file, inst_addr,	NULL);
+
+	if((!strncmp(symbol->name, "$d",2)))
+	{
+		printf (".word   0x%08x\n", *(unsigned int *)inst_ptr);
+		word_flag = 1;
+	}
+	else
+	{
+		word_flag = 0;
+	}
+
+	return (word_flag);
+}
+
 /*
  * Arm disassembler
  */
-
 void arm_emu_disasm(char *path)
 {
 	struct elf_file_t *elf_file;
 	struct elf_section_t *section;
-	struct elf_symbol_t *symbol;
 
 	char inst_str[MAX_STRING_SIZE];
 	int i;
@@ -53,15 +93,17 @@ void arm_emu_disasm(char *path)
 	for (inst_ptr = section->buffer.ptr; inst_ptr < section->buffer.ptr +
 			section->buffer.size; inst_ptr += 4)
 	{
-		symbol = elf_symbol_get_by_address(elf_file, (section->header->sh_addr + inst_index),
-			NULL);
-		if((section->header->sh_addr + inst_index) == (symbol->value))
-		{
-			printf ("\n%08x <%s>\n", symbol->value, symbol->name);
-		}
+
+		arm_elf_function_symbol(elf_file, (section->header->sh_addr + inst_index));
+
 		arm_inst_hex_dump(stdout, inst_ptr, (section->header->sh_addr + inst_index));
-		arm_inst_dump(stdout, inst_str, MAX_STRING_SIZE, inst_ptr , inst_index,
-			(section->header->sh_addr + inst_index));
+
+		if (!arm_dump_word_symbol(
+			elf_file, (section->header->sh_addr + inst_index),
+			inst_ptr))
+			arm_inst_dump(stdout, inst_str, MAX_STRING_SIZE, inst_ptr , inst_index,
+				(section->header->sh_addr + inst_index));
+
 		inst_index += 4;
 	}
 
