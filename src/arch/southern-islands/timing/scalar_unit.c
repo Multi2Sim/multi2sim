e103c7cc2b755f0087bfe5cba34be8c8693100d9
dschaa <dschaa@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2012-07-25 16:58:58 +0000

    First pass on generalized pipelines for SI
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@971 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- src/arch/southern-islands/timing/scalar_unit.c
+++ src/arch/southern-islands/timing/scalar_unit.c
@@ -1,183 +1,191 @@
 #include <southern-islands-timing.h>
-#include <heap.h>
 
 int si_gpu_scalar_unit_inflight_mem_accesses = 32;
-int si_gpu_scalar_unit_exec_latency = 1;
+int si_gpu_scalar_unit_alu_latency = 4;
+int si_gpu_scalar_unit_issue_width = 2;
 
-void si_scalar_unit_execute_writeback(struct si_scalar_unit_t *scalar_unit)
+
+void si_scalar_unit_writeback(struct si_scalar_unit_t *scalar_unit)
 {
+	struct si_uop_t *uop = NULL;
+	struct si_wavefront_t *wavefront;
+	struct si_work_group_t *work_group;
+	int i;
+	int list_count;
+
+	/* Process completed memory instructions */
+	list_count = linked_list_count(scalar_unit->mem_queue);
+	if (list_count)
+	{
+		linked_list_head(scalar_unit->mem_queue);
+		for (i = 0; i < list_count; i++)
+		{
+			uop = linked_list_get(scalar_unit->mem_queue);
+			assert(uop);
+
+			if (!uop->global_mem_witness)
+			{
+				/* Access complete, remove the uop from the queue */
+				linked_list_remove(scalar_unit->mem_queue);
+
+				/* Make the wavefront active again */
+				wavefront = uop->wavefront;
+				wavefront->ready = 1;
+
+				/* Free uop */
+				if (si_tracing())
+					si_gpu_uop_trash_add(uop);
+				else
+					si_uop_free(uop);
+			}
+			else 
+			{
+				linked_list_next(scalar_unit->mem_queue);
+			}
+		}
+	}
+
+	/* Process completed ALU instructions */
+	list_count = linked_list_count(scalar_unit->alu_queue);
+	if (list_count)
+	{
+		linked_list_head(scalar_unit->alu_queue);
+		for (i = 0; i < list_count; i++)
+		{
+			uop = linked_list_get(scalar_unit->alu_queue);
+			assert(uop);
+
+			if (uop->execute_ready <= si_gpu->cycle)
+			{
+				/* Access complete, remove the uop from the queue */
+				linked_list_remove(scalar_unit->alu_queue);
+
+				/* Make the wavefront active again */
+				wavefront = uop->wavefront;
+				work_group = wavefront->work_group;
+				if (!wavefront->finished)
+					wavefront->ready = 1;
+				else 
+					work_group->compute_unit_finished_count++;
+
+				/* Remove the uop from the execute buffer */
+				linked_list_remove(scalar_unit->exec_inst_buffer);
+
+				/* Check if wavefront finishes a work-group */
+				assert(work_group->compute_unit_finished_count <= 
+					work_group->wavefront_count);
+				if (work_group->compute_unit_finished_count == 
+					work_group->wavefront_count)
+					si_compute_unit_unmap_work_group(scalar_unit->compute_unit,
+						work_group);
+
+				/* Free uop */
+				if (si_tracing())
+					si_gpu_uop_trash_add(uop);
+				else
+					si_uop_free(uop);
+			}
+			else 
+			{
+				linked_list_next(scalar_unit->alu_queue);
+			}
+		}
+	}
+
+	/* Statistics */
+	si_gpu->last_complete_cycle = esim_cycle;
 }
 
 void si_scalar_unit_execute(struct si_scalar_unit_t *scalar_unit)
 {
 	struct si_uop_t *uop;
-	struct si_wavefront_t *wavefront;
-	struct si_work_group_t *work_group;
+	int issued_instructions = 0;
+	int list_count;
+	int i;
 
-	uop = scalar_unit->exec_buffer;
+	list_count = linked_list_count(scalar_unit->exec_inst_buffer);
 
 	/* No instruction to process */
-	if (!uop)
-		return;
-
-#if 0
-	heap_insert(scalar_unit->event_queue, si_gpu->cycle + si_gpu_scalar_unit_exec_latency, uop);
-#endif
-	
-	wavefront = uop->wavefront;
-	work_group = wavefront->work_group;
-
-	/* Make the wavefront active again */
-	if (!wavefront->finished)
-		wavefront->ready = 1;
-	else 
-		work_group->compute_unit_finished_count++;
-
-	/* Remove the uop from the execute buffer */
-	scalar_unit->exec_buffer = NULL;
-
-	/* Wavefront finishes a work-group */
-	assert(work_group->compute_unit_finished_count <= work_group->wavefront_count);
-	if (work_group->compute_unit_finished_count == work_group->wavefront_count)
-		si_compute_unit_unmap_work_group(scalar_unit->compute_unit, work_group);
-
-	/* Free uop */
-	if (si_tracing())
-		si_gpu_uop_trash_add(uop);
-	else
-		si_uop_free(uop);
-
-	/* Statistics */
-	si_gpu->last_complete_cycle = esim_cycle;
-}
-
-void si_scalar_unit_memory_writeback(struct si_scalar_unit_t *scalar_unit)
-{
-	struct si_uop_t *uop = NULL;
-	struct si_wavefront_t *wavefront;
-	int i;
-	int list_count;
-
-	list_count = linked_list_count(scalar_unit->mem_queue);
 	if (!list_count)
 		return;
 
-	linked_list_head(scalar_unit->mem_queue);
-	for (i = 0; i < list_count; i++)
+	linked_list_head(scalar_unit->exec_inst_buffer);
+	for (i = 0; i < list_count; i++) 
 	{
-		uop = linked_list_get(scalar_unit->mem_queue);
+		if (issued_instructions == si_gpu_scalar_unit_issue_width)
+			break;
+
+		/* Process uop */
+		uop = linked_list_get(scalar_unit->exec_inst_buffer); 
 		assert(uop);
 
-		if (!uop->global_mem_witness)
-		{
-			/* Access complete, remove the uop from the list */
-			linked_list_remove(scalar_unit->mem_queue);
+		/* Decode has not completed.  Safe to assume that no other
+		 * decoded instructions are ready */
+		if (si_gpu->cycle < uop->decode_ready)
 			break;
+		
+		/* Check if type is memory or ALU */
+		if (uop->wavefront->inst.info->fmt == SI_FMT_SMRD)
+		{	
+			/* Scalar memory read */
+			assert(linked_list_count(scalar_unit->mem_queue) <= 
+				si_gpu_scalar_unit_inflight_mem_accesses);
+
+			if (linked_list_count(scalar_unit->mem_queue) <
+				si_gpu_scalar_unit_inflight_mem_accesses)
+			{
+				/* Access global memory */
+				uop->global_mem_witness--;
+				uop->global_mem_access_addr = 
+					uop->wavefront->scalar_work_item->global_mem_access_addr;
+				mod_access(scalar_unit->compute_unit->global_memory, 
+					mod_access_load, uop->global_mem_access_addr, 
+					&uop->global_mem_witness, NULL, NULL);
+
+				/* Add the uop to the inflight memory access queue */
+				linked_list_remove(scalar_unit->exec_inst_buffer); 
+				linked_list_add(scalar_unit->mem_queue, uop);
+
+				issued_instructions++;
+				scalar_unit->inst_count++;
+				scalar_unit->wavefront_count++;
+			}
+			else 
+			{
+				/* Memory unit is busy, try the next instruction */
+				linked_list_next(scalar_unit->exec_inst_buffer);
+			}
+			continue;
 		}
-
-		linked_list_next(scalar_unit->mem_queue);
-	}
-
-	/* No completed accesses */
-	if (i == list_count)
-		return;
-
-	/* Make the wavefront active again */
-	wavefront = uop->wavefront;
-	wavefront->ready = 1;
-
-	/* Free uop */
-	if (si_tracing())
-		si_gpu_uop_trash_add(uop);
-	else
-		si_uop_free(uop);
-
-	/* Statistics */
-	si_gpu->last_complete_cycle = esim_cycle;
-}
-
-void si_scalar_unit_memory(struct si_scalar_unit_t *scalar_unit)
-{
-	struct si_uop_t *uop;
-
-	uop = scalar_unit->mem_buffer; 
-
-	/* No instruction to process */
-	if (!uop)
-		return;
-
-	assert(linked_list_count(scalar_unit->mem_queue) < 
-		si_gpu_scalar_unit_inflight_mem_accesses);
-
-	/* Max outstanding memory accesses is reached, stall */
-	if (linked_list_count(scalar_unit->mem_queue) == si_gpu_scalar_unit_inflight_mem_accesses-1)
-		return;
-
-	/* Access global memory */
-	uop->global_mem_witness--;
-	uop->global_mem_access_addr = uop->wavefront->scalar_work_item->global_mem_access_addr;
-	mod_access(scalar_unit->compute_unit->global_memory, mod_access_load, 
-		uop->global_mem_access_addr, &uop->global_mem_witness, NULL, NULL);
-
-	/* Add the uop to the inflight memory access queue */
-	linked_list_add(scalar_unit->mem_queue, uop);
-
-	/* Remove the uop from the memory buffer */
-	scalar_unit->mem_buffer = NULL;
-}
-
-void si_scalar_unit_read(struct si_scalar_unit_t *scalar_unit)
-{
-	struct si_uop_t *uop;
-
-	/* Process uop */
-	uop = scalar_unit->inst_buffer; 
-
-	/* No instruction to process */
-	if (!uop)
-		return;
-
-	/* Decode has not completed */
-	if (si_gpu->cycle < uop->decode_ready)
-		return;
-	
-	/* Check if type is memory or ALU */
-	if (uop->wavefront->inst.info->fmt == SI_FMT_SMRD)
-	{	
-		/* Scalar memory read */
-		if (!scalar_unit->mem_buffer)
+		else
 		{
-			scalar_unit->mem_buffer = uop;
-		}
-		else 
-		{
-			/* Memory unit is busy */
-			return;
+			/* Scalar ALU */
+			if (linked_list_count(scalar_unit->alu_queue) <= 
+				si_gpu_scalar_unit_issue_width)
+			{
+				/* Execution unit is available */
+				uop->execute_ready = si_gpu->cycle + 
+					si_gpu_scalar_unit_alu_latency;
+				linked_list_remove(scalar_unit->exec_inst_buffer); 
+				linked_list_add(scalar_unit->alu_queue, uop);
+
+				issued_instructions++;
+				scalar_unit->inst_count++;
+				scalar_unit->wavefront_count++;
+			}
+			else 
+			{
+				/* Execution unit is busy, try the next instruction */
+				linked_list_next(scalar_unit->exec_inst_buffer);
+			}
+			continue;
 		}
 	}
-	else
-	{
-		/* Scalar ALU */
-		if (!scalar_unit->exec_buffer)
-		{
-			scalar_unit->exec_buffer = uop;
-		}
-		else 
-		{
-			/* Execution unit is busy */
-			return;
-		}
-	}
-
-	/* Uop was processed, so remove it from instruction buffer */
-	scalar_unit->inst_buffer = NULL;
 }
 
 void si_scalar_unit_run(struct si_scalar_unit_t *scalar_unit)
 {
-	si_scalar_unit_memory_writeback(scalar_unit);
+	si_scalar_unit_writeback(scalar_unit);
 	si_scalar_unit_execute(scalar_unit);
-	si_scalar_unit_memory(scalar_unit);
-	si_scalar_unit_read(scalar_unit);
 }
 
--- src/arch/southern-islands/timing/vector_mem.c
+++ src/arch/southern-islands/timing/vector_mem.c
@@ -1,7 +1,7 @@
 #include <southern-islands-timing.h>
 
-int si_gpu_vector_mem_issue_rate = 1;
-int si_gpu_vector_mem_latency = 4;
+int si_gpu_vector_mem_issue_width = 1;
+
 
 void si_vector_mem_writeback(struct si_vector_mem_unit_t *vector_mem)
 {
@@ -11,25 +11,21 @@ void si_vector_mem_writeback(struct si_vector_mem_unit_t *vector_mem)
 
 void si_vector_mem_execute(struct si_vector_mem_unit_t *vector_mem)
 {
-
-
-}
-
-void si_vector_mem_read(struct si_vector_mem_unit_t *vector_mem)
-{
-	/* FIXME */
-
 	struct si_uop_t *uop;
-	uop = vector_mem->inst_buffer; 
+	int list_count;
+
+	list_count = linked_list_count(vector_mem->exec_inst_buffer);
 
 	/* No instruction to process */
-	if (!uop)
+	if (!list_count)
 		return;
+
+	linked_list_head(vector_mem->exec_inst_buffer);
+	uop = linked_list_get(vector_mem->exec_inst_buffer);
+	linked_list_remove(vector_mem->exec_inst_buffer);
 
 	/* Make the wavefront active again */
 	uop->wavefront->ready = 1;
-
-	vector_mem->inst_buffer = NULL;
 
 	si_uop_free(uop);
 }
@@ -39,8 +35,4 @@ void si_vector_mem_run(struct si_vector_mem_unit_t *vector_mem)
 	/* SIMD stages */
 	si_vector_mem_writeback(vector_mem);
 	si_vector_mem_execute(vector_mem);
-	si_vector_mem_read(vector_mem);
-
-	/* Stats */
-	vector_mem->cycle++;
 }
