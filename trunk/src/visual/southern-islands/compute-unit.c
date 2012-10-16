11eca4bf6bd07ee69703560bf017e406cc0a9606
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2012-10-16 03:41:15 +0000

    Headers in src/visual/southern-islands

--- trunk/src/visual/southern-islands/compute-unit.c
+++ trunk/src/visual/southern-islands/compute-unit.c
@@ -17,13 +17,16 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <gtk/gtk.h>
 #include <stdlib.h>
 
 #include <lib/mhandle/mhandle.h>
 #include <lib/util/debug.h>
 #include <lib/util/hash-table.h>
 
-#include "visual-southern-islands.h"
+#include "compute-unit.h"
+#include "inst.h"
+#include "work-group.h"
 
 
 struct vi_si_compute_unit_t *vi_si_compute_unit_create(char *name)
