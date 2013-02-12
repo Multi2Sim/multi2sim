b7b90e1ffe3012e12233306cdbff166e4c7304fc
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-02-12 17:00:17 +0000

    psrlw, psrld, psrlq, psrldq
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@1356 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- src/arch/x86/emu/machine-sse2.c
+++ src/arch/x86/emu/machine-sse2.c
@@ -1403,6 +1403,33 @@ void x86_isa_psraw_xmm_xmmm128_impl(struct x86_ctx_t *ctx)
 }
 
 
+void x86_isa_psrlw_xmmm128_imm8_impl(struct x86_ctx_t *ctx)
+{
+	union x86_xmm_reg_t dest;
+	union x86_xmm_reg_t src;
+
+	x86_isa_load_xmmm128(ctx, dest.as_uchar);
+	memset(src.as_uchar, 0, sizeof src);
+	src.as_uchar[0] = ctx->inst.imm.b;
+
+	__X86_ISA_ASM_START__
+	asm volatile (
+		"movdqu %1, %%xmm0\n\t"
+		"movdqu %0, %%xmm1\n\t"
+		"psrlw %%xmm0, %%xmm1\n\t"
+		"movdqu %%xmm1, %0\n\t"
+		: "=m" (dest)
+		: "m" (src)
+		: "xmm0", "xmm1"
+	);
+	__X86_ISA_ASM_END__
+
+	x86_isa_store_xmmm128(ctx, dest.as_uchar);
+
+	x86_uinst_new(ctx, x86_uinst_xmm_shift, x86_dep_xmmm128, 0, 0, x86_dep_xmmm128, 0, 0, 0);
+}
+
+
 void x86_isa_psrad_xmmm128_imm8_impl(struct x86_ctx_t *ctx)
 {
 	union x86_xmm_reg_t dest;
@@ -1456,9 +1483,158 @@ void x86_isa_psrad_xmm_xmmm128_impl(struct x86_ctx_t *ctx)
 }
 
 
+void x86_isa_psrlw_xmm_xmmm128_impl(struct x86_ctx_t *ctx)
+{
+	union x86_xmm_reg_t dest;
+	union x86_xmm_reg_t src;
+
+	x86_isa_load_xmm(ctx, dest.as_uchar);
+	x86_isa_load_xmmm128(ctx, src.as_uchar);
+
+	__X86_ISA_ASM_START__
+	asm volatile (
+		"movdqu %1, %%xmm0\n\t"
+		"movdqu %0, %%xmm1\n\t"
+		"psrlw %%xmm0, %%xmm1\n\t"
+		"movdqu %%xmm1, %0\n\t"
+		: "=m" (dest)
+		: "m" (src)
+		: "xmm0", "xmm1"
+	);
+	__X86_ISA_ASM_END__
+
+	x86_isa_store_xmm(ctx, dest.as_uchar);
+
+	x86_uinst_new(ctx, x86_uinst_xmm_shift, x86_dep_xmmm128, x86_dep_xmm, 0, x86_dep_xmm, 0, 0, 0);
+}
+
+
+void x86_isa_psrld_xmm_xmmm128_impl(struct x86_ctx_t *ctx)
+{
+	union x86_xmm_reg_t dest;
+	union x86_xmm_reg_t src;
+
+	x86_isa_load_xmm(ctx, dest.as_uchar);
+	x86_isa_load_xmmm128(ctx, src.as_uchar);
+
+	__X86_ISA_ASM_START__
+	asm volatile (
+		"movdqu %1, %%xmm0\n\t"
+		"movdqu %0, %%xmm1\n\t"
+		"psrld %%xmm0, %%xmm1\n\t"
+		"movdqu %%xmm1, %0\n\t"
+		: "=m" (dest)
+		: "m" (src)
+		: "xmm0", "xmm1"
+	);
+	__X86_ISA_ASM_END__
+
+	x86_isa_store_xmm(ctx, dest.as_uchar);
+
+	x86_uinst_new(ctx, x86_uinst_xmm_shift, x86_dep_xmmm128, x86_dep_xmm, 0, x86_dep_xmm, 0, 0, 0);
+}
+
+
+void x86_isa_psrld_xmmm128_imm8_impl(struct x86_ctx_t *ctx)
+{
+	union x86_xmm_reg_t dest;
+	union x86_xmm_reg_t src;
+
+	x86_isa_load_xmmm128(ctx, dest.as_uchar);
+	memset(src.as_uchar, 0, sizeof src);
+	src.as_uchar[0] = ctx->inst.imm.b;
+
+	__X86_ISA_ASM_START__
+	asm volatile (
+		"movdqu %1, %%xmm0\n\t"
+		"movdqu %0, %%xmm1\n\t"
+		"psrld %%xmm0, %%xmm1\n\t"
+		"movdqu %%xmm1, %0\n\t"
+		: "=m" (dest)
+		: "m" (src)
+		: "xmm0", "xmm1"
+	);
+	__X86_ISA_ASM_END__
+
+	x86_isa_store_xmmm128(ctx, dest.as_uchar);
+
+	x86_uinst_new(ctx, x86_uinst_xmm_shift, x86_dep_xmmm128, 0, 0, x86_dep_xmmm128, 0, 0, 0);
+}
+
+
+void x86_isa_psrlq_xmm_xmmm128_impl(struct x86_ctx_t *ctx)
+{
+	union x86_xmm_reg_t dest;
+	union x86_xmm_reg_t src;
+
+	x86_isa_load_xmm(ctx, dest.as_uchar);
+	x86_isa_load_xmmm128(ctx, src.as_uchar);
+
+	__X86_ISA_ASM_START__
+	asm volatile (
+		"movdqu %1, %%xmm0\n\t"
+		"movdqu %0, %%xmm1\n\t"
+		"psrlq %%xmm0, %%xmm1\n\t"
+		"movdqu %%xmm1, %0\n\t"
+		: "=m" (dest)
+		: "m" (src)
+		: "xmm0", "xmm1"
+	);
+	__X86_ISA_ASM_END__
+
+	x86_isa_store_xmm(ctx, dest.as_uchar);
+
+	x86_uinst_new(ctx, x86_uinst_xmm_shift, x86_dep_xmmm128, x86_dep_xmm, 0, x86_dep_xmm, 0, 0, 0);
+}
+
+
+void x86_isa_psrlq_xmmm128_imm8_impl(struct x86_ctx_t *ctx)
+{
+	union x86_xmm_reg_t dest;
+	union x86_xmm_reg_t src;
+
+	x86_isa_load_xmmm128(ctx, dest.as_uchar);
+	memset(src.as_uchar, 0, sizeof src);
+	src.as_uchar[0] = ctx->inst.imm.b;
+
+	__X86_ISA_ASM_START__
+	asm volatile (
+		"movdqu %1, %%xmm0\n\t"
+		"movdqu %0, %%xmm1\n\t"
+		"psrlq %%xmm0, %%xmm1\n\t"
+		"movdqu %%xmm1, %0\n\t"
+		: "=m" (dest)
+		: "m" (src)
+		: "xmm0", "xmm1"
+	);
+	__X86_ISA_ASM_END__
+
+	x86_isa_store_xmmm128(ctx, dest.as_uchar);
+
+	x86_uinst_new(ctx, x86_uinst_xmm_shift, x86_dep_xmmm128, 0, 0, x86_dep_xmmm128, 0, 0, 0);
+}
+
+
 void x86_isa_psrldq_xmmm128_imm8_impl(struct x86_ctx_t *ctx)
 {
-	x86_isa_error(ctx, "%s: not implemented", __FUNCTION__);
+	union x86_xmm_reg_t dest;
+	union x86_xmm_reg_t src;
+
+	int count;
+	int i;
+
+	x86_isa_load_xmmm128(ctx, src.as_uchar);
+	count = ctx->inst.imm.b;
+	if (count > 16)
+		count = 16;
+
+	memset(dest.as_uchar, 0, sizeof dest);
+	for (i = 0; i < 16 - count; i++)
+		dest.as_uchar[i] = src.as_uchar[i + count];
+
+
+	x86_isa_store_xmmm128(ctx, dest.as_uchar);
+	x86_uinst_new(ctx, x86_uinst_xmm_shift, x86_dep_xmmm128, 0, 0, x86_dep_xmmm128, 0, 0, 0);
 }
 
 
