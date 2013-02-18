68ebe3958e4540f08ddf21b413d25503e501d51b
tushar.swamy <tushar.swamy@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-02-18 17:51:32 +0000

    SMRD and DS format dump functions replaced. SDST token for SMRD formats replaced with SMRD_SDST. VDST token for DS formats replaced with DS_VDST. Dump functions also replaced in wavefront.c

--- trunk/src/arch/southern-islands/emu/wavefront.c
+++ trunk/src/arch/southern-islands/emu/wavefront.c
@@ -311,7 +311,7 @@ void si_wavefront_execute(struct si_wavefront_t *wavefront)
 		/* Dump instruction string when debugging */
 		if (debug_status(si_isa_debug_category))
 		{
-			si_inst_dump_smrd(inst, wavefront->inst_size, wavefront->pc,
+			si_inst_dump_new(inst, wavefront->inst_size, wavefront->pc,
 					ndrange->inst_buffer + wavefront->pc,
 					inst_dump, MAX_INST_STR_SIZE);
 			si_isa_debug("\n%s", inst_dump);
@@ -517,7 +517,7 @@ void si_wavefront_execute(struct si_wavefront_t *wavefront)
 		/* Dump instruction string when debugging */
 		if (debug_status(si_isa_debug_category))
 		{
-			si_inst_dump_ds(inst, wavefront->inst_size, wavefront->pc,
+			si_inst_dump_new(inst, wavefront->inst_size, wavefront->pc,
 					ndrange->inst_buffer + wavefront->pc,
 					inst_dump, MAX_INST_STR_SIZE);
 			si_isa_debug("\n%s", inst_dump);
