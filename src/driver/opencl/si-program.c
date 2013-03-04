5875b9a787eaffb7d1b74f54962102db1abefc25
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-03-04 22:27:53 +0000

    Starting to port all of Dana's code from 'driver/opencl-old' into 'driver/opencl'. - Program object populated. - Constant buffers set up in ABI call 'si_program_set_binary'
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@1411 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- src/driver/opencl/si-program.c
+++ src/driver/opencl/si-program.c
@@ -17,9 +17,17 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <lib/mhandle/mhandle.h>
-#include <lib/util/list.h>
+#include <assert.h>
 
+#include <arch/southern-islands/emu/emu.h>
+#include <lib/mhandle/mhandle.h>
+#include <lib/util/debug.h>
+#include <lib/util/elf-format.h>
+#include <lib/util/list.h>
+#include <lib/util/string.h>
+#include <mem-system/memory.h>
+
+#include "opencl.h"
 #include "si-program.h"
 
 
@@ -65,8 +73,87 @@ void opencl_si_program_list_done(void)
 
 
 /*
+ * Constant Buffer
+ */
+
+struct opencl_si_constant_buffer_t *opencl_si_constant_buffer_create(int id,
+	unsigned int device_ptr)
+{
+	struct opencl_si_constant_buffer_t *constant_buffer;
+
+	/* Initialize */
+	constant_buffer = xcalloc(1, sizeof(struct opencl_si_constant_buffer_t));
+	constant_buffer->id = id;
+	constant_buffer->device_ptr = device_ptr;
+
+	/* Return */
+	return constant_buffer;
+}
+
+
+void opencl_si_constant_buffer_free(struct opencl_si_constant_buffer_t *constant_buffer)
+{
+	free(constant_buffer);
+}
+
+
+
+
+/*
  * Program
  */
+
+static void opencl_si_program_initialize_constant_buffers(
+		struct opencl_si_program_t *program)
+{
+	struct elf_file_t *elf_file;
+	struct elf_buffer_t elf_buffer;
+	struct elf_symbol_t *symbol;
+	struct opencl_si_constant_buffer_t *constant_buffer;
+
+	char symbol_name[MAX_STRING_SIZE];
+	int i;
+
+	elf_file = program->elf_file;
+	assert(elf_file);
+
+	/* Constant buffers encoded in ELF file */
+	program->constant_buffer_list = list_create_with_size(25);
+	for (i = 0; i < 25; i++) 
+		list_add(program->constant_buffer_list, NULL);
+
+	/* We can't tell how many constant buffers exist in advance, but we
+	 * know they should be enumerated, starting with '2'.  This loop
+	 * searches until a constant buffer matching the format is not 
+	 * found. */
+	for (i = 2; i < 25; i++) 
+	{
+		/* Create string of symbol name */
+		sprintf(symbol_name, "__OpenCL_%d_global", i);
+
+		/* Check to see if symbol exists */
+		symbol = elf_symbol_get_by_name(elf_file, symbol_name);
+		if (!symbol)
+			break;
+
+		/* Read the elf symbol into a buffer */
+		elf_symbol_read_content(elf_file, symbol, &elf_buffer);
+		opencl_debug("\tconstant buffer '%s' found with size %d\n",
+			symbol->name, symbol->size);
+
+		/* Copy constant buffer into device memory */
+		mem_write(si_emu->global_mem, si_emu->global_mem_top,
+				symbol->size, elf_buffer.ptr);
+
+		/* Create buffer */
+		constant_buffer = opencl_si_constant_buffer_create(i,
+				si_emu->global_mem_top);
+		si_emu->global_mem_top += symbol->size;
+
+		/* Add the constant buffer to the list */
+		list_set(program->constant_buffer_list, i, constant_buffer);
+	}
+} 
 
 struct opencl_si_program_t *opencl_si_program_create(void)
 {
@@ -87,5 +174,41 @@ struct opencl_si_program_t *opencl_si_program_create(void)
 
 void opencl_si_program_free(struct opencl_si_program_t *program)
 {
+	int index;
+
+	/* Free constant buffers */
+	if (program->constant_buffer_list)
+	{
+		LIST_FOR_EACH(program->constant_buffer_list, index)
+			opencl_si_constant_buffer_free(list_get(
+					program->constant_buffer_list,
+					index));
+		list_free(program->constant_buffer_list);
+	}
+
+	/* ELF file */
+	if (program->elf_file)
+		elf_file_free(program->elf_file);
+
+	/* Free program */
 	free(program);
 }
+
+
+void opencl_si_program_set_binary(struct opencl_si_program_t *program,
+		void *buf, unsigned int size)
+{
+	char name[MAX_STRING_SIZE];
+
+	/* Already set */
+	if (program->elf_file)
+		fatal("%s: binary already set", __FUNCTION__);
+
+	/* Load ELF binary from guest memory */
+	snprintf(name, sizeof name, "program[%d].externalELF", program->id);
+	program->elf_file = elf_file_create_from_buffer(buf, size, name);
+
+	/* Initialize constant buffers */
+	opencl_si_program_initialize_constant_buffers(program);
+}
+
