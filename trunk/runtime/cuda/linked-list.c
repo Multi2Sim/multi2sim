b3929a80b5eb3ba217a16ca9aae5edc7ad26b040
chen <chen@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-03-19 01:27:54 +0000

    * cuda runtime cleaned up

--- trunk/runtime/cuda/linked-list.c
+++ trunk/runtime/cuda/linked-list.c
@@ -17,10 +17,9 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <assert.h>
-
 #include "linked-list.h"
-#include "mhandle.h"
+
+
 
 
 /* Creation */
