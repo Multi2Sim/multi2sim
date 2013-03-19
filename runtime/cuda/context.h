5d704abf602d19aec8c869f20604c459b4dfcc29
chen <chen@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-03-19 21:22:07 +0000

    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@1472 a6503f54-2359-11de-8ce0-bfd7a313a9c3

---
+++ runtime/cuda/context.h
@@ -0,0 +1,44 @@
+/*
+ *  Multi2Sim
+ *  Copyright (C) 2012  Rafael Ubal (ubal@ece.neu.edu)
+ *
+ *  This module is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This module is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this module; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef DRIVER_CUDA_CONTEXT_H
+#define DRIVER_CUDA_CONTEXT_H
+
+#include <assert.h>
+#include "../include/cuda.h"
+#include "../include/cuda_runtime_api.h"
+
+#include "mhandle.h"
+
+
+
+
+struct CUctx_st
+{
+	unsigned int id;
+	int ref_count;
+
+	unsigned int device_id;
+};
+
+CUcontext cuda_context_create(void);
+void cuda_context_free(CUcontext context);
+
+#endif
+
