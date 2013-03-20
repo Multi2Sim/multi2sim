05e487966be84318983ef885f928bd774380e5fd
ubal <rafael.ubal.83@gmail.com>
2013-03-20 15:51:14 +0000

    - Flag '-Werror' added again to prevent most common compilation warnings. The   Southern Islands disassembler using Flex and Bison overwrites now the   compilation rules for 'lex.c' and 'parser.c' (automatically generated by the   tools), and removes '-Werror' only for these. This will tolerate older   Flex/Bison versions that generate code with warnings, while still being strict   with the warnings in the rest of the simulator. - Existing warnings in new Fermi code fixed.

--- runtime/cuda/debug.c
+++ runtime/cuda/debug.c
@@ -17,9 +17,10 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <stdarg.h>
+#include <stdlib.h>
+
 #include "debug.h"
-
-
 
 
 void fatal(char *fmt, ...)
