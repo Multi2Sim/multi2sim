7ce6d23d6c5e01ff3449b66094b3b1d4de700479
steven.gurfinkel <steven.gurfinkel@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-04-30 23:26:13 +0000

    Fused OpenCL Execution - Added ID parameter so that scheduler knows which device is requeting work.

--- trunk/runtime/opencl/partition-strategy.h
+++ trunk/runtime/opencl/partition-strategy.h
@@ -2,7 +2,7 @@
 #define __PARTITION_STRATEGY_H__
 
 typedef void *(*opencl_strategy_create_t)(int num_devices, unsigned int dims, unsigned int *groups);
-typedef int (*opencl_strategy_get_partition_t)(void *inst, int desired_groups, unsigned int *group_offset, unsigned int *group_count);
+typedef int (*opencl_strategy_get_partition_t)(void *inst, int id, int desired_groups, unsigned int *group_offset, unsigned int *group_count);
 typedef void (*opencl_strategy_destroy_t)(void *inst);
 
 struct opencl_partition_strategy
@@ -12,6 +12,6 @@ struct opencl_partition_strategy
 	opencl_strategy_destroy_t destroy;
 };
 
-struct opencl_partition_strategy *get_strategy();
+const struct opencl_partition_strategy *get_strategy();
 
 #endif
