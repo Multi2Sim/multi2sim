23e5d11c8dcf21ba71a3f3a43bdb6ac7dbe28b30
steven.gurfinkel <steven.gurfinkel@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-05-09 21:00:36 +0000

    Fused OpenCL Execution - Supports new nested ELF format - Added some more flexibility to scheduler.

--- runtime/opencl/union-program.c
+++ runtime/opencl/union-program.c
@@ -3,6 +3,8 @@
 #include "mhandle.h"
 #include "program.h"
 #include "union-device.h"
+#include "elf-format.h"
+#include "elf.h"
 
 struct opencl_union_program_t *opencl_union_program_create(
 	struct opencl_program_t *parent,
@@ -10,55 +12,46 @@ struct opencl_union_program_t *opencl_union_program_create(
 	void *binary,
 	unsigned int length)
 {
-	int total;
 	int i, j;
 	struct opencl_union_program_t *program;
-	struct opencl_union_binary_t *bin;
-	void **binaries;
 	struct opencl_device_t *subdevice;
-
-	bin = binary;
-	assert(bin->num_entries >= list_count(device->devices));
+	struct elf_file_t *elf_file;
 
 	program = xcalloc(1, sizeof (struct opencl_union_program_t));
 	program->parent = parent;
 	program->programs = list_create();
 	program->device = device;
 
-	total = sizeof *bin + bin->num_entries * sizeof bin->entry_sizes[0];
-	binaries = xcalloc(bin->num_entries, sizeof (void *));
+	elf_file = elf_file_create_from_buffer(binary, length, "Fused OpenCL Binary");
 
-	/* go through all the binaries an assign them to a two-dimensional array for easy access */
-	for (i = 0; i < bin->num_entries; i++)
-	{
-		binaries[i] = (char *)binary + total;
-		total += bin->entry_sizes[i];
-	}
-	assert(total == length);
 	/* find the binary that corresponds to each device in the union */
 	LIST_FOR_EACH(device->devices, i)
 	{
 		int match = 0;
 		subdevice = list_get(device->devices, i);
-		for (j = 0; j < bin->num_entries; j++)
+
+		/* go through all the binaries.  When one matches, use it to create a program for the current device */
+		LIST_FOR_EACH(elf_file->section_list, j)
 		{
-			/* if it's a match, create a subdevice-specific program object */
-			if (subdevice->arch_program_valid_binary_func(subdevice->arch_device, binaries[j], bin->entry_sizes[j]))
+			struct elf_section_t *section = list_get(elf_file->section_list, j);
+			if (!strcmp(section->name, ".text"))
 			{
-				list_add(
-					program->programs, 
-					subdevice->arch_program_create_func(
-						NULL, 
-						subdevice->arch_device, 
-						binaries[j], 
-						bin->entry_sizes[j]));
-				match = 1;
+				if (subdevice->arch_program_valid_binary_func(subdevice->arch_device, section->buffer.ptr, section->buffer.size))
+				{
+					list_add(
+						program->programs, 
+						subdevice->arch_program_create_func(
+							NULL, 
+							subdevice->arch_device, 
+							section->buffer.ptr,
+							section->buffer.size));
+					match = 1;
+				}
 			}
 		}
 		assert(match == 1);
 	}
-
-	free(binaries);
+	elf_file_free(elf_file);
 	return program;
 }
 
@@ -81,10 +74,20 @@ void opencl_union_program_free(struct opencl_union_program_t *program)
 int opencl_union_program_valid_binary(void *dev, void *binary, unsigned int length)
 {
 	int i;
-	struct opencl_union_binary_t *bin;
 	struct opencl_union_device_t *device = dev;
+	Elf32_Ehdr *h = (Elf32_Ehdr *) binary;
+	struct elf_file_t *elf_file;
 
-	bin = (struct opencl_union_binary_t *)binary;
+	if (
+		h->e_ident[EI_MAG0] != ELFMAG0 || 
+		h->e_ident[EI_MAG1] != ELFMAG1 || 
+		h->e_ident[EI_MAG2] != ELFMAG2 || 
+		h->e_ident[EI_MAG3] != ELFMAG3 ||
+		h->e_machine != EM_NONE)
+	{
+		return 0;
+	}
+	elf_file = elf_file_create_from_buffer(binary, length, "Fused OpenCL Binary");
 
 	/* get each subdevice and check to see if it has a binary */
 	LIST_FOR_EACH(device->devices, i)
@@ -94,23 +97,24 @@ int opencl_union_program_valid_binary(void *dev, void *binary, unsigned int leng
 		subdevice = list_get(device->devices, i);
 
 		int valid = 0;
-		int offset = sizeof *bin + bin->num_entries * sizeof bin->entry_sizes[0];
+
 		/* go through binaries. */
-		for (j = 0; j < bin->num_entries; j++)
+		LIST_FOR_EACH(elf_file->section_list, j)
 		{
-			if (subdevice->arch_program_valid_binary_func(subdevice->arch_device, (char *)binary + offset, bin->entry_sizes[j]))
+			struct elf_section_t *section = list_get(elf_file->section_list, j);	
+			if (subdevice->arch_program_valid_binary_func(subdevice->arch_device, section->buffer.ptr, section->buffer.size))
 			{
 				valid = 1;
 				break;
 			}
 
-			offset += bin->entry_sizes[j];
 		}
 
-		/* make sure that a device acepts the binary */
+		/* make sure that there's a binary for every device */
 		if (!valid)
 			return 0;
 	}
+	elf_file_free(elf_file);
 
 	return 1;
 }
