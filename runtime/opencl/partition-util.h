d67306c8d722d9e50f9b69663a207ac54f179b16
steven.gurfinkel <steven.gurfinkel@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-05-09 02:50:55 +0000

    Fused OpenCL Execution - Fixed some bugs in binary detection - Additions to partition utilities - Execution still not fully working

--- runtime/opencl/partition-util.h
+++ runtime/opencl/partition-util.h
@@ -11,12 +11,39 @@ struct cube_t
 	struct list_t *list; /* each int *, allocated 2 * dims ints. */
 };
 
-struct cube_t *cube_init(int dims, unsigned int *size);
-void cube_remove_region(unsigned int *start, unsigned int *size);
+struct cube_t *cube_init(int dims, const unsigned int *size);
+void cube_remove_region(struct cube_t *cube, const unsigned int *start, const unsigned int *size);
 void cube_destroy(struct cube_t *cube);
+void cube_get_region(
+	struct cube_t *cube,
+	unsigned int *start_out, 
+	unsigned int *size_out, 
+	const unsigned int *target_size, 
+	const unsigned int *target_loc);
 
 /* non-member helper functions */
-int get_intersection(int dims, unsigned int *inter, unsigned int *a, unsigned int *b);
+int get_intersection(
+	int dims, 
+	unsigned int *inter, 
+	const unsigned int *a, 
+	const unsigned int *b);
 
+int get_linear_intersection(
+	unsigned int *start, 
+	unsigned int *len, 
+	unsigned int ainit, 
+	unsigned int alen, 
+	unsigned int binit, 
+	unsigned int blen);
+
+void remove_from_cube(
+	int dims,
+	struct list_t *result,
+	const unsigned int *cube,
+	const unsigned int *remove);
+
+int covers_cube(int dims, unsigned int *cube, unsigned int *other);
+
+unsigned int get_centroid_distance(int dims, unsigned int *cube, unsigned int *point);
 
 #endif
