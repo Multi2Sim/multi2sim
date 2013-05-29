81e7928fa0215e107f0f78ccd76a9c3b18afb688
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-05-29 16:45:10 +0000

    - Integrated C++ code in LLVM-to-SI back-end, needed to access LLVM functionality that does not have C bindings. - LLVM pass to module loaded from bitcode that just dumps instruction by instruction. This will be the baseline code for translation to Southern Islands.

---
+++ trunk/tools/clcc/llvm2si/translate.h
@@ -0,0 +1,32 @@
+/*
+ *  Multi2Sim
+ *  Copyright (C) 2013  Rafael Ubal (ubal@ece.neu.edu)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef TOOLS_CLCC_LLVM2SI_TRANSLATE_H
+#define TOOLS_CLCC_LLVM2SI_TRANSLATE_H
+
+#ifdef __cplusplus
+#define __EXTERN_C__ extern "C"
+#else
+#define __EXTERN_C__
+#endif
+
+__EXTERN_C__ void llvm2si_translate(char *source_file, char *output_file);
+
+#endif
+
