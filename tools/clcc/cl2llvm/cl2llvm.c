d0a531d994e69db7fa11ea206cdd43965bcc04f9
chris.barton <chris.barton@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-06-12 17:48:38 +0000

    Added the no unwind function attribute to built-in function declarations Built-in function declarations only occur when they are used in the program
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@1756 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- tools/clcc/cl2llvm/cl2llvm.c
+++ tools/clcc/cl2llvm/cl2llvm.c
@@ -17,16 +17,15 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <llvm-c/Core.h>
 #include <llvm-c/Analysis.h>
+#include <llvm-c/BitWriter.h>
+#include <llvm-c/Core.h>
 #include <llvm-c/ExecutionEngine.h>
 #include <llvm-c/Target.h>
 #include <llvm-c/Transforms/Scalar.h>
-#include <llvm-c/BitWriter.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <lib/mhandle/mhandle.h>
 #include <lib/util/debug.h>
@@ -34,12 +33,13 @@
 #include <lib/util/list.h>
 
 #include "arg.h"
-#include "type.h"
+#include "cl2llvm.h"
 #include "declarator-list.h"
 #include "function.h"
-#include "symbol.h"
-#include "cl2llvm.h"
 #include "parser.h"
+#include "symbol.h"
+#include "type.h"
+#include "built-in-funcs.h"
 
 extern void cl2llvm_set_lineno(int);
 extern int cl2llvm_col_num;
@@ -50,17 +50,20 @@ extern int block_count;
 LLVMBuilderRef cl2llvm_builder;
 LLVMModuleRef cl2llvm_module;
 
+/* Built-in function table */
+struct hash_table_t *cl2llvm_built_in_func_table;
+
 /* Current file being compiled */
 char *cl2llvm_file_name;
 
-/*Preprocessor file list*/
+/* Preprocessor file list */
 struct list_t *cl2llvm_preprcr_file_list;
 
 
 void cl2llvm_yyerror(char *s)
 {
-	printf("%s:%d:%d: error: %s\n", cl2llvm_file_name, cl2llvm_yyget_lineno(),
-			cl2llvm_get_col_num(), s);
+	printf("%s:%d:%d: error: %s\n", cl2llvm_file_name,
+		cl2llvm_yyget_lineno(), cl2llvm_get_col_num(), s);
 	exit(1);
 }
 
@@ -68,9 +71,10 @@ void cl2llvm_yyerror(char *s)
 void cl2llvm_yyerror_fmt(char *fmt, ...)
 {
 	va_list va;
+
 	va_start(va, fmt);
 	fprintf(stderr, "%s:%d:%d: error: ", cl2llvm_file_name,
-			cl2llvm_yyget_lineno(), cl2llvm_get_col_num());
+		cl2llvm_yyget_lineno(), cl2llvm_get_col_num());
 	vfprintf(stderr, fmt, va);
 	fprintf(stderr, "\n");
 	fflush(NULL);
@@ -80,6 +84,8 @@ void cl2llvm_yyerror_fmt(char *fmt, ...)
 
 void cl2llvm_init(void)
 {
+	/* Initialize built in function table */
+	cl2llvm_built_in_func_table = built_in_func_table_create();
 }
 
 
@@ -88,48 +94,22 @@ void cl2llvm_init_global_vars(void)
 {
 	LLVMLinkInJIT();
 	LLVMInitializeNativeTarget();
-	
+
 	cl2llvm_set_lineno(1);
 	cl2llvm_col_num = 0;
 	temp_var_count = 0;
 	block_count = 0;
-	
+
 	/* Initialize LLVM */
 	cl2llvm_builder = LLVMCreateBuilder();
 	cl2llvm_module = LLVMModuleCreateWithName("module");
 
-	/*Initialize preprocessor file list*/
+	/* Initialize preprocessor file list */
 	cl2llvm_preprcr_file_list = list_create();
 
 	/* Initialize global symbol table */
 	cl2llvm_symbol_table = hash_table_create(10, 1);
 
-	/*Declare get_global_id*/
-	
-	/*Arguments*/
-	LLVMTypeRef args_get_global_id_array[] = {LLVMInt32Type()};
-	struct list_t *args_get_global_id = list_create();
-	struct cl2llvm_decl_list_t *ggi_dimindex_decl = cl2llvm_decl_list_create();
-	ggi_dimindex_decl->type_spec = cl2llvm_type_create_w_init(LLVMInt32Type(), 0);
-	struct cl2llvm_arg_t *ggi_dimindex_arg1 = cl2llvm_arg_create(ggi_dimindex_decl, "dimindex");
-	list_add(args_get_global_id, ggi_dimindex_arg1);
-
-	/*Function*/
-	struct cl2llvm_function_t *cl2llvm_get_global_id = cl2llvm_function_create("get_global_id",
-		args_get_global_id);
-	
-	 cl2llvm_get_global_id->func_type = LLVMFunctionType( LLVMInt32Type(),
-	 	args_get_global_id_array, 1, 0);
- 	cl2llvm_get_global_id->func = LLVMAddFunction(cl2llvm_module, "get_global_id",
-		cl2llvm_get_global_id->func_type);
-	cl2llvm_get_global_id->sign = 1;
-	LLVMSetFunctionCallConv(cl2llvm_get_global_id->func, LLVMCCallConv);
-
-	/*Insert function in global symbol table*/
-	hash_table_insert(cl2llvm_symbol_table, "get_global_id", cl2llvm_get_global_id);
-
-	/*free pointers*/
-	cl2llvm_decl_list_struct_free(ggi_dimindex_decl);
 }
 
 
@@ -138,10 +118,10 @@ void cl2llvm_done(void)
 }
 
 void cl2llvm_erase_global_vars(void)
-{	
+{
 	char *file_name;
-	int index;
 	char *name;
+	int index;
 	struct cl2llvm_function_t *function;
 
 	/* Free symbol table */
@@ -191,6 +171,7 @@ void cl2llvm_compile(struct list_t *source_file_list, struct list_t *llvm_file_l
 		/* Verify module and optimize */
 		LLVMVerifyModule(cl2llvm_module, LLVMAbortProcessAction, &error);
 		LLVMPassManagerRef pm = LLVMCreatePassManager();
+
 		switch (opt_level)
 		{
 		case 1:
