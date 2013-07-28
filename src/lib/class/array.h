2ef8f09d1fa824a08c3615452189abf8507384e5
ubal <rafael.ubal.83@gmail.com>
2013-07-28 00:28:27 +0000

    * Internal iterator for class 'Array' for convenient use of 'ArrayForEach'. * Iterations in 'List' and 'Array' based on calls to 'ListIsEnd' and 'ArrayIsEnd' to support NULL objects. * Calls to List-/Array- Dump/DeleteObjects now support NULL objects.

--- src/lib/class/array.h
+++ src/lib/class/array.h
@@ -27,14 +27,18 @@
  * Class 'Array'
  */
 
-#define ArrayForEach(_array, _index) \
-	for ((_index) = 0; (_index) < (_array)->count; (_index)++)
+#define ArrayForEach(_array, _object, _class) \
+	for ((_object) = as##_class(ArrayHead((_array))); \
+		!ArrayIsEnd((_array)); \
+		(_object) = as##_class(ArrayNext((_array))))
 
 typedef enum
 {
 	ArrayErrOK = 0,
 	ArrayErrBounds,
 	ArrayErrNotFound,
+	ArrayErrEmpty,
+	ArrayErrEnd
 } ArrayError;
 
 
@@ -52,6 +56,9 @@ CLASS_BEGIN(Array, Object)
 	/* Pointers in list */
 	int head;
 	int tail;
+
+	/* Internal iterator */
+	int index;
 
 	/* Elements */
 	Object **array;
@@ -74,16 +81,16 @@ void ArrayDumpWithDelim(Array *self, FILE *f, const char *first,
 		const char *middle, const char *last);
 
 /* Clear the content of the array. Freeing the elements that were contained
- * before the call is the responsibility of the caller. The call always succeeds
- * and updates the error code. */
+ * before the call is the responsibility of the caller. Set the error code to
+ * 'ArrayErrOK' and the internal iterator to 0. */
 void ArrayClear(Array *self);
 
 /* Free all objects added to the array with a call to 'delete()', and clear the
  * content of the list. This call always succeeds and updates the error code. */
 void ArrayDeleteObjects(Array *self);
 
-/* Add an object at the end of the array. This call always succeeds and updates
- * the error code. */
+/* Add an object at the end of the array and set the error code to 'ArrayErrOK'.
+ * The internal iterator is set to point to the last element of the array. */
 void ArrayAdd(Array *self, Object *object);
 
 /* Obtain the element at position 'index'. If 'index' is out of bounds, NULL is
@@ -91,11 +98,13 @@ void ArrayAdd(Array *self, Object *object);
 Object *ArrayGet(Array *self, int index);
 
 /* Set the element at position 'index' to 'object'. If 'index' is out of bounds,
- * the error code is set to 'ArrayErrBounds'. */
+ * the error code is set to 'ArrayErrBounds'. The internal iterator is moved to
+ * position 'index'. */
 void ArraySet(Array *self, int index, Object *object);
 
 /* Insert element 'object' at position 'index'. If 'index' is less than 0 or
- * greater than 'Array->count', the error code is set to 'ArrayErrBounds'. */
+ * greater than 'Array->count', the error code is set to 'ArrayErrBounds'. Set
+ * the internal iterator to 'index'. */
 void ArrayInsert(Array *self, int index, Object *object);
 
 /* Return the index of the first occurrence of 'object' in the array. If not
@@ -103,12 +112,41 @@ void ArrayInsert(Array *self, int index, Object *object);
 int ArrayFind(Array *self, Object *object);
 
 /* Remove the object at position 'index' and return it. If 'index' is out of
- * bounds, NULL is returned and the error code is set to 'ArrayErrBounds'. */
+ * bounds, NULL is returned and the error code is set to 'ArrayErrBounds'. The
+ * internal iterator is set to 'index' (which is past the end of the array if
+ * the removed element was the last). */
 Object *ArrayRemove(Array *self, int index);
 
 /* Sort the content of the list using virtual function 'Compare' of the
  * contained objects. */
 void ArraySort(Array *self);
+
+/* Set the internal iterator to the first element of the array and return the
+ * object at that position. If the array is empty, return NULL and set the error
+ * code to 'ArrayErrEmpty'. */
+Object *ArrayHead(Array *self);
+
+/* Set the internal iterator to the last element of the array and return the
+ * object at that position. If the array is empty, return NULL and set the error
+ * code to 'ArrayErrEmpty'. */
+Object *ArrayTail(Array *self);
+
+/* Move the internal iterator one position forward and return the object at that
+ * position. If the end of the array is reached for the first time, return NULL
+ * and set the error code to 'ArrayErrEnd'. If the iterator was already at the
+ * end of the array, return NULL as well and set the error code to
+ * 'ArrayErrBounds'. */
+Object *ArrayNext(Array *self);
+
+/* Move the internal iterator one position backward and return the object at
+ * that position. If the iterator was already at the beginning of the list, NULL
+ * is returned and the error code is set to 'ArrayErrBounds'. */
+Object *ArrayPrev(Array *self);
+
+/* Return true if the internal iterator points past the end of the array. If so,
+ * the error code is set to 'ArrayErrEnd'. */
+int ArrayIsEnd(Array *self);
+
 
 #endif
 
