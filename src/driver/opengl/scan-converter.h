37fea692cf026203ae32872c9d88986045eb3710
xiang <jackallen0714@gmail.com>
2013-08-20 20:05:01 +0000

    * Added support of triangle to scan converter

--- src/driver/opengl/scan-converter.h
+++ src/driver/opengl/scan-converter.h
@@ -20,15 +20,122 @@
 #ifndef DRIVER_OPENGL_SCAN_CONVERTER_H
 #define DRIVER_OPENGL_SCAN_CONVERTER_H
 
-struct list_t;
-struct opengl_si_pixel_info_t
+#define SWRAST_MAX_WIDTH 16384
+#define MAX_GLUINT	0xffffffff
+
+#define X_COMP 0
+#define Y_COMP 1
+#define Z_COMP 2
+#define W_COMP 3
+
+/* Convert float to int by rounding to nearest integer, away from zero */
+static inline int IROUND(float f)
 {
-	/* Barycentric coordinate */
-	float i; 
-	float j;
+	return (int) ((f >= 0.0F) ? (f + 0.5F) : (f - 0.5F));
+}
+
+#define SUB_PIXEL_BITS 4
+
+/* Fixed point arithmetic macros */
+#ifndef FIXED_FRAC_BITS
+#define FIXED_FRAC_BITS 11
+#endif
+
+#define FIXED_SHIFT     FIXED_FRAC_BITS
+#define FIXED_ONE       (1 << FIXED_SHIFT)
+#define FIXED_HALF      (1 << (FIXED_SHIFT-1))
+#define FIXED_FRAC_MASK (FIXED_ONE - 1)
+#define FIXED_INT_MASK  (~FIXED_FRAC_MASK)
+#define FIXED_EPSILON   1
+#define FIXED_SCALE     ((float) FIXED_ONE)
+#define FIXED_DBL_SCALE ((double) FIXED_ONE)
+#define FloatToFixed(X) (IROUND((X) * FIXED_SCALE))
+#define FixedToDouble(X) ((X) * (1.0 / FIXED_DBL_SCALE))
+#define IntToFixed(I)   ((I) << FIXED_SHIFT)
+#define FixedToInt(X)   ((X) >> FIXED_SHIFT)
+#define FixedToUns(X)   (((unsigned int)(X)) >> FIXED_SHIFT)
+#define FixedCeil(X)    (((X) + FIXED_ONE - FIXED_EPSILON) & FIXED_INT_MASK)
+#define FixedFloor(X)   ((X) & FIXED_INT_MASK)
+#define FixedToFloat(X) ((X) * (1.0F / FIXED_SCALE))
+#define PosFloatToFixed(X)      FloatToFixed(X)
+#define SignedFloatToFixed(X)   FloatToFixed(X)
+
+/* Forward declaration */
+struct list_t;
+
+struct opengl_sc_vertex_t
+{
+	float pos[4];
 };
 
-struct opengl_si_pixel_info_t *opengl_si_pixel_info_create();
-void opengl_si_pixel_info_free(struct opengl_si_pixel_info_t *pxl_info);
+struct opengl_sc_triangle_t
+{
+	struct opengl_sc_vertex_t *vtx0;
+	struct opengl_sc_vertex_t *vtx1;
+	struct opengl_sc_vertex_t *vtx2;
+};
+
+struct opengl_sc_edge_t
+{
+	struct opengl_sc_vertex_t *vtx0; 		/* Y(vtx0) < Y(vtx1) */
+	struct opengl_sc_vertex_t *vtx1;
+	float dx;				/* X(vtx1) - X(vtx0) */
+	float dy;				/* Y(vtx1) - Y(vtx0) */
+	float dxdy;				/* dx/dy */
+	int fdxdy;				/* dx/dy in fixed-point */
+	float adjy;				/* adjust from v[0]->fy to fsy, scaled */
+	int fsx;				/* first sample point x coord */
+	int fsy;				/* first sample point y coord */
+	int fx0;				/* fixed pt X of lower endpoint */
+	int lines;				/* number of lines to be sampled on this edge */	
+};
+
+struct opengl_sc_pixel_info_t
+{
+	/* Window coordinates of a pixel */
+	int wndw_i;
+	int wndw_j;
+	char wndw_init;
+
+	/* Barycentric coordinates to be load to VGPRs */
+	float brctrc_i; 
+	float brctrc_j;
+};
+
+struct opengl_sc_span_array_t
+{
+	unsigned int  z[SWRAST_MAX_WIDTH];  /**< fragment Z coords */	
+};
+
+struct opengl_sc_span_t
+{
+	/* Coord of first fragment in horizontal span/run */
+	int x;
+	int y;
+	int z;
+
+	int zStep;
+
+	/* Number of fragments in the span */
+	unsigned int end;
+
+	struct opengl_sc_span_array_t *array;
+
+};
+
+struct opengl_sc_vertex_t *opengl_sc_vertex_create();
+void opengl_sc_vertex_free(struct opengl_sc_vertex_t *vtx);
+
+struct opengl_sc_edge_t *opengl_sc_edge_create(struct opengl_sc_vertex_t *vtx0, struct opengl_sc_vertex_t *vtx1);
+void opengl_sc_edge_free(struct opengl_sc_edge_t *edge);
+
+struct opengl_sc_span_t *opengl_sc_span_create();
+void opengl_sc_span_free(struct opengl_sc_span_t *spn);
+void opengl_sc_span_interpolate_z(struct opengl_sc_span_t *spn);
+
+struct opengl_sc_pixel_info_t *opengl_sc_pixel_info_create();
+void opengl_sc_pixel_info_free(struct opengl_sc_pixel_info_t *pxl_info);
+
+struct list_t *opengl_sc_triangle(struct opengl_sc_triangle_t *triangle);
 
 #endif
