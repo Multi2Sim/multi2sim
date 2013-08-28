a72bdbd29370f99ea188218a056b4706d8021f7e
chris.barton <chris.barton@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-08-28 18:27:30 +0000

    Fixed bugs related to return commands
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@2138 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- m2c/cl2llvm/built-in-funcs.c
+++ m2c/cl2llvm/built-in-funcs.c
@@ -30,7 +30,7 @@
 #include "cl2llvm.h"
 #include "format.h"
 
-#define BUILT_IN_FUNC_COUNT 10
+#define BUILT_IN_FUNC_COUNT 42
 
 extern LLVMModuleRef cl2llvm_module;
 extern struct hash_table_t *cl2llvm_symbol_table;
@@ -45,7 +45,7 @@ struct hash_table_t *built_in_func_table_create(void)
 	int i;
 	struct cl2llvm_built_in_func_info_t *built_in_func_info;
 	
-	char built_in_func_info_list[BUILT_IN_FUNC_COUNT][3][400] = {
+	char built_in_func_info_list[BUILT_IN_FUNC_COUNT][3][1400] = {
 		{"get_work_dim", "1", "void u32 get_work_dim"},
 		{"get_global_id", "1", "u32 u32 get_global_id"},
 		{"get_global_size", "1", "u32 u32 get_global_size"},
@@ -55,7 +55,39 @@ struct hash_table_t *built_in_func_table_create(void)
 		{"get_group_id", "1", "u32 u32 get_group_id"},
 		{"get_global_offset", "1", "u32 u32 get_global_offset"},
 		{"barrier", "1", "u32 void barrier"},
-		{"atan2", "2", "f32 f32 f32 __atan2_f32 f32v2 f32v2 f32v2 __atan2_2f32 f32v3 f32v3 f32v3 __atan2_3f32 f32v4 f32v4 f32v4 __atan2_4f32 f32v8 f32v8 f32v8 __atan2_8f32 f32v16 f32v16 f32v16 __atan2_16f32 f64 f64 f64 __atan2_f64 f64v2 f64v2 f62v2 __atan2_2f64 f64v3 f64v3 f64v3 __atan2_3f64 f64v4 f64v4 f64v4 __atan2_4f64 f64v8 f64v8 f64v8 __atan2_8f64 f64v16 f64v16 f64v16 __atan2_16f64"}
+		{"acos", "1", "f32 f32 __acos_f32 f32v2 f32v2 __acos_2f32 f32v3 f32v3 __acos_3f32 f32v4 f32v4 __acos_4f32 f32v8 f32v8 __acos_8f32 f32v16 f32v16 __acos_16f32 f64 f64 __acos_f64 f64v2 f64v2 __acos_2f64 f64v3 f64v3 __acos_3f64 f64v4 f64v4 __acos_4f64 f64v8 f64v8 __acos_8f64 f64v16 f64v16 __acos_16f64"},
+		{"acosh", "1", "f32 f32 __acosh_f32 f32v2 f32v2 __acosh_2f32 f32v3 f32v3 __acosh_3f32 f32v4 f32v4 __acosh_4f32 f32v8 f32v8 __acosh_8f32 f32v16 f32v16 __acosh_16f32 f64 f64 __acosh_f64 f64v2 f64v2 __acosh_2f64 f64v3 f64v3 __acosh_3f64 f64v4 f64v4 __acosh_4f64 f64v8 f64v8 __acosh_8f64 f64v16 f64v16 __acosh_16f64"},
+		{"acospi", "1", "f32 f32 __acospi_f32 f32v2 f32v2 __acospi_2f32 f32v3 f32v3 __acospi_3f32 f32v4 f32v4 __acospi_4f32 f32v8 f32v8 __acospi_8f32 f32v16 f32v16 __acospi_16f32 f64 f64 __acospi_f64 f64v2 f64v2 __acospi_2f64 f64v3 f64v3 __acospi_3f64 f64v4 f64v4 __acospi_4f64 f64v8 f64v8 __acospi_8f64 f64v16 f64v16 __acospi_16f64"},
+		{"asin", "1", "f32 f32 __asin_f32 f32v2 f32v2 __asin_2f32 f32v3 f32v3 __asin_3f32 f32v4 f32v4 __asin_4f32 f32v8 f32v8 __asin_8f32 f32v16 f32v16 __asin_16f32 f64 f64 __asin_f64 f64v2 f64v2 __asin_2f64 f64v3 f64v3 __asin_3f64 f64v4 f64v4 __asin_4f64 f64v8 f64v8 __asin_8f64 f64v16 f64v16 __asin_16f64"},
+		{"asinh", "1", "f32 f32 __asinh_f32 f32v2 f32v2 __asinh_2f32 f32v3 f32v3 __asinh_3f32 f32v4 f32v4 __asinh_4f32 f32v8 f32v8 __asinh_8f32 f32v16 f32v16 __asinh_16f32 f64 f64 __asinh_f64 f64v2 f64v2 __asinh_2f64 f64v3 f64v3 __asinh_3f64 f64v4 f64v4 __asinh_4f64 f64v8 f64v8 __asinh_8f64 f64v16 f64v16 __asinh_16f64"},
+		{"asinpi", "1", "f32 f32 __asinpi_f32 f32v2 f32v2 __asinpi_2f32 f32v3 f32v3 __asinpi_3f32 f32v4 f32v4 __asinpi_4f32 f32v8 f32v8 __asinpi_8f32 f32v16 f32v16 __asinpi_16f32 f64 f64 __asinpi_f64 f64v2 f64v2 __asinpi_2f64 f64v3 f64v3 __asinpi_3f64 f64v4 f64v4 __asinpi_4f64 f64v8 f64v8 __asinpi_8f64 f64v16 f64v16 __asinpi_16f64"},
+		{"atan", "1", "f32 f32 __atan_f32 f32v2 f32v2 __atan_2f32 f32v3 f32v3 __atan_3f32 f32v4 f32v4 __atan_4f32 f32v8 f32v8 __atan_8f32 f32v16 f32v16 __atan_16f32 f64 f64 __atan_f64 f64v2 f64v2 __atan_2f64 f64v3 f64v3 __atan_3f64 f64v4 f64v4 __atan_4f64 f64v8 f64v8 __atan_8f64 f64v16 f64v16 __atan_16f64"},
+		{"atan2", "2", "f32 f32 f32 __atan2_f32 f32v2 f32v2 f32v2 __atan2_2f32 f32v3 f32v3 f32v3 __atan2_3f32 f32v4 f32v4 f32v4 __atan2_4f32 f32v8 f32v8 f32v8 __atan2_8f32 f32v16 f32v16 f32v16 __atan2_16f32 f64 f64 f64 __atan2_f64 f64v2 f64v2 f62v2 __atan2_2f64 f64v3 f64v3 f64v3 __atan2_3f64 f64v4 f64v4 f64v4 __atan2_4f64 f64v8 f64v8 f64v8 __atan2_8f64 f64v16 f64v16 f64v16 __atan2_16f64"},
+		{"atanh", "1", "f32 f32 __atanh_f32 f32v2 f32v2 __atanh_2f32 f32v3 f32v3 __atanh_3f32 f32v4 f32v4 __atanh_4f32 f32v8 f32v8 __atanh_8f32 f32v16 f32v16 __atanh_16f32 f64 f64 __atanh_f64 f64v2 f64v2 __atanh_2f64 f64v3 f64v3 __atanh_3f64 f64v4 f64v4 __atanh_4f64 f64v8 f64v8 __atanh_8f64 f64v16 f64v16 __atanh_16f64"},
+		{"atanpi", "1", "f32 f32 __atanpi_f32 f32v2 f32v2 __atanpi_2f32 f32v3 f32v3 __atanpi_3f32 f32v4 f32v4 __atanpi_4f32 f32v8 f32v8 __atanpi_8f32 f32v16 f32v16 __atanpi_16f32 f64 f64 __atanpi_f64 f64v2 f64v2 __atanpi_2f64 f64v3 f64v3 __atanpi_3f64 f64v4 f64v4 __atanpi_4f64 f64v8 f64v8 __atanpi_8f64 f64v16 f64v16 __atanpi_16f64"},
+		{"atan2pi", "2", "f32 f32 f32 __atan2pi_f32 f32v2 f32v2 f32v2 __atan2pi_2f32 f32v3 f32v3 f32v3 __atan2pi_3f32 f32v4 f32v4 f32v4 __atan2pi_4f32 f32v8 f32v8 f32v8 __atan2pi_8f32 f32v16 f32v16 f32v16 __atan2pi_16f32 f64 f64 f64 __atan2pi_f64 f64v2 f64v2 f62v2 __atan2pi_2f64 f64v3 f64v3 f64v3 __atan2pi_3f64 f64v4 f64v4 f64v4 __atan2pi_4f64 f64v8 f64v8 f64v8 __atan2pi_8f64 f64v16 f64v16 f64v16 __atan2pi_16f64"},
+		{"chrt", "1", "f32 f32 __chrt_f32 f32v2 f32v2 __chrt_2f32 f32v3 f32v3 __chrt_3f32 f32v4 f32v4 __chrt_4f32 f32v8 f32v8 __chrt_8f32 f32v16 f32v16 __chrt_16f32 f64 f64 __chrt_f64 f64v2 f64v2 __chrt_2f64 f64v3 f64v3 __chrt_3f64 f64v4 f64v4 __chrt_4f64 f64v8 f64v8 __chrt_8f64 f64v16 f64v16 __chrt_16f64"},
+		{"ceil", "1", "f32 f32 __ceil_f32 f32v2 f32v2 __ceil_2f32 f32v3 f32v3 __ceil_3f32 f32v4 f32v4 __ceil_4f32 f32v8 f32v8 __ceil_8f32 f32v16 f32v16 __ceil_16f32 f64 f64 __ceil_f64 f64v2 f64v2 __ceil_2f64 f64v3 f64v3 __ceil_3f64 f64v4 f64v4 __ceil_4f64 f64v8 f64v8 __ceil_8f64 f64v16 f64v16 __ceil_16f64"},
+		{"ceil", "1", "f32 f32 __ceil_f32 f32v2 f32v2 __ceil_2f32 f32v3 f32v3 __ceil_3f32 f32v4 f32v4 __ceil_4f32 f32v8 f32v8 __ceil_8f32 f32v16 f32v16 __ceil_16f32 f64 f64 __ceil_f64 f64v2 f64v2 __ceil_2f64 f64v3 f64v3 __ceil_3f64 f64v4 f64v4 __ceil_4f64 f64v8 f64v8 __ceil_8f64 f64v16 f64v16 __ceil_16f64"},
+		{"ceil", "1", "f32 f32 __ceil_f32 f32v2 f32v2 __ceil_2f32 f32v3 f32v3 __ceil_3f32 f32v4 f32v4 __ceil_4f32 f32v8 f32v8 __ceil_8f32 f32v16 f32v16 __ceil_16f32 f64 f64 __ceil_f64 f64v2 f64v2 __ceil_2f64 f64v3 f64v3 __ceil_3f64 f64v4 f64v4 __ceil_4f64 f64v8 f64v8 __ceil_8f64 f64v16 f64v16 __ceil_16f64"},
+		{"copysign", "2", "f32 f32 f32 __copysign_f32 f32v2 f32v2 f32v2 __copysign_2f32 f32v3 f32v3 f32v3 __copysign_3f32 f32v4 f32v4 f32v4 __copysign_4f32 f32v8 f32v8 f32v8 __copysign_8f32 f32v16 f32v16 f32v16 __copysign_16f32 f64 f64 f64 __copysign_f64 f64v2 f64v2 f62v2 __copysign_2f64 f64v3 f64v3 f64v3 __copysign_3f64 f64v4 f64v4 f64v4 __copysign_4f64 f64v8 f64v8 f64v8 __copysign_8f64 f64v16 f64v16 f64v16 __copysign_16f64"},
+		{"cos", "1", "f32 f32 __cos_f32 f32v2 f32v2 __cos_2f32 f32v3 f32v3 __cos_3f32 f32v4 f32v4 __cos_4f32 f32v8 f32v8 __cos_8f32 f32v16 f32v16 __cos_16f32 f64 f64 __cos_f64 f64v2 f64v2 __cos_2f64 f64v3 f64v3 __cos_3f64 f64v4 f64v4 __cos_4f64 f64v8 f64v8 __cos_8f64 f64v16 f64v16 __cos_16f64"},
+		{"cosh", "1", "f32 f32 __cosh_f32 f32v2 f32v2 __cosh_2f32 f32v3 f32v3 __cosh_3f32 f32v4 f32v4 __cosh_4f32 f32v8 f32v8 __cosh_8f32 f32v16 f32v16 __cosh_16f32 f64 f64 __cosh_f64 f64v2 f64v2 __cosh_2f64 f64v3 f64v3 __cosh_3f64 f64v4 f64v4 __cosh_4f64 f64v8 f64v8 __cosh_8f64 f64v16 f64v16 __cosh_16f64"},
+		{"cospi", "1", "f32 f32 __cospi_f32 f32v2 f32v2 __cospi_2f32 f32v3 f32v3 __cospi_3f32 f32v4 f32v4 __cospi_4f32 f32v8 f32v8 __cospi_8f32 f32v16 f32v16 __cospi_16f32 f64 f64 __cospi_f64 f64v2 f64v2 __cospi_2f64 f64v3 f64v3 __cospi_3f64 f64v4 f64v4 __cospi_4f64 f64v8 f64v8 __cospi_8f64 f64v16 f64v16 __cospi_16f64"},
+		{"erfc", "1", "f32 f32 __erfc_f32 f32v2 f32v2 __erfc_2f32 f32v3 f32v3 __erfc_3f32 f32v4 f32v4 __erfc_4f32 f32v8 f32v8 __erfc_8f32 f32v16 f32v16 __erfc_16f32 f64 f64 __erfc_f64 f64v2 f64v2 __erfc_2f64 f64v3 f64v3 __erfc_3f64 f64v4 f64v4 __erfc_4f64 f64v8 f64v8 __erfc_8f64 f64v16 f64v16 __erfc_16f64"},
+		{"erf", "1", "f32 f32 __erf_f32 f32v2 f32v2 __erf_2f32 f32v3 f32v3 __erf_3f32 f32v4 f32v4 __erf_4f32 f32v8 f32v8 __erf_8f32 f32v16 f32v16 __erf_16f32 f64 f64 __erf_f64 f64v2 f64v2 __erf_2f64 f64v3 f64v3 __erf_3f64 f64v4 f64v4 __erf_4f64 f64v8 f64v8 __erf_8f64 f64v16 f64v16 __erf_16f64"},
+		{"exp", "1", "f32 f32 __exp_f32 f32v2 f32v2 __exp_2f32 f32v3 f32v3 __exp_3f32 f32v4 f32v4 __exp_4f32 f32v8 f32v8 __exp_8f32 f32v16 f32v16 __exp_16f32 f64 f64 __exp_f64 f64v2 f64v2 __exp_2f64 f64v3 f64v3 __exp_3f64 f64v4 f64v4 __exp_4f64 f64v8 f64v8 __exp_8f64 f64v16 f64v16 __exp_16f64"},
+		{"exp2", "1", "f32 f32 __exp2_f32 f32v2 f32v2 __exp2_2f32 f32v3 f32v3 __exp2_3f32 f32v4 f32v4 __exp2_4f32 f32v8 f32v8 __exp2_8f32 f32v16 f32v16 __exp2_16f32 f64 f64 __exp2_f64 f64v2 f64v2 __exp2_2f64 f64v3 f64v3 __exp2_3f64 f64v4 f64v4 __exp2_4f64 f64v8 f64v8 __exp2_8f64 f64v16 f64v16 __exp2_16f64"},
+		{"exp10", "1", "f32 f32 __exp10_f32 f32v2 f32v2 __exp10_2f32 f32v3 f32v3 __exp10_3f32 f32v4 f32v4 __exp10_4f32 f32v8 f32v8 __exp10_8f32 f32v16 f32v16 __exp10_16f32 f64 f64 __exp10_f64 f64v2 f64v2 __exp10_2f64 f64v3 f64v3 __exp10_3f64 f64v4 f64v4 __exp10_4f64 f64v8 f64v8 __exp10_8f64 f64v16 f64v16 __exp10_16f64"},
+		{"expm1", "1", "f32 f32 __expm1_f32 f32v2 f32v2 __expm1_2f32 f32v3 f32v3 __expm1_3f32 f32v4 f32v4 __expm1_4f32 f32v8 f32v8 __expm1_8f32 f32v16 f32v16 __expm1_16f32 f64 f64 __expm1_f64 f64v2 f64v2 __expm1_2f64 f64v3 f64v3 __expm1_3f64 f64v4 f64v4 __expm1_4f64 f64v8 f64v8 __expm1_8f64 f64v16 f64v16 __expm1_16f64"},
+		{"fabs", "1", "f32 f32 __fabs_f32 f32v2 f32v2 __fabs_2f32 f32v3 f32v3 __fabs_3f32 f32v4 f32v4 __fabs_4f32 f32v8 f32v8 __fabs_8f32 f32v16 f32v16 __fabs_16f32 f64 f64 __fabs_f64 f64v2 f64v2 __fabs_2f64 f64v3 f64v3 __fabs_3f64 f64v4 f64v4 __fabs_4f64 f64v8 f64v8 __fabs_8f64 f64v16 f64v16 __fabs_16f64"},
+		{"fdim", "2", "f32 f32 f32 __fdim_f32 f32v2 f32v2 f32v2 __fdim_2f32 f32v3 f32v3 f32v3 __fdim_3f32 f32v4 f32v4 f32v4 __fdim_4f32 f32v8 f32v8 f32v8 __fdim_8f32 f32v16 f32v16 f32v16 __fdim_16f32 f64 f64 f64 __fdim_f64 f64v2 f64v2 f62v2 __fdim_2f64 f64v3 f64v3 f64v3 __fdim_3f64 f64v4 f64v4 f64v4 __fdim_4f64 f64v8 f64v8 f64v8 __fdim_8f64 f64v16 f64v16 f64v16 __fdim_16f64"},
+		{"floor", "1", "f32 f32 __floor_f32 f32v2 f32v2 __floor_2f32 f32v3 f32v3 __floor_3f32 f32v4 f32v4 __floor_4f32 f32v8 f32v8 __floor_8f32 f32v16 f32v16 __floor_16f32 f64 f64 __floor_f64 f64v2 f64v2 __floor_2f64 f64v3 f64v3 __floor_3f64 f64v4 f64v4 __floor_4f64 f64v8 f64v8 __floor_8f64 f64v16 f64v16 __floor_16f64"},
+		{"fma", "2", "f32 f32 f32 f32 __fma_f32 f32v2 f32v2 f32v2 f32v2 __fma_2f32 f32v3 f32v3 f32v3 f32v3 __fma_3f32 f32v4 f32v4 f32v4 f32v4 __fma_4f32 f32v8 f32v8 f32v8 f32v8 __fma_8f32 f32v16 f32v16 f32v16 f32v16 __fma_16f32 f64 f64 f64 f64 __fma_f64 f64v2 f64v2 f64v2 f64v2 __fma_2f64 f64v3 f64v3 f64v3 f64v3 __fma_3f64 f64v4 f64v4 f64v4 f64v4 __fma_4f64 f64v8 f64v8 f64v8 f64v8 __fma_8f64 f64v16 f64v16 f64v16 f64v16 __fma_16f64"},
+		{"fmax", "2", "f32 f32 f32 __fmax_f32 f32v2 f32v2 f32v2 __fmax_2f32 f32v3 f32v3 f32v3 __fmax_3f32 f32v4 f32v4 f32v4 __fmax_4f32 f32v8 f32v8 f32v8 __fmax_8f32 f32v16 f32v16 f32v16 __fmax_16f32 f64 f64 f64 __fmax_f64 f64v2 f64v2 f62v2 __fmax_2f64 f64v3 f64v3 f64v3 __fmax_3f64 f64v4 f64v4 f64v4 __fmax_4f64 f64v8 f64v8 f64v8 __fmax_8f64 f64v16 f64v16 f64v16 __fmax_16f64"},
+		{"fmin", "2", "f32 f32 f32 __fmin_f32 f32v2 f32v2 f32v2 __fmin_2f32 f32v3 f32v3 f32v3 __fmin_3f32 f32v4 f32v4 f32v4 __fmin_4f32 f32v8 f32v8 f32v8 __fmin_8f32 f32v16 f32v16 f32v16 __fmin_16f32 f64 f64 f64 __fmin_f64 f64v2 f64v2 f62v2 __fmin_2f64 f64v3 f64v3 f64v3 __fmin_3f64 f64v4 f64v4 f64v4 __fmin_4f64 f64v8 f64v8 f64v8 __fmin_8f64 f64v16 f64v16 f64v16 __fmin_16f64 f32v2 f32 f32v2 __fmin_2f32 f32v3 f32 f32v3 __fmin_3f32 f32v4 f32 f32v4 __fmin_4f32 f32v8 f32 f32v8 __fmin_8f32 f32v16 f32 f32v16 __fmin_16f32 f64v2 f64 f62v2 __fmin_2f64 f64v3 f64 f64v3 __fmin_3f64 f64v4 f64 f64v4 __fmin_4f64 f64v8 f64 f64v8 __fmin_8f64 f64v16 f64 f64v16 __fmin_16f64"},
+		{"fmod", "2", "f32 f32 f32 __fmod_f32 f32v2 f32v2 f32v2 __fmod_2f32 f32v3 f32v3 f32v3 __fmod_3f32 f32v4 f32v4 f32v4 __fmod_4f32 f32v8 f32v8 f32v8 __fmod_8f32 f32v16 f32v16 f32v16 __fmod_16f32 f64 f64 f64 __fmod_f64 f64v2 f64v2 f62v2 __fmod_2f64 f64v3 f64v3 f64v3 __fmod_3f64 f64v4 f64v4 f64v4 __fmod_4f64 f64v8 f64v8 f64v8 __fmod_8f64 f64v16 f64v16 f64v16 __fmod_16f64"},
+		{"fract", "2", "f32 f32g* f32 __fract_f32 f32v2 f32v2g* f32v2 __fract_2f32 f32v3 f32v3g* f32v3 __fract_3f32 f32v4 f32v4g* f32v4 __fract_4f32 f32v8 f32v8g* f32v8 __fract_8f32 f32v16 f32v16g* f32v16 __fract_16f32 f64 f64g* f64 __fract_f64 f64v2 f64v2g* f64v2 __fract_2f64 f64v3 f64v3g* f64v3 __fract_3f64 f64v4 f64v4g* f64v4 __fract_4f64 f64v8 f64v8g* f64v8 __fract_8f64 f64v16 f64v16g* f64v16 __fract_16f64 f32 f32l* f32 __fract_f32 f32v2 f32v2l* f32v2 __fract_2f32 f32v3 f32v3l* f32v3 __fract_3f32 f32v4 f32v4l* f32v4 __fract_4f32 f32v8 f32v8l* f32v8 __fract_8f32 f32v16 f32v16l* f32v16 __fract_16f32 f64 f64l* f64 __fract_f64 f64v2 f64v2l* f64v2 __fract_2f64 f64v3 f64v3l* f64v3 __fract_3f64 f64v4 f64v4l* f64v4 __fract_4f64 f64v8 f64v8l* f64v8 __fract_8f64 f64v16 f64v16l* f64v16 __fract_16f64f32 f32* f32 __fract_f32 f32v2 f32v2* f32v2 __fract_2f32 f32v3 f32v3* f32v3 __fract_3f32 f32v4 f32v4* f32v4 __fract_4f32 f32v8 f32v8* f32v8 __fract_8f32 f32v16 f32v16* f32v16 __fract_16f32 f64 f64* f64 __fract_f64 f64v2 f64v2* f64v2 __fract_2f64 f64v3 f64v3* f64v3 __fract_3f64 f64v4 f64v4* f64v4 __fract_4f64 f64v8 f64v8* f64v8 __fract_8f64 f64v16 f64v16* f64v16 __fract_16f64"},
 	};
 
 
@@ -501,7 +533,9 @@ char *cl2llvm_error_built_in_func_arg_mismatch(struct list_t *param_list,
 				index1++;
 				index2++;
 			}
-			cl2llvm_type_to_string(string_to_type(arg_type_string), format_arg_str);
+			arg_type_string[++index2] = '\00';
+
+cl2llvm_type_to_string(string_to_type(arg_type_string), format_arg_str);
 
 			strcpy(error_message_cpy, error_message);
 			snprintf(error_message, 1000 * sizeof(char),
