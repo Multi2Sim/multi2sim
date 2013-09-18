676a417ed987c62fe1b99d7562114c3978d72e5e
steven.gurfinkel <steven.gurfinkel@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-09-18 01:52:13 +0000

    OpenCL Runtime log enhanced
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@2182 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- runtime/opencl/partition-issue-log.h
+++ runtime/opencl/partition-issue-log.h
@@ -8,29 +8,37 @@ extern "C" {
 #include <stdio.h>
 
 #define PARTITION_ISSUE_MAX_DIMS 3
+#define PARTITION_NUM_DEVICES 2
 
 struct partition_issue_log_entry_t
 {
-	int device;
-	long long start;
+	unsigned int executed;
 	unsigned int group_offset[PARTITION_ISSUE_MAX_DIMS];
 	unsigned int group_size[PARTITION_ISSUE_MAX_DIMS];
-	int kernel_number;
 };
 
 struct partition_issue_kernel_entry_t
 {
 	unsigned int num_dims;
 	unsigned int groups[PARTITION_ISSUE_MAX_DIMS];
+	struct partition_issue_log_entry_t *first_part;
+};
+
+struct partition_issue_device_entry_t
+{
+	unsigned int num_invocations;
+	unsigned int num_groups;
 };
 
 struct partition_issue_log_t
 {
-//	struct partition_issue_log_entry_t *parts;
-//	struct partition_issue_kernel_entry_t *kernels;
+	struct partition_issue_log_entry_t *parts;
+	struct partition_issue_kernel_entry_t *kernels;
+	struct partition_issue_device_entry_t devices[PARTITION_NUM_DEVICES];
+	
 	int num_entries;
 	int num_kernels;
-//	volatile int lock;
+	volatile int lock;
 };
 
 void partition_issue_log_init(struct partition_issue_log_t *log);
@@ -47,6 +55,8 @@ void partition_issue_log_record(
 	long long start, 
 	const unsigned int *group_offset,
 	const unsigned int *group_size);
+	
+void partition_issue_log_done_kernel(struct partition_issue_log_t *log);
 
 void partition_issue_log_write(struct partition_issue_log_t *log, FILE *file);
 
