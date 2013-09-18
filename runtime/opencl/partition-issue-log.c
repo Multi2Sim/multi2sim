ec9f4bc15f2f1daafb5df640abe35af2106a255c
steven.gurfinkel <steven.gurfinkel@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-09-18 01:52:13 +0000

    OpenCL Runtime log enhanced

--- runtime/opencl/partition-issue-log.c
+++ runtime/opencl/partition-issue-log.c
@@ -7,17 +7,17 @@
 
 void partition_issue_log_init(struct partition_issue_log_t *log)
 {
-/*	log->parts = (struct partition_issue_log_entry_t *)calloc(MAX_ENTRIES, sizeof (struct partition_issue_log_entry_t));
+	log->parts = (struct partition_issue_log_entry_t *)calloc(MAX_ENTRIES + 1, sizeof (struct partition_issue_log_entry_t));
 	log->kernels = (struct partition_issue_kernel_entry_t *)calloc(MAX_ENTRIES, sizeof (struct partition_issue_kernel_entry_t));
-	log->lock = 0; */
+	log->lock = 0;
 	log->num_entries = 0;
 	log->num_kernels = 0;
 }
 
 void partition_issue_log_destroy(struct partition_issue_log_t *log)
 {
-/*	free(log->parts);
-	free(log->kernels); */
+	free(log->parts);
+	free(log->kernels);
 }
 
 int partition_issue_still_room(struct partition_issue_log_t *log)
@@ -30,15 +30,15 @@ void partition_issue_log_kernel(
 	unsigned int num_dims, 
 	const unsigned int *groups)
 {
-/*	while (__sync_lock_test_and_set(&log->lock, 1));
+	while (__sync_lock_test_and_set(&log->lock, 1));
 	if (partition_issue_still_room(log))
 	{
 		log->kernels[log->num_kernels].num_dims = num_dims;
+		log->kernels[log->num_kernels].first_part = log->parts + log->num_entries;
 		memcpy(log->kernels[log->num_kernels].groups, groups, sizeof (unsigned int) * num_dims);
 		log->num_kernels++;
 	}
-	__sync_lock_release(&log->lock);*/
-	__sync_fetch_and_add(&log->num_kernels, 1);
+	__sync_lock_release(&log->lock);
 }
 
 void partition_issue_log_record(
@@ -48,23 +48,33 @@ void partition_issue_log_record(
 	const unsigned int *group_offset,
 	const unsigned int *group_size)
 {
-/*	while (__sync_lock_test_and_set(&log->lock, 1));
+
+	while (__sync_lock_test_and_set(&log->lock, 1));
 	if (partition_issue_still_room(log))
 	{
 		struct partition_issue_log_entry_t *entry = log->parts + log->num_entries;
 		unsigned int num_dims;
+		unsigned int total_groups = 1;
+		unsigned int i;
 
-		entry->device = device;
-		entry->start = start;
-		entry->kernel_number = log->num_kernels - 1;
-
-		num_dims = log->kernels[entry->kernel_number].num_dims;
+		entry->executed = 1;
+		num_dims = log->kernels[log->num_kernels - 1].num_dims;
+		
+		for (i = 0; i < num_dims; i++)
+			total_groups *= group_size[i];
+		log->devices[device].num_invocations++;
+		log->devices[device].num_groups += total_groups;
+		
 		memcpy(entry->group_offset, group_offset, sizeof (unsigned int) * num_dims);
 		memcpy(entry->group_size, group_size, sizeof (unsigned int) * num_dims);
-		log->num_entries++;
 	}
-	__sync_lock_release(&log->lock);*/
-	__sync_fetch_and_add(&log->num_entries, 1);
+	__sync_lock_release(&log->lock);
+	__sync_fetch_and_add(&log->num_entries, 1);	
+}
+
+void partition_issue_log_done_kernel(struct partition_issue_log_t *log)
+{
+	__sync_fetch_and_add(&log->num_entries, 1);	
 }
 
 void partition_issue_log_print_vector(unsigned int dims, unsigned int *vec, FILE *f)
@@ -78,30 +88,15 @@ void partition_issue_log_print_vector(unsigned int dims, unsigned int *vec, FILE
 
 void partition_issue_log_write(struct partition_issue_log_t *log, FILE *file)
 {
-/*	unsigned int i;
-	struct partition_issue_log_entry_t *entry = log->parts; */
-	fprintf(file, "PARTITION_LOG kernels:%d partitions:%d\n", log->num_kernels, log->num_entries);
-/*	for (i = 0; i < log->num_kernels; i++)
+	int i;
+	fprintf(file, "PARTITION_LOG: kernels %d\n", log->num_kernels);
+	fprintf(file, "PARTITION_LOG: partitions %d\n", log->num_entries - log->num_kernels);
+	fprintf(file, "PARTITION_LOG: devices %d\n", PARTITION_NUM_DEVICES);
+	for (i = 0; i < PARTITION_NUM_DEVICES; i++)
 	{
-		struct partition_issue_kernel_entry_t *kernel = log->kernels + i;
-		fprintf(file, "Kernel %d ", i);
-		
-		partition_issue_log_print_vector(kernel->num_dims, kernel->groups, file);
-		fprintf(file, "\n");
-
-		long long start = entry->start;
-		while (entry - log->parts < log->num_entries && entry->kernel_number == i)
-		{
-			fprintf(file, "\tDevice %d Time: %10lld offset: ", entry->device, entry->start - start);
-			partition_issue_log_print_vector(kernel->num_dims, entry->group_offset, file);
-			fprintf(file, " size: ");
-			partition_issue_log_print_vector(kernel->num_dims, entry->group_size, file);
-			fprintf(file, "\n");
-			entry++;
-		}
-		
-		
-	} */
+		fprintf(file, "PARTITION_LOG: invocations%d %d\n", i, log->devices[i].num_invocations);
+		fprintf(file, "PARTITION_LOG: groups%d %d\n", i, log->devices[i].num_groups);
+	}
 }
 
 
@@ -137,63 +132,59 @@ void part_nd_address(int dim, unsigned int addr, const unsigned int *size, unsig
 	part_nd_address_rec(1, 0, dim, addr, size, pos);
 }
 
-int part_is_in_range(int dims, const unsigned int *point, const unsigned int *start, const unsigned int *size)
-{
-	int i;
-	for (i = 0; i < dims; i++)
-		if (point[i] < start[i] || point[i] >= start[i] + size[i])
-			return 0;
-	return 1;
-}
-
 void partition_issue_log_verify(struct partition_issue_log_t *log, FILE *show_errors)
 {
-/*	unsigned int i, j;
-	struct partition_issue_log_entry_t *entry = log->parts;
+	unsigned int i;
 	for (i = 0; i < log->num_kernels; i++)
 	{
 		struct partition_issue_kernel_entry_t *kernel = log->kernels + i;
-		unsigned int num_wg = 1;
+		unsigned int num_groups = 1;
+		unsigned int j;
 		char *wg;
-
+		int aborted = 0;
+		
 		for (j = 0; j < kernel->num_dims; j++)
-			num_wg *= kernel->groups[j];
+			num_groups *= kernel->groups[j];
+		
+		wg = (char *)calloc(1, num_groups);	/* allocate array for work-groups */
 
-		wg = (char *)calloc(1, num_wg);		
-
-		while (entry - log->parts < log->num_entries && entry->kernel_number == i)
+		struct partition_issue_log_entry_t *part = kernel->first_part;
+		
+		while (part->executed)
 		{
-			unsigned int *offset = entry->group_offset;
-			unsigned int *size = entry->group_size;
-
-			for (j = 0; j < num_wg; j++)
-			{
-				unsigned int cur[PARTITION_ISSUE_MAX_DIMS];
-				part_nd_address(kernel->num_dims, j, kernel->groups, cur);
+			unsigned int part_size = 1;
+			unsigned int cur_wg[PARTITION_ISSUE_MAX_DIMS];
+			
+			for (j = 0; j < kernel->num_dims; j++)
+				part_size *= part->group_size[j];
 				
-				if (part_is_in_range(kernel->num_dims, cur, offset, size))
-				{
-					if (wg[j] == 1)
-					{
-						fprintf(stderr, "Kernel %d is doing the same work twice\n", i);
-						abort();
-					}	
-					wg[j] = 1;
-				}
-			}
-			entry++;
-		}
-		for (j = 0; j < num_wg; j++)
-		{
-			if (wg[j] == 0)
+			/* go through each of these groups */
+			for (j = 0; j < part_size; j++)
 			{
-				fprintf(stderr, "Kernel number %d is incomplete. Missing %d\n", i, j);
-				abort();
+				unsigned int k;
+				unsigned int id;
+				part_nd_address(kernel->num_dims, j, part->group_size, cur_wg); /* get n-dimensional position of group within part */
+				for (k = 0; k < kernel->num_dims; k++)
+					cur_wg[k] += part->group_offset[k]; /* add the offset */
+					
+				id = part_linear_address(kernel->num_dims, cur_wg, kernel->groups); /* get linear position of group in NDRange */
+				wg[id]++;
 			}
+
+			part++;
 		}
 		
+		for (j = 0; j < num_groups; j++)
+			if (wg[j] != 1)
+			{
+				fprintf(stderr, "Kernel %d, work_group %d executed %d times\n", i, j, wg[j]);
+				aborted = 1;
+			}
+			
+		if (aborted)
+			abort();
+		
 		free(wg);
-	}*/
-	
+	}
 }
 
