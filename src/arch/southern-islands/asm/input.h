4138ea88c33c94326b0d9fe4cb179585aee92b48
xiang <jackallen0714@gmail.com>
2013-09-24 19:22:26 +0000

    * Updated OpenGL input

--- src/arch/southern-islands/asm/input.h
+++ src/arch/southern-islands/asm/input.h
@@ -17,41 +17,55 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef ARCH_SOUTHERN_ISLANDS_INPUT_H
-#define ARCH_SOUTHERN_ISLANDS_INPUT_H
+#ifndef ARCH_SOUTHERN_ISLANDS_ASM_INPUT_H
+#define ARCH_SOUTHERN_ISLANDS_ASM_INPUT_H
 
-enum si_input_data_type_t
-{
-	si_input_data_type_invalid = 0,
-	si_input_byte,
-	si_input_ubyte,
-	si_input_short,
-	si_input_ushort,
-	si_input_int,
-	si_input_uint,
-	si_input_hfloat,
-	si_input_float,
-	si_input_double,
-	si_input_fixed,
-	si_input_int_2_10_10_10_rev,
-	si_input_uint_2_10_10_10_rev
-};
+#include <lib/class/class.h>
 
-struct si_input_t
+
+/*
+ * Class SIInput
+ */
+
+typedef enum 
 {
+	SIInputInvalid = 0,
+	SIInputByte,
+	SIInputUbyte,
+	SIInputShort,
+	SIInputUshort,
+	SIInputInt,
+	SIInputUint,
+	SIInputHfloat,
+	SIInputFloat,
+	SIInputDouble,
+	SIInputFixed,
+	SIInputInt2101010Rev,
+	SIInputUint2101010Rev
+} SIInputDataType;
+
+CLASS_BEGIN(SIInput, Object)
+
 	unsigned int usage_index; /* The associated vertex attribute index */
 	int set;  /* Set to true when it is assigned */
 
-	enum si_input_data_type_t type;
+	SIInputDataType type;
 	int num_elems;
 	int size;
 	unsigned int device_ptr; 
-};
 
-struct si_input_t *si_input_create();
-void si_input_free(struct si_input_t *input);
-void si_input_set_usage_index(struct si_input_t *input, unsigned int usage_index);
-unsigned int si_input_get_type(unsigned int gl_data_type);
-int si_input_get_data_size(enum si_input_data_type_t data_type);
+CLASS_END(SIInput)
+
+void SIInputCreate(SIInput *self);
+void SIInputDestroy(SIInput *input);
+
+
+/*
+ * Public
+ */
+
+void SIInputSetUsageIndex(SIInput *input, unsigned int usage_index);
+unsigned int SIInputGetType(unsigned int gl_data_type);
+int SIInputGetDataSize(SIInputDataType data_type);
 
 #endif
