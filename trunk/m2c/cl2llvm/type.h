5dd48d185b98b8cbe68f3f64b11279d90ee52351
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-09-26 23:55:04 +0000

    * Starting to port cl2llvm front-end to C++ * cl2llvm::Type class

--- trunk/m2c/cl2llvm/type.h
+++ trunk/m2c/cl2llvm/type.h
@@ -17,8 +17,8 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef M2C_CL2LLVM_TYPE_H
-#define M2C_CL2LLVM_TYPE_H
+#ifndef M2C_CL2LLVM_TYPE_H_OLD
+#define M2C_CL2LLVM_TYPE_H_OLD
 
 #include <llvm-c/Core.h>
 #include <llvm-c/Analysis.h>
