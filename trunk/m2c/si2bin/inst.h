fdc5751efd228ffa11075d210b8d702a01aaa1a6
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-10-12 22:21:12 +0000

    Files Token.cc/Token.h

--- trunk/m2c/si2bin/inst.h
+++ trunk/m2c/si2bin/inst.h
@@ -17,8 +17,8 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef M2C_SI2BIN_INST_H
-#define M2C_SI2BIN_INST_H
+#ifndef M2C_SI2BIN_INST_H_OLD
+#define M2C_SI2BIN_INST_H_OLD
 
 #include <stdio.h>
 #include <arch/southern-islands/asm/Inst.h>
