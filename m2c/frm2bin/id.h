42bd8c64b7b2ae625132d1775ec911f341861442
chulian <chulian@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-10-24 01:47:56 +0000

    move from obj-C to C++, it's for id.c id.h this time

--- m2c/frm2bin/id.h
+++ m2c/frm2bin/id.h
@@ -17,8 +17,8 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef M2C_FRM2BIN_ID_H
-#define M2C_FRM2BIN_ID_H
+#ifndef M2C_FRM2BIN_ID_H_OLD
+#define M2C_FRM2BIN_ID_H_OLD
 
 struct frm_id_t
 {
