7d4d313fc6e6169d41af0a93175bd529f1b93eab
chulian <chulian@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-11-06 22:36:36 +0000

    add Context.h Context.cc to hold inst_info and inst_info_table
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@2356 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- m2c/frm2bin/Token.cc
+++ m2c/frm2bin/Token.cc
@@ -1,8 +1,20 @@
 /*
- * Token.cc
+ *  Multi2Sim
+ *  Copyright (C) 2012  Rafael Ubal (ubal@ece.neu.edu)
  *
- *  Created on: Oct 28, 2013
- *      Author: Chulian Zhang
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 
@@ -14,7 +26,8 @@
 
 using namespace misc;
 
-using namespace frm2bin;
+namespace frm2bin
+{
 
 StringMap TokenTypeMap =
 {
@@ -106,6 +119,7 @@ Token::Token(TokenType type)
 	this->type = type;
 }
 
+} /* namespace frm2bin */
 
 /*
  * C Wrapper
