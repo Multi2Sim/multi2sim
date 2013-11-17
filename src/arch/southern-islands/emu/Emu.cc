2026b4298335fe5b4b7276d1f5556ce88df35ebb
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2013-11-17 04:30:34 +0000

    Interface for SI - Emu.h
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@2377 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- src/arch/southern-islands/emu/Emu.cc
+++ src/arch/southern-islands/emu/Emu.cc
@@ -17,6 +17,8 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "Emu.h"
+
 #if 0
 #include <arch/southern-islands/asm/Asm.h>
 #include <arch/southern-islands/asm/opengl-bin-file.h>
