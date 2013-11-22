24173207268061b68ea602e676b0a83b59a90ea0
ubal <rafael.ubal.83@gmail.com>
2013-11-22 01:11:48 +0000

    * x86 disassembler ported to C++. * Invokable through option --cpp --x86-disasm <file>.

--- src/arch/x86/asm/asm.h
+++ src/arch/x86/asm/asm.h
@@ -17,8 +17,8 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef X86_ASM_H
-#define X86_ASM_H
+#ifndef ARCH_X86_ASM_ASM_OLD_H
+#define ARCH_X86_ASM_ASM_OLD_H
 
 #include <arch/common/asm.h>
 #include <lib/class/class.h>
