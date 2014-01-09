411988448d12d2ccf550ed7265dc84ea49ec2186
chen <chen@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2014-01-09 03:44:37 +0000

    * runtime updated to version 5.5

--- trunk/runtime/cuda/stream.c
+++ trunk/runtime/cuda/stream.c
@@ -17,6 +17,8 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <pthread.h>
+
 #include "../include/cuda.h"
 #include "list.h"
 #include "mhandle.h"
@@ -25,16 +27,76 @@
 
 struct list_t *stream_list;
 
+struct cuda_stream_command_t *cuda_stream_command_create(CUstream stream,
+		enum cuda_stream_command_type type, cuda_stream_command_func_t func)
+{
+	struct cuda_stream_command_t *command;
+
+	/* Allocate command */
+	command = (struct cuda_stream_command_t *)xcalloc(1,
+			sizeof(struct cuda_stream_command_t));
+
+	/* Initialize */
+	command->id = list_count(stream->command_list);
+	command->type = type;
+	command->func = func;
+	command->completed = 0;
+
+	list_enqueue(stream->command_list, command);
+
+	return command;
+}
+
+void cuda_stream_command_free(struct cuda_stream_command_t *command)
+{
+	free(command);
+}
+
+void cuda_stream_command_run(struct cuda_stream_command_t *command)
+{
+	command->func(command);
+	command->completed = 1;
+}
+
+static void *cuda_stream_thread_func(void *thread_data)
+{
+	CUstream stream = thread_data;
+	struct cuda_stream_command_t *command;
+
+	/* Execute commands sequentially */
+	while (1)
+	{
+		/* Get command */
+		command = cuda_stream_dequeue(stream);
+		if (! command)
+			break;
+
+		/* Run command */
+		cuda_stream_command_run(command);
+		cuda_stream_command_free(command);
+	}
+
+	pthread_exit(NULL);
+
+	return NULL;
+}
+
 /* Create a stream */
 CUstream cuda_stream_create(void)
 {
 	CUstream stream;
 
-	/* Create stream */
+	/* Allocate stream */
 	stream = (CUstream)xcalloc(1, sizeof(struct CUstream_st));
 
 	/* Initialize */
 	stream->id = list_count(stream_list);
+	stream->command_list = list_create();
+
+	/* Create thread associated with stream */
+	pthread_mutex_init(&stream->lock, NULL);
+	pthread_cond_init(&stream->cond, NULL);
+	pthread_create(&stream->thread, NULL, cuda_stream_thread_func, stream);
 
 	list_add(stream_list, stream);
 
@@ -44,8 +106,39 @@ CUstream cuda_stream_create(void)
 /* Free stream */
 void cuda_stream_free(CUstream stream)
 {
+	pthread_join(stream->thread, NULL);
+
+	pthread_mutex_destroy(&stream->lock);
+	pthread_cond_destroy(&stream->cond);
+	pthread_exit(NULL);
+
 	list_remove(stream_list, stream);
 
 	free(stream);
 }
 
+/* Enqueue a command */
+void cuda_stream_enqueue(CUstream stream, struct cuda_stream_command_t *command)
+{
+	pthread_mutex_lock(&stream->lock);
+
+	list_add(stream->command_list, command);
+
+	pthread_mutex_unlock(&stream->lock);
+}
+
+/* Dequeue a command */
+struct cuda_stream_command_t *cuda_stream_dequeue(CUstream stream)
+{
+	struct cuda_stream_command_t *command;
+
+	pthread_mutex_lock(&stream->lock);
+
+	while (! list_count(stream->command_list))
+		pthread_cond_wait(&stream->cond, &stream->lock);
+	command = list_dequeue(stream->command_list);
+
+	pthread_mutex_unlock(&stream->lock);
+
+	return command;
+}
