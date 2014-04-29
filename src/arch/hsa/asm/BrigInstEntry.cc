ca1deb4dd52a7edfb09c8c5d9d19a0a7d711fb74
yifan.sun <sunyifan112358@gmail.com>
2014-04-29 02:03:25 +0000

    hsa asm now support operands and insts in functions

--- src/arch/hsa/asm/BrigInstEntry.cc
+++ src/arch/hsa/asm/BrigInstEntry.cc
@@ -1,4 +1,5 @@
 #include "BrigInstEntry.h"
+#include "BrigOperandEntry.h"
 #include "Asm.h"
 
 namespace HSA{
@@ -9,6 +10,66 @@ const char *BrigInstEntry::opcode2str(InstOpcode opcode) const
 	Asm *as = Asm::getInstance();
 	const struct InstInfo *inst_info = as->getInstInfo();
 	return inst_info[opcode].fmt_str;
+}
+
+template <typename BrigInst>
+const char *BrigInstEntry::rounding2str(BrigInst* base) const
+{
+	unsigned short rounding;
+	//unsigned short defaultRounding;
+	rounding = base->modifier.allBits;
+	rounding &= 15;
+	//FIXME compare rouding with deaultRounding	
+	return BrigInstEntry::rounding_to_str_map.MapValue(rounding);
+}
+
+misc::StringMap BrigInstEntry::rounding_to_str_map = 
+{
+	{"", 0},
+	{"_near", 1},
+	{"_zero", 2},
+	{"_up", 3},
+	{"_down", 4},
+	{"_neari", 5},
+	{"_zeroi", 6},
+	{"_upi", 7},
+	{"_downi", 8},
+	{"_neari_sat", 9},
+	{"_zeroi_sat", 10},
+	{"_upi_sat", 11},
+	{"_downi_sat", 12}
+};
+
+const char *BrigInstEntry::modifier2str(unsigned short modifier) const
+{
+	unsigned short ftz = modifier & 16;
+	if(ftz)
+	{
+		return "_ftz";
+	}
+	return "";
+}
+
+misc::StringMap BrigInstEntry::pack_to_str_map = 
+{
+	{"", 0},
+	{"_pp", 1},
+	{"_ps", 2},
+	{"_sp", 3},
+	{"_ss", 4},
+	{"_s", 5},
+	{"_p", 6},
+	{"_pp_sat", 7},
+	{"_ps_sat", 8},
+	{"_sp_sat", 9},
+	{"_ss_sat", 10},
+	{"_s_sat", 11},
+	{"_p_sat", 12}
+};
+
+const char *BrigInstEntry::pack2str(unsigned char pack) const
+{
+	return BrigInstEntry::pack_to_str_map.MapValue(pack);
 }
 
 bool BrigInstEntry::hasType() const
@@ -38,6 +99,22 @@ bool BrigInstEntry::hasType() const
 	
 	default:
 		return true;
+	}
+}
+
+void BrigInstEntry::dumpOperands(std::ostream &os = std::cout) const
+{
+	struct BrigInstBase *inst = (struct BrigInstBase *)base;
+	os << "\t";
+	for(int i=0; i<5; i++)
+	{
+		if(inst->operands[i] == 0 ) return;
+		if(i>0) os << ", ";
+		BrigOperandEntry op(
+			BrigOperandEntry::GetOperandBufferByOffset(file, inst->operands[i]),
+			file
+		);
+		op.Dump(os);
 	}
 }
 
@@ -85,16 +162,9 @@ void BrigInstEntry::DumpInstNone(std::ostream &os) const
 void BrigInstEntry::DumpInstBasic(std::ostream &os) const
 {
 	struct BrigInstBasic *inst = (struct BrigInstBasic *)base;
-	if(inst->opcode < InstOpcodeCount)
-	{
-		os << this->opcode2str((InstOpcode)inst->opcode);
-	}
-	else
-	{
-		os << "<unknown opcode>";
-	}
+	os << this->opcode2str((InstOpcode)inst->opcode);
 	if(this->hasType()) os << "_" << BrigEntry::type2str(inst->type);
-	this->DumpInstUnsupported("Basic", os);
+	this->dumpOperands(os);
 	os << "\n";
 }
 
@@ -150,7 +220,14 @@ void BrigInstEntry::DumpInstAddr(std::ostream &os) const
 
 void BrigInstEntry::DumpInstMod(std::ostream &os) const
 {
-	this->DumpInstUnsupported("Mod", os);
+	struct BrigInstMod *inst = (struct BrigInstMod *)base;
+	os << this->opcode2str((InstOpcode)inst->opcode);
+	os << this->modifier2str(inst->modifier.allBits);
+	os << this->rounding2str(inst);
+	os << this->pack2str(inst->pack);
+	if(this->hasType()) os << "_" << BrigEntry::type2str(inst->type);
+	this->dumpOperands(os);
+	os << "\n";
 }
 
 void BrigInstEntry::DumpInstSeg(std::ostream &os) const
