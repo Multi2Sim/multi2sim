3d549183f3f73cce1198d9bdd073fa6177bb653f
yifan.sun <sunyifan112358@gmail.com>
2014-05-08 04:01:26 +0000

    Accessing HSA disassembler from m2s-cpp

--- src/arch/hsa/asm/Asm.cc
+++ src/arch/hsa/asm/Asm.cc
@@ -6,6 +6,28 @@
 namespace HSA
 {
 
+
+void AsmConfig::Register(misc::CommandLine &command_line)
+{
+	// Option --hsa-disasm <file>
+	command_line.RegisterString("--hsa-disasm", path, 
+			"Disassemble the HSA BRIG ELF file provided in <arg>. "	
+			"This options is incompatible with any other option."
+		);
+	command_line.setIncompatible("--hsa-disasm");
+}
+
+void AsmConfig::Process()
+{
+	// Run hsa disassembler
+	if(!path.empty())
+	{
+		Asm *as = Asm::getInstance();
+		as->DisassembleBinary(path);
+		exit(0);
+	}
+}
+
 std::unique_ptr<Asm> Asm::as;
 	
 Asm *Asm::getInstance()
@@ -15,6 +37,9 @@ Asm *Asm::getInstance()
 	Asm::as.reset(new Asm());
 	return Asm::as.get();
 }
+
+// hsa disassembler configuration
+AsmConfig Asm::config;
 
 Asm::Asm()
 {
