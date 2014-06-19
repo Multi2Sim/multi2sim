3475ea48eef612c8cf0ce9d2611094aa64cbc9d7
dudlykoo <dudlykoo@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2014-06-19 19:53:52 +0000

    mips/emu/contextIsa

--- src/arch/mips/emu/Context.h
+++ src/arch/mips/emu/Context.h
@@ -46,6 +46,9 @@ enum ContextState
 	ContextInvalid      = 0x00000,
 	ContextRunning      = 0x00001,  // it is able to run instructions
 	ContextSpecMode     = 0x00002,  // executing in speculative mode
+	ContextSuspended    = 0x00004,  // suspended in a system call
+	ContextHandler      = 0x00040,  // executing a signal handler
+	ContextCallback     = 0x10000,  // suspended after syscall with callback
 };
 
 /// Context list identifiers
@@ -107,6 +110,18 @@ class Context
 	// Update the context state, updating also the presence on the context
 	// in the various context lists in the emulator.
 	void UpdateState(unsigned state);
+
+	// Callbacks for suspended contexts
+	typedef bool (Context::*CanWakeupFn)();
+	typedef void (Context::*WakeupFn)();
+
+	// Stored callbacks for functions used to the wakeup mechanism of
+	// suspended contexts. Variable 'wakeup_state' contains the state
+	// or states that will be set when suspended and cleared when
+	// waken up
+	CanWakeupFn can_wakeup_fn;
+	WakeupFn wakeup_fn;
+	ContextState wakeup_state;
 
 	///////////////////////////////////////////////////////////////////////
 	//
@@ -206,6 +221,13 @@ class Context
 	// Table of signal handlers, possibly shared by multiple contexts
 	std::shared_ptr<SignalHandlerTable> signal_handler_table;
 
+	// Table of signal masks, each context has its own
+	SignalMaskTable signal_mask_table;
+
+	// Run a signal handler for signal \a sig. The value of \a sig must be
+	// between 1 and 64.
+	void RunSignalHandler(int sig);
+
 	///////////////////////////////////////////////////////////////////////
 	//
 	// Functions and fields related with mips instruction emulation,
@@ -221,15 +243,15 @@ class Context
 
 	// Instruction emulation functions. Each entry of Inst.def will be
 	// expanded into a function prototype. For example, entry
-	// 	DEFINST(adc_al_imm8, 0x14, SKIP, SKIP, SKIP, IB, 0)
+	// 	DEFINST(J,"j%target",0x02,0x00,0x00,0x00)
 	// is expanded to
 	//	void ExecuteInst_adc_al_imm8();
-#define DEFINST(name, fmt_str, fmt, opcode, func, imm) void ExecuteInst_##name();
+#define DEFINST(_name, _fmt_str, _op0, _op1, _op2, _op3) void ExecuteInst_##_name();
 #include <arch/mips/asm/Inst.def>
 #undef DEFINST
 
 	// Table of functions
-	static ExecuteInstFn execute_inst_fn[InstOpcodeCount];
+	ExecuteInstFn execute_inst_fn[InstOpcodeCount];
 
 
 public:
@@ -237,6 +259,15 @@ public:
 	/// managed by the emulator. When a context is removed from the main
 	/// context list, it is automatically freed.
 	std::list<std::unique_ptr<Context>>::iterator contexts_iter;
+
+	/// Flag indicating whether this context is present in a certain context
+	/// list of the emulator. This field is exclusively managed by the
+	/// emulator.
+	bool context_list_present[ContextListCount];
+
+	/// Position of the context in a certain context list. This field is
+	/// exclusively managed by the emulator.
+	std::list<Context *>::iterator context_list_iter[ContextListCount];
 
 	/// Constructor
 	Context();
@@ -266,6 +297,23 @@ public:
 
 	/// Set flag \a state in the context state
 	void setState(ContextState state) { UpdateState(this->state | state); }
+
+	/// Clear flag \a state in the context state
+	void clearState(ContextState state) { UpdateState(this->state
+			& ~state); }
+
+	/// Check whether a context suspended with a call to Suspend() is ready
+	/// to wake up, by invoking the 'can_wakeup' callback.
+	bool CanWakeup();
+
+	/// Wake up a context in suspended state that went to sleep with a call
+	/// to Suspend(). The 'wakeup_fn' callback function is invoked, and the
+	/// wakeup data is internally freed by reseting the smart pointer.
+	void Wakeup();
+
+	// Check whether there is any pending unblocked signal in the context,
+	// and invoke the corresponding signal handler.
+	void CheckSignalHandler();
 };
 
 }  // namespace MIPS
