a92ab42b409eeef541f9668b77419d5cc37fa681
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2014-06-22 20:22:26 +0000

    Completed class mem::Cache
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@2840 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- src/memory/Cache.h
+++ src/memory/Cache.h
@@ -75,24 +75,48 @@ public:
 	/// misc::List::Node for details.
 	class Block : public misc::List::Node
 	{
+		// Only Cache needs to initialize fields
+		friend class Cache;
+
+		// Block properties
 		unsigned tag;
 		unsigned transient_tag;
-		unsigned way;
+		unsigned way_id;
 		bool prefetched;
 		BlockState state;
+	
+	public:
+
+		/// Get the block tag
+		unsigned getTag() const { return tag; }
+
+		/// Get the way index of this block
+		unsigned getWayId() const { return way_id; }
+
+		/// Get the transient trag set in this block
+		unsigned getTransientTag() const { return transient_tag; }
+
+		/// Get the block state
+		BlockState getState() const { return state; }
+
+		/// Returned whether the block was prefetched
+		bool isPrefetched() const { return prefetched; }
 	};
 
-	/// Cache set
+private:
+
+	// Cache set
 	class Set
 	{
+		// Only Cache needs to initialize fields
+		friend class Cache;
+
 		// List of blocks in LRU order
-		misc::List lru;
+		misc::List lru_list;
 
 		// Position in Cache::blocks where the blocks start for this set
 		Block *blocks;
 	};
-
-private:
 
 	// Name of the cache, used for debugging purposes
 	std::string name;
@@ -101,6 +125,9 @@ private:
 	unsigned num_sets;
 	unsigned num_ways;
 	unsigned block_size;
+
+	// Number of sets times number of ways
+	unsigned num_blocks;
 	
 	// Mask used to get the block address
 	unsigned block_mask;
@@ -122,7 +149,125 @@ private:
 
 public:
 
+	/// Constructor
+	Cache(const std::string &name,
+			unsigned num_sets,
+			unsigned num_ways,
+			unsigned block_size,
+			ReplacementPolicy replacement_policy,
+			WritePolicy write_policy);
+	
+	/// Return a pointer to a cache set
+	Set *getSet(unsigned set_id)
+	{
+		assert(misc::inRange(set_id, 0, num_sets - 1));
+		return &sets.get()[set_id];
+	}
 
+	/// Return a pointer to a cache block
+	Block *getBlock(unsigned set_id, unsigned way_id)
+	{
+		assert(misc::inRange(set_id, 0, num_sets - 1));
+		assert(misc::inRange(way_id, 0, num_ways - 1));
+		return &blocks.get()[set_id * num_ways + way_id];
+	}
+
+	/// Decode a physical address.
+	///
+	/// \param address
+	///	Address to be decoded
+	///
+	/// \param set_id
+	///	Return here the set index for the address.
+	///
+	/// \param tag
+	///	Return here the tag for the address
+	///
+	/// \param block_offset
+	///	Return here the block offset for the address
+	void DecodeAddress(unsigned address,
+			unsigned &set_id,
+			unsigned &tag,
+			unsigned &block_offset);
+
+	/// Check whether an address is present in the cache.
+	///
+	/// \param address
+	///	Physical address to search for.
+	///
+	/// \param set_id
+	///	If the address was found in the cache, return here the set
+	///	index.
+	///
+	/// \param way_id
+	///	If the address was found in the cache, return here the way
+	///	index.
+	///
+	/// \param state
+	///	If the address was found in the cache, return here the state of
+	///	the block containing the address.
+	///
+	/// \return
+	///	The function returns true if the address was found in the cache
+	///	in a block with a valid state.
+	bool FindBlock(unsigned address,
+			unsigned &set_id,
+			unsigned &way_id,
+			BlockState &state);
+
+	/// Set a new tag and state for a cache block. If a new tag is set to
+	/// the block, this function also updates the FIFO counters to indicate
+	/// that a new block was brought to the cache.
+	///
+	/// \param set_id
+	///	Set of the block to modify.
+	///
+	/// \param way_id
+	///	Way of the block to modify.
+	///
+	/// \param tag
+	///	New tag for the block
+	///
+	/// \param state
+	///	New state for the block
+	void setBlock(unsigned set_id,
+			unsigned way_id,
+			unsigned tag,
+			BlockState state);
+
+	/// Return the tag and the state of a cache block.
+	///
+	/// \param set_id
+	///	Set of the block to query.
+	///
+	/// \param way_id
+	///	Way of the block to query.
+	///
+	/// \param tag
+	///	The tag of the queried block is returned here.
+	///
+	/// \param state
+	///	The state of the queried block is returned here.
+	void getBlock(unsigned set_id,
+			unsigned way_id,
+			unsigned &tag,
+			BlockState &state);
+
+	/// Mark a block as last accessed as per the LRU policy. This function
+	/// internally updates the linked list that keeps track of the LRU order
+	/// of the blocks in a set.
+	void AccessBlock(unsigned set_id, unsigned way_id);
+
+	/// Return the way index of the block to be replaced in the given set,
+	/// as per the current block replacement policy.
+	unsigned ReplaceBlock(unsigned set_id);
+
+	/// Set the transient tag of a block.
+	void setTransientTag(unsigned set_id, unsigned way_id, unsigned tag)
+	{
+		Block *block = getBlock(set_id, way_id);
+		block->transient_tag = tag;
+	}
 };
 
 }  // namespace mem
