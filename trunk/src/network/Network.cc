70b8cc9509dd7f1b7f90426e49e08b229f0ef4fe
amir <amir@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2014-06-23 20:11:14 +0000

    Network Header files

--- trunk/src/network/Network.cc
+++ trunk/src/network/Network.cc
@@ -17,54 +17,12 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef NETWORK_NETWORK_H
-#define NETWORK_NETWORK_H
+#include <cstring>
 
-#include "System.h"
+#include "Network.h"
 
 namespace net
 {
 
-class Node;
-class Link;
 
-
-class Network
-{
-
-	//
-	// Class Members
-	//
-
-	// Network Name
-	static std::string name;
-
-	// Message ID counter
-	static long long msg_id_counter;
-
-	// Node list of the Network
-	static const std::vector<std::unique_ptr<Node>> node_list;
-
-	// Total number of End nodes in the Network
-	static int end_node_count;
-
-	// Link List of the Network
-	static const std::vector<std::unique_ptr<Link>> link_list;
-
-	// Unique instance of singleton
-	static std::unique_ptr<Network> network;
-
-	// Private constructor for singleton
-	Network();
-
-public:
-
-	/// Get instance of singleton
-	static Network *getInstance();
-
-
-};
-}  // namespace net
-
-#endif
-
+}
