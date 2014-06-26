55124f82768c43a12da23440ee9c4b25f4646b85
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2014-06-26 17:34:57 +0000

    Back trace for panic messages
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@2872 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- src/lib/cpp/Error.cc
+++ src/lib/cpp/Error.cc
@@ -17,6 +17,10 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <cxxabi.h>
+#include <execinfo.h>
+#include <memory>
+
 #include "Error.h"
 #include "String.h"
 
@@ -49,13 +53,76 @@ void Exception::Dump(std::ostream &os) const
 	// Message
 	formatter << getMessage();
 
+	// Start red
+	os << red;
+
 	// Dump it
-	os << '\n' << red << formatter << black << "\n\n";
+	os << '\n' << formatter << "\n\n";
+
+	// Call stack
+	if (!call_stack.empty())
+	{
+		formatter.Clear();
+		formatter.setIndent(8);
+		formatter.setFirstLineIndent(0);
+		formatter << "Call stack:\n" << call_stack << "\n\n";
+		os << formatter;
+	}
+
+	// Start black
+	os << black;
+}
+
+
+std::string Exception::DemangleSymbol(const char* const symbol)
+{
+	const std::unique_ptr<char, decltype(&std::free)> demangled(
+			abi::__cxa_demangle(symbol, 0, 0, 0 ), &std::free);
+	return demangled ? demangled.get() : symbol;
 }
 
 
 void Exception::SaveCallStack()
 {
+	// Get back trace and symbol names
+	void *addresses[256];
+	const int n = backtrace(addresses,
+			std::extent<decltype(addresses)>::value);
+	const std::unique_ptr<char *, decltype(&std::free)> symbols(
+			backtrace_symbols(addresses, n), &std::free);
+
+	// Code below obtained from:
+	// http://stackoverflow.com/questions/19190273/how-to-print-call-stack-
+	// in-c-c-more-beautifully
+	//
+	// We start at two, in order to skip the two top levels of the stack,
+	// which are this function and the constructor of the exception.
+	for (int i = 2; i < n; ++i)
+	{
+		char *const symbol = symbols.get()[i];
+		char *end = symbol;
+		
+		while (*end)
+			++end;
+		while (end != symbol && *end != '+')
+			--end;
+		char *begin = end;
+		while (begin != symbol && *begin != '(')
+			--begin;
+
+		call_stack += misc::fmt("[%d] ", i - 1);
+		if (begin != symbol)
+		{
+			call_stack += std::string(symbol, ++begin - symbol);
+			*end++ = '\0';
+			call_stack += DemangleSymbol(begin) + '+' + end;
+		}
+		else
+		{
+			call_stack += symbol;
+		}
+		call_stack += '\n';
+	}
 }
 
 
