83b2324194f9ac3ab5c6e65b0e12a1e35777175f
xun.gong <xun.gong@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2014-07-06 13:37:08 +0000

    addition for Kepler Driver

---
+++ src/arch/kepler/driver/Function.h
@@ -0,0 +1,90 @@
+/*
+d *  Multi2Sim
+ *  Copyright (C) 2012  Rafael Ubal (ubal@ece.neu.edu)
+ *
+ *  This module is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This module is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this module; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef ARCH_KEPLER_DRIVER_FUNCTION_H
+#define ARCH_KEPLER_DRIVER_FUNCTION_H
+
+#include <memory>
+#include <vector>
+
+#include"Function-arg.h"
+#include"Module.h"
+
+
+namespace Kepler
+{
+
+class CUfunction
+{
+	// ID
+	int id;
+
+	// Name
+	std::unique_ptr<char> name;
+
+	// Module ID
+	unsigned module_id;
+
+	// Instruction binary
+	int inst_bin_size;
+	unsigned long long *inst_buffer;
+
+	// Number of GPRs used by one thread
+	int num_gpr;
+
+	// Arguments
+	int arg_count;
+	CUfunctionarg **arg_array;
+
+public:
+
+	/// Constructor
+	CUfunction(CUmodule *module, char *function_name);
+
+	/// Destructor
+	~CUfunction();
+
+	/// Static Function List
+	static std::vector<CUfunction*> function_list;
+
+	/// Get Instruction binary
+	int getInstructionBinarySize()	{ return inst_bin_size; }
+
+	/// Get Instruction buffer
+	unsigned long long* getInstructionBuffer()	{ return inst_buffer; }
+
+	/// Get Number of GPRS
+	int getNumberofGPR()	{ return num_gpr; }
+
+	/// Get Argument count
+	int getArgCount()	{ return arg_count; }
+
+	/// Get Argument member
+	CUfunctionarg* getArgMember (int i)	{return arg_array[i];}
+
+	/// Set Argument count
+	void setArgCount(int arg_count)	{ this->arg_count = arg_count; }
+
+	/// Set Argument member
+	void setArgMember (CUfunctionarg* arg, int index) { this->arg_array[index] = arg; }
+};
+
+} // namespace Kepler
+
+#endif
