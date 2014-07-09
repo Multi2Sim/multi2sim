18832843ee02595ae02630d71dcbf28ac55e14c5
ubal <rafael.ubal.83@gmail.com>
2014-07-09 17:25:30 +0000

    Functions in MMU

--- src/memory/MMU.cc
+++ src/memory/MMU.cc
@@ -23,5 +23,37 @@
 namespace mem
 {
 
+bool MMU::TranslatePhysicalAddress(unsigned physical_address,
+		Space *&space,
+		unsigned &virtual_address)
+{
+	// Find page
+	unsigned physical_tag = physical_address & PageMask;
+	auto it = physical_pages.find(physical_tag);
+
+	// Page not found
+	if (it == physical_pages.end())
+	{
+		space = nullptr;
+		virtual_address = 0;
+		return false;
+	}
+
+	// Return page information
+	Page *page = it->second;
+	space = page->getSpace();
+	virtual_address = page->getVirtualAddress();
+	return true;
+}
+	
+
+bool MMU::isValidPhysicalAddress(unsigned physical_address)
+{
+	unsigned physical_tag = physical_address & PageMask;
+	auto it = physical_pages.find(physical_tag);
+	return it != physical_pages.end();
+}
+
+
 } // namespace mem
 
