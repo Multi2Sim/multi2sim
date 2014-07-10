270619a1a63afac73d9c94191ed90ab016abf915
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2014-07-10 03:54:02 +0000

    Memory management unit completed with debug information. Command-line option --mmu-debug

--- trunk/src/memory/MMU.h
+++ trunk/src/memory/MMU.h
@@ -24,6 +24,8 @@
 #include <unordered_map>
 #include <vector>
 
+#include <lib/cpp/Debug.h>
+
 
 namespace mem
 {
@@ -77,12 +79,15 @@ public:
 	public:
 
 		/// Constructor
-		Page(Space *space, unsigned virtual_address,
+		Page(Space *space,
+				unsigned virtual_address,
 				unsigned physical_address) :
 				space(space),
 				virtual_address(virtual_address),
 				physical_address(physical_address)
 		{
+			assert((virtual_address & ~PageMask) == 0);
+			assert((physical_address & ~PageMask) == 0);
 		}
 
 		/// Return the virtual address space that the page belongs to
@@ -98,6 +103,11 @@ public:
 	/// Virtual memory space in the MMU
 	class Space
 	{
+		// Name of the irtual memory space
+		std::string name;
+
+		// Memory management unit that it belongs to
+		MMU *mmu;
 
 		// Hash table of pages in this virtual space indexed by their
 		// virtual address.
@@ -105,16 +115,43 @@ public:
 
 	public:
 
+		/// Constructor
+		Space(const std::string &name, MMU *mmu);
+
+		/// Return the name of the virtual memory space
+		const std::string &getName() const { return name; }
+
+		/// Return memory management unit that the virtual memory
+		/// space belongs to.
+		MMU *getMMU() const { return mmu; }
+
 		/// Add a new page to the virtual address space. A page must
 		/// not exist with the same address.
-		void addPage(unsigned virtual_address, Page *page);
+		void addPage(Page *page);
 
 		/// Return the page associated with the given virtual address,
-		/// or `nullptr` if none is.
+		/// or `nullptr` if none is. Argument \a virtual_address must be
+		/// a multiple of the page size.
 		Page *getPage(unsigned virtual_address);
 	};
 
 private:
+
+	// Output report file, as set by the user
+	static std::string report_file_name;
+
+	// File to dump MMU debug information, as set by the user
+	static std::string debug_file;
+
+	// Debugger for MMU
+	static misc::Debug debug;
+	
+	// Name of the MMU
+	std::string name;
+
+	// Top of the physical address space. Every time a new page is
+	// allocated, this value is incremented by PageSize.
+	unsigned top_physical_address = 0;
 
 	// Vector containing all virtual address spaces
 	std::vector<std::unique_ptr<Space>> spaces;
@@ -125,9 +162,6 @@ private:
 	// Hash table of pages indexed by their physical address
 	std::unordered_map<unsigned, Page *> physical_pages;
 
-	// Output report file, as set by the user
-	static std::string report_file_name;
-
 public:
 
 	/// Register command-line options
@@ -136,8 +170,20 @@ public:
 	/// Process command-line options
 	static void ProcessOptions();
 
+	/// Constructor
+	///
+	/// \param name
+	///	Name of the MMU used for debugging purposes.
+	MMU(const std::string &name);
+
+	/// Return the name of the MMU
+	const std::string &getName() const { return name; }
+
 	/// Create a new virtual address space
-	Space *newSpace();
+	///
+	/// \param name
+	///	Name of the virtual address space used for debugging purposes.
+	Space *newSpace(const std::string &name);
 
 	/// Translate virtual to physical address.
 	///
