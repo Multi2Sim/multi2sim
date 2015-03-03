bfafeb1f1932cefa924dc9c9859bf187d39623c4
yifan.sun <yifan.sun@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2015-03-03 22:09:40 +0000

    HSA: support for argc argv arguments

--- trunk/src/arch/hsa/emu/Emu.cc
+++ trunk/src/arch/hsa/emu/Emu.cc
@@ -17,6 +17,8 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <cstring>
+
 #include <arch/hsa/asm/Asm.h>
 
 #include "Emu.h"
@@ -199,6 +201,40 @@ void Emu::LoadProgram(const std::vector<std::string> &args,
 	ProgramLoader::LoadProgram(args, env, cwd,
 			stdin_file_name, stdout_file_name);
 
+	/*
+	for (unsigned int i = 0; i < args.size(); i++)
+	{
+		std::cout << args.at(i);
+	}
+	*/
+
+	// Create an array of kernel arguments
+	unsigned int argc = args.size();
+	unsigned long long argv = manager.Allocate(8 * argc);
+	char *arguments_buf = memory.getBuffer(argv, 8 * argc, 
+			mem::Memory::AccessWrite);
+	for (unsigned int i = 0; i < argc; i++)
+	{
+		unsigned int str_size = args.at(i).length() + 1;
+		unsigned int str_addr = manager.Allocate(str_size);
+		char *str_buffer = memory.getBuffer(str_addr, str_size, 
+				mem::Memory::AccessWrite);
+		memcpy(str_buffer, args.at(i).c_str(), str_size - 1);
+		str_buffer[str_size - 1] = 0;
+		
+		*(unsigned long long *)arguments_buf = str_addr;
+		arguments_buf += 8;
+	}
+
+	// Create kernel argument memory spaces
+	unsigned kernarg_address = manager.Allocate(12);
+	char *kernarg_buf = memory.getBuffer(kernarg_address, 12, 
+			mem::Memory::AccessWrite);
+	*(unsigned int *)kernarg_buf = argc;
+	*(unsigned long long *)(kernarg_buf + 4) = argv;
+	std::cout << misc::fmt("Kern address 0x%x, argv 0x%llx\n", 
+			kernarg_address, argv);
+
 	// Create a simple queue and add it to host cpu
 	auto queue = misc::new_unique<AQLQueue>(2, HSA_QUEUE_TYPE_MULTI);
 	AQLQueue *queue_ptr = queue.get();
@@ -211,8 +247,10 @@ void Emu::LoadProgram(const std::vector<std::string> &args,
 	packet->setWorkGroupSize(1, 1, 1);
 	ProgramLoader *loader = ProgramLoader::getInstance();
 	Function *main_function = loader->getMainFunction();
-	unsigned int function_dir = main_function->getFunctionDirective()->getOffset();
+	unsigned int function_dir = main_function->
+			getFunctionDirective()->getOffset();
 	packet->setKernalObjectAddress((unsigned long long)function_dir);
+	packet->setKernargAddress((unsigned long long)kernarg_address);
 	packet->setPrivateSegmentSizeBytes(0x10000);
 	packet->setGroupSegmentSizeBytes(0x10000);
 
