ce43f7b10994c9e561624c932c37b38c4c8d9c22
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2015-05-06 15:13:18 +0000

    Headers for memory configuration parsing
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@3199 a6503f54-2359-11de-8ce0-bfd7a313a9c3

---
+++ src/arch/common/Timing.cc
@@ -0,0 +1,42 @@
+/*
+ *  Multi2Sim
+ *  Copyright (C) 2014  Rafael Ubal (ubal@ece.neu.edu)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received as copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <lib/cpp/Error.h>
+
+#include "Timing.h"
+
+
+namespace comm
+{
+	
+void Timing::WriteMemoryConfiguration(misc::IniFile *ini_file)
+{
+	throw misc::Panic("Architecture does not have a default memory "
+			"configuration");
+}
+
+
+void Timing::CheckMemoryConfiguration(misc::IniFile *ini_file)
+{
+	throw misc::Panic("Architecture does not have a memory configuration "
+			"checker");
+}
+
+}
+
