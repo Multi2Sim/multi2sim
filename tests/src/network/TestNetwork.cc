99643e09d2cd97cdac4186a178cc9d7ce33902f9
yifan.sun <yifan.sun@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2015-05-08 16:12:07 +0000

    Remove node factory and link factory
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@3220 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- tests/src/network/TestNetwork.cc
+++ tests/src/network/TestNetwork.cc
@@ -21,64 +21,15 @@
 
 #include <lib/cpp/IniFile.h>
 #include <network/Network.h>
+#include <network/EndNode.h>
+#include <network/Switch.h>
+#include <network/Link.h>
 
 namespace net
 {
 
-TEST(TestNetwork, should_create_according_to_ini_file)
+TEST(Network, should_create_according_to_ini_file)
 {
-	class MockupNode : public Node
-	{
-		std::string getType() const override
-		{
-			return "MockupNode";
-		}
-		void Dump(std::ostream &os = std::cout) const override
-		{
-			os << misc::fmt("\nMockup node : %s\n",
-					name.c_str());
-		}
-	};
-
-	class MockupNodeFactory : public NodeFactory
-	{
-		std::unique_ptr<Node> ProduceNodeByIniSection(Network *network,
-				const std::string &section, 
-				misc::IniFile &config) override
-		{
-			std::vector<std::string> tokens;
-			misc::StringTokenize(section, tokens, ".");
-			std::unique_ptr<Node> node = 
-				std::unique_ptr<MockupNode>(new MockupNode());
-			node->setName(tokens[3]);
-			return node;
-		}
-	};
-
-	class MockupLink : public Link
-	{
-		void Dump(std::ostream &os = std::cout) const override
-		{
-			os << misc::fmt("\nMockup link : %s\n", 
-					name.c_str());
-		}
-	};
-
-	class MockupLinkFactory : public LinkFactory
-	{
-		std::unique_ptr<Link> ProduceLinkByIniSection(Network *network,
-				const std::string &section, 
-				misc::IniFile &config) override
-		{
-			std::vector<std::string> tokens;
-			misc::StringTokenize(section, tokens, ".");
-			std::unique_ptr<Link> link = 
-				std::unique_ptr<MockupLink>(new MockupLink());
-			link->setName(tokens[3]);
-			return link;
-		}
-	};
-
 	// Setup ini file
 	std::string ini_file = 
 		"[ Network.mynet ]\n"
@@ -88,36 +39,32 @@ TEST(TestNetwork, should_create_according_to_ini_file)
 		"\n"
 		"[ Network.mynet.Node.N1 ]\n"
 		"Type = EndNode \n"
-		"[ Network.mynet.Node.N2 ]\n"
-		"Type = EndNode \n"
-		"[Network.mynet.Link.N1-N2]\n"
+		"[ Network.mynet.Node.S1 ]\n"
+		"Type = Switch \n"
+		"[Network.mynet.Link.N1-S1]\n"
 		"Type = Bidirectional\n"
 		"Source = N1\n"
-		"Dest = N2";
+		"Dest = S1";
 	misc::IniFile ini;
 	ini.LoadFromString(ini_file);
 
 	// Setup network
 	Network net("mynet");
-	auto node_factory = misc::new_unique<MockupNodeFactory>();
-	auto link_factory = misc::new_unique<MockupLinkFactory>();
-	net.setNodeFactory(std::move(node_factory));
-	net.setLinkFactory(std::move(link_factory));
 	net.ParseConfiguration("Network.mynet", ini);
 
-	// Output stream
-	std::ostringstream oss;
+	// Assert N1 is created 
+	Node *n1 = net.getNodeByName("N1");
+	EXPECT_TRUE(n1 != nullptr);
+	EXPECT_TRUE(dynamic_cast<EndNode *>(n1) != 0);
 
-	// Assertions
-	net.Dump(oss);
-	std::string target = "\n***** Network mynet *****\n"
-			"\tDefault input buffer size: 16\n"
-			"\tDefault output buffer size: 16\n"
-			"\tDefault bandwidth: 1\n"
-			"\nMockup node : N1\n"
-			"\nMockup node : N2\n"
-			"\nMockup link : N1-N2\n";
-	EXPECT_STREQ(target.c_str(), oss.str().c_str());
+	// Assert S1 is created
+	Node *s1 = net.getNodeByName("S1");
+	EXPECT_TRUE(s1 != nullptr);
+	EXPECT_TRUE(dynamic_cast<Switch *>(s1) != 0);
+
+	// Assert link n1-s1 is created
+	Link *n1_s1 = net.getLinkByName("N1-S1");
+	EXPECT_TRUE(n1_s1 != nullptr);
 }
 
 }
