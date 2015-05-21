ca130e4c85456856d8537674dd39a3e58b1763e2
amir <ziabari@gmail.com>
2015-05-21 01:40:16 +0000

    Adding test for bus. adding support for busport. test for busport in the next commit

--- tests/src/network/TestNetwork.cc
+++ tests/src/network/TestNetwork.cc
@@ -24,15 +24,16 @@
 #include <network/EndNode.h>
 #include <network/Switch.h>
 #include <network/Link.h>
+#include <network/Bus.h>
 #include <network/Connection.h>
 
 namespace net
 {
 
-TEST(Network, should_create_according_to_ini_file)
+TEST(Network, INI_FILE_TEST_LINK_1)
 {
 	// Setup ini file
-	std::string ini_file = 
+	std::string ini_file =
 		"[ Network.mynet ]\n"
 		"DefaultInputBufferSize = 16 \n"
 		"DefaultOutputBufferSize = 16 \n"
@@ -53,7 +54,7 @@ TEST(Network, should_create_according_to_ini_file)
 	Network net("mynet");
 	net.ParseConfiguration("Network.mynet", ini);
 
-	// Assert N1 is created 
+	// Assert N1 is created
 	Node *n1 = net.getNodeByName("N1");
 	EXPECT_TRUE(n1 != nullptr);
 	EXPECT_TRUE(dynamic_cast<EndNode *>(n1) != 0);
@@ -65,35 +66,77 @@ TEST(Network, should_create_according_to_ini_file)
 
 	// Assert link n1-s1 is created
 	Connection *n1_s1 = net.getConnectionByName(
-		"link_<N1.out_buf_0>_<S1.in_buf_0>");
-	EXPECT_TRUE(!strcasecmp(dynamic_cast<Link *> 
+			"link_<N1.out_buf_0>_<S1.in_buf_0>");
+	EXPECT_TRUE(!strcasecmp(dynamic_cast<Link *>
 		(n1_s1)->getUserAssignedName().c_str(), "N1-S1"));
 	EXPECT_TRUE(n1_s1 != nullptr);
 	EXPECT_TRUE(dynamic_cast<Link *>(n1_s1) != 0);
-	
-	Connection *s1_n1 = net.getConnectionByName("link_<S1.out_buf_0>_<N1.in_buf_0>");
-	EXPECT_TRUE(!strcasecmp(dynamic_cast<Link *> 
+
+	Connection *s1_n1 = net.getConnectionByName(
+			"link_<S1.out_buf_0>_<N1.in_buf_0>");
+	EXPECT_TRUE(!strcasecmp(dynamic_cast<Link *>
 		(s1_n1)->getUserAssignedName().c_str(), "N1-S1"));
 	EXPECT_TRUE(s1_n1 != nullptr);
 	EXPECT_TRUE(dynamic_cast<Link *>(s1_n1) != 0);
 };
 
-/*
-TEST(Network, should_throw_error_if_no_route)
+TEST(Network, INI_FILE_TEST_BUS_1)
 {
-	// Mockup routing table
-	class MockupRoutingTable : public RoutingTable
+	// Setup ini file
+	std::string ini_file = 
+		"[ Network.mynet ]\n"
+		"DefaultInputBufferSize = 16 \n"
+		"DefaultOutputBufferSize = 16 \n"
+		"DefaultBandwidth = 1 \n"
+		"\n"
+		"[ Network.mynet.Node.N1 ]\n"
+		"Type = EndNode \n"
+		"[ Network.mynet.Node.S1 ]\n"
+		"Type = Switch \n"
+		"[Network.mynet.BUS.B1]\n"
+		"[Network.mynet.BUS.B2]\n"
+		"Lanes = 3";
+	misc::IniFile ini;
+	ini.LoadFromString(ini_file);
+
+	// Setup network
+	Network net("mynet");
+	net.ParseConfiguration("Network.mynet", ini);
+
+	// Assert N1 is created 
+	Node *n1 = net.getNodeByName("N1");
+	EXPECT_TRUE(n1 != nullptr);
+	EXPECT_TRUE(dynamic_cast<EndNode *>(n1) != 0);
+
+	// Assert S1 is created
+	Node *s1 = net.getNodeByName("S1");
+	EXPECT_TRUE(s1 != nullptr);
+	EXPECT_TRUE(dynamic_cast<Switch *>(s1) != 0);
+
+	// Assert link n1-s1 is created
+	Connection *B1 = net.getConnectionByName("B1");
+	EXPECT_TRUE(B1 != nullptr);
+	Bus * b1 = dynamic_cast<Bus *> (B1);
+	EXPECT_TRUE(b1 != 0);
+	EXPECT_TRUE(b1->getNumberLanes() == 1);
+
+	Connection *B2 = net.getConnectionByName("B2");
+	EXPECT_TRUE( B2 != nullptr );
+	Bus * b2 = dynamic_cast<Bus *> (B2);
+	EXPECT_TRUE(b2 != 0);
+	EXPECT_TRUE(b2->getNumberLanes() == 3);
+	for (int i = 0 ; i < b2->getNumberLanes(); i++)
 	{
-		RoutingTableEntry *Lookup(Node *source, Node *destination) 
-				override
-		{
-			return nullptr;
-		}
+		Bus::Lane *lane;
+		lane = b2->getLaneByIndex(i);
+		EXPECT_TRUE( lane != nullptr);
 	}
 
-	Network net("mynet");
-};
-*/
+
+	EXPECT_TRUE(net.getNumberConnections() == 2);
+
+
+}
 
 }
 
