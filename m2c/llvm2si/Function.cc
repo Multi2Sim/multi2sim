28e7f331113bf6523392c5b9d64603877df6f679
xiang <xiang@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2015-06-20 17:09:56 +0000

    Remove move instructions from header basic block for liveness analysis
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@3389 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- m2c/llvm2si/Function.cc
+++ m2c/llvm2si/Function.cc
@@ -478,47 +478,6 @@ void Function::EmitHeader()
 	// in COMPUTE_PGM_RSRC2 metadata.
 	sreg_wgid = AllocSReg(3);
 
-	// Dummy instructions for liveness analysis pass
-	for (int index = 0; index < 3; index++)
-	{
-		Instruction *inst = basic_block->addInstruction(SI::INST_V_MOV_B32);
-		inst->addVectorRegister(vreg_lid + index);
-		inst->addVectorRegister(vreg_lid + index);
-		assert(inst->hasValidArguments());
-	}
-
-	for (int index = 0; index < 2; ++index)
-	{
-		Instruction *inst = basic_block->addInstruction(SI::INST_S_MOV_B32);
-		inst->addScalarRegister(sreg_uav_table + index);
-		inst->addScalarRegister(sreg_uav_table + index);
-		assert(inst->hasValidArguments());
-	}
-
-	for (int index = 0; index < 4; ++index)
-	{
-		Instruction *inst = basic_block->addInstruction(SI::INST_S_MOV_B32);
-		inst->addScalarRegister(sreg_cb0 + index);
-		inst->addScalarRegister(sreg_cb0 + index);
-		assert(inst->hasValidArguments());
-	}
-
-	for (int index = 0; index < 4; ++index)
-	{
-		Instruction *inst = basic_block->addInstruction(SI::INST_S_MOV_B32);
-		inst->addScalarRegister(sreg_cb1 + index);
-		inst->addScalarRegister(sreg_cb1 + index);
-		assert(inst->hasValidArguments());
-	}
-
-	for (int index = 0; index < 3; ++index)
-	{
-		Instruction *inst = basic_block->addInstruction(SI::INST_S_MOV_B32);
-		inst->addScalarRegister(sreg_wgid + index);
-		inst->addScalarRegister(sreg_wgid + index);
-		assert(inst->hasValidArguments());
-	}
-
 	// Obtain global size in s[gsize:gsize + 2].
 	//
 	// s_buffer_load_dword s[gsize], s[cb0:cb0 + 3], 0x00
