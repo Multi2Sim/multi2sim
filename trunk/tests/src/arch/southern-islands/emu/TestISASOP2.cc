10d2a2eca800c21e6ffc342840cf39e38fd7efd5
trevor.gale <trevor.gale@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2015-06-21 20:34:41 +0000

    Added another test once Tushar fixed the seg fault in the object pool, and I got the same seg fault. Committing changes so Tushar can take a look.

--- trunk/tests/src/arch/southern-islands/emu/TestISASOP2.cc
+++ trunk/tests/src/arch/southern-islands/emu/TestISASOP2.cc
@@ -124,16 +124,15 @@ TEST(TestISASOP2, S_ADD_U32)
 	EXPECT_EQ(0, work_item->ReadReg(scc)) << "lit const: error with carry";
 }
 
-/*
 // This test checks the implementation of the S_ADD_I32 instruction.
 // It checks for basic addition, proper implementation of the overflow, 
 // and proper implementation of the literal constant
 TEST(TestISASOP2, S_ADD_I32)
 {
 	// Initialize environment using the ObjectPool
-	ObjectPool pool;
-	WorkItem *work_item = pool.getWorkItem();
-	Inst *inst = pool.getInst();
+	auto pool = misc::new_unique<ObjectPool>();
+	WorkItem *work_item = pool->getWorkItem();
+	Inst *inst = pool->getInst();
 
 	
 	// Build instruction -  S_add_I32 s2, scc, s0, s1
@@ -231,9 +230,9 @@ TEST(TestISASOP2, S_ADD_I32)
 TEST(TestISASOP2, S_SUB_I32)
 {
 	// Initialize environment using the ObjectPool
-	ObjectPool pool;
-	WorkItem *work_item = pool.getWorkItem();
-	Inst *inst = pool.getInst();
+	auto pool = misc::new_unique<ObjectPool>();
+	WorkItem *work_item = pool->getWorkItem();
+	Inst *inst = pool->getInst();
 
 	
 	// Build instruction -  S_SUB_I32 s2, scc, s0, s1
@@ -291,16 +290,16 @@ TEST(TestISASOP2, S_SUB_I32)
 	// Test overflow
 	//
 
-	// Set values in registers - s0 and s1 = max 32 bit unsigned value
-	work_item->WriteSReg(ssrc0, 0xffffffff);
+	// Set values in registers - s0 and s1 = max 32 bit signed value
+	work_item->WriteSReg(ssrc0, 0x7fffffff);
 	work_item->WriteSReg(ssrc1, 0xffffffff);
 
 	// Execute instruction
 	work_item->Execute(inst->getOpcode(), inst);
 
 	// Read results - s2 should be 0 and scc should be 0
-	EXPECT_EQ(0, work_item->ReadSReg(sdst)) << "overflow test: sum error";
-	EXPECT_EQ(0, work_item->ReadReg(scc)) << "overflow test: overflow error";
+	EXPECT_EQ(2147483648, work_item->ReadSReg(sdst)) << "overflow test: sum error";
+	EXPECT_EQ(1, work_item->ReadReg(scc)) << "overflow test: overflow error";
 
 
 
@@ -331,9 +330,9 @@ TEST(TestISASOP2, S_SUB_I32)
 TEST(TestISASOP2, S_MIN_U32)
 {
 	// Initialize environment using the ObjectPool
-	ObjectPool pool;
-	WorkItem *work_item = pool.getWorkItem();
-	Inst *inst = pool.getInst();
+	auto pool = misc::new_unique<ObjectPool>();
+	WorkItem *work_item = pool->getWorkItem();
+	Inst *inst = pool->getInst();
 
 	
 	// Build instruction -  S_MIN_U32 s2, scc, s0, s1
@@ -380,8 +379,102 @@ TEST(TestISASOP2, S_MIN_U32)
 	// Execute instruction
 	work_item->Execute(inst->getOpcode(), inst);
 
-	// Read results - s2 should be 1 and scc should be 1
-	EXPECT_EQ(1, work_item->ReadSReg(sdst));
+	// Read results - s2 should be 2 and scc should be 0
+	EXPECT_EQ(2, work_item->ReadSReg(sdst));
+	EXPECT_EQ(0, work_item->ReadReg(scc));
+
+
+	// Set values in registers - s0 and s1
+	work_item->WriteSReg(ssrc0, 20);
+	work_item->WriteSReg(ssrc1, 5000);
+
+	// Execute instruction
+	work_item->Execute(inst->getOpcode(), inst);
+
+	// Read results - s2 should be 20 and scc should be 1
+	EXPECT_EQ(20, work_item->ReadSReg(sdst));
+	EXPECT_EQ(1, work_item->ReadReg(scc));
+
+
+
+
+	//
+	// Test literal constant support
+	//
+
+	// Build new instruction with literal constant
+	inst_bytes.ssrc0 = 0xff;
+	inst_bytes.lit_cnst = 10;
+	inst->Decode((char *) &inst_bytes, 0);
+
+	// Set values in registers - s1 = 2, s0 is replaced with the literal
+	work_item->WriteSReg(ssrc1, 2);
+	
+	// Execute instruction
+	work_item->Execute(inst->getOpcode(), inst);
+
+	// Read results - s2 should be 2 and scc should be 0
+	EXPECT_EQ(2, work_item->ReadSReg(sdst));
+	EXPECT_EQ(0, work_item->ReadReg(scc));
+}
+
+// This test checks the implementation of the S_MAX_I32 instruction.
+// It checks for basic greater than operand, and proper implementation 
+// of the literal constant
+TEST(TestISASOP2, S_MAX_I32)
+{
+	// Initialize environment using the ObjectPool
+	auto pool = misc::new_unique<ObjectPool>();
+	WorkItem *work_item = pool->getWorkItem();
+	Inst *inst = pool->getInst();
+
+	
+	// Build instruction -  S_MAX_I32 s2, scc, s0, s1
+	// inst_bytes fields are defined in southern-islands/asm/Inst.h
+	InstBytesSOP2 inst_bytes = 
+	{
+		// ssrc0 - s0
+		0,
+
+		// ssrc1 - s1
+		1,
+
+		// sdst - s2
+		2,
+
+		// op - S_MAX_I32 has opcode = 8                    
+		8,
+
+		// enc - 2
+		2,
+
+		// lit_cnst - 1
+		0
+	};
+	inst->Decode((char *) &inst_bytes, 0);
+
+	// Get registers
+	int ssrc0 = inst->getBytes()->sop2.ssrc0;
+	int ssrc1 = inst->getBytes()->sop2.ssrc1;
+	int sdst = inst->getBytes()->sop2.sdst;
+	int scc = Inst::RegisterScc;
+
+
+
+
+	//
+	// Test basic less-than operand
+	//
+
+	// Set values in registers - s0 = 4 and s1 = 2
+	work_item->WriteSReg(ssrc0, 4);
+	work_item->WriteSReg(ssrc1, 2);
+
+	// Execute instruction
+	work_item->Execute(inst->getOpcode(), inst);
+
+	// Read results - s2 should be 4 and scc should be 1
+	EXPECT_EQ(4, work_item->ReadSReg(sdst));
 	EXPECT_EQ(1, work_item->ReadReg(scc));
 
 
@@ -392,8 +485,8 @@ TEST(TestISASOP2, S_MIN_U32)
 	// Execute instruction
 	work_item->Execute(inst->getOpcode(), inst);
 
-	// Read results - s2 should be 0 and scc should be 0
-	EXPECT_EQ(0, work_item->ReadSReg(sdst));
+	// Read results - s2 should be 5000 and scc should be 0
+	EXPECT_EQ(5000, work_item->ReadSReg(sdst));
 	EXPECT_EQ(0, work_item->ReadReg(scc));
 
 
@@ -414,10 +507,10 @@ TEST(TestISASOP2, S_MIN_U32)
 	// Execute instruction
 	work_item->Execute(inst->getOpcode(), inst);
 
-	// Read results - s2 should be 0 and scc should be 0
-	EXPECT_EQ(0, work_item->ReadSReg(sdst));
-	EXPECT_EQ(0, work_item->ReadReg(scc));
+	// Read results - s2 should be 10 and scc should be 1
+	EXPECT_EQ(10, work_item->ReadSReg(sdst));
+	EXPECT_EQ(1, work_item->ReadReg(scc));
 }
-*/
+
 } //SI
 
