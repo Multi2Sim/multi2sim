1ac1a5497998548a88e536ac962705a98f8841d6
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2015-06-22 19:45:54 +0000

    Disassemblers' include guards
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@3400 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- src/arch/hsa/asm/BrigImmed.h
+++ src/arch/hsa/asm/BrigImmed.h
@@ -17,8 +17,8 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef HSA_ASM_BRIGIMMED_H
-#define HSA_ASM_BRIGIMMED_H
+#ifndef ARCH_HSA_DISASSEMBLER_BRIGIMMED_H
+#define ARCH_HSA_DISASSEMBLER_BRIGIMMED_H
 
 #include <map>
 
