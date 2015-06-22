fcf0ac92ea762e7221958f18c1b7aed2486c7343
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2015-06-22 13:50:24 +0000

    Commented out NMOESI test from main()
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@3393 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- src/m2s.cc
+++ src/m2s.cc
@@ -543,7 +543,6 @@ int MainProgram(int argc, char **argv)
 
 int main(int argc, char **argv)
 {
-	/*
 	// Main exception handler
 	try
 	{
@@ -555,8 +554,8 @@ int main(int argc, char **argv)
 		e.Dump();
 		return 1;
 	}
-	*/
 
+/*
 	using namespace mem; //////////
 
 	const std::string mem_config_0 =
@@ -810,5 +809,6 @@ int main(int argc, char **argv)
 		e.Dump();
 		return 1;
 	}
+	*/
 }
 
