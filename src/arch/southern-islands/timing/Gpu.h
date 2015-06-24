1234584b35196016ffde05284ea015873e05e44e
ubal <ubal@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2015-06-24 10:57:51 +0000

    - Fixed issues reported by Kavi - Begin porting SI timing simulator - Removed Makefile/Makefile.in from repo
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@3412 a6503f54-2359-11de-8ce0-bfd7a313a9c3

---
+++ src/arch/southern-islands/timing/Gpu.h
@@ -0,0 +1,44 @@
+/*
+ *  Multi2Sim
+ *  Copyright (C) 2012  Rafael Ubal (ubal@ece.neu.edu)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef ARCH_SOUTHERN_ISLANDS_TIMING_GPU_H
+#define ARCH_SOUTHERN_ISLANDS_TIMING_GPU_H
+
+namespace SI
+{
+
+class Gpu
+{
+	//
+	// Configuration
+	//
+
+	// Number of compute units
+	static int num_compute_units;
+
+public:
+
+	/// Return the number of compute units, as set up by the user
+	static int getNumComputeUnits() { return num_compute_units; }
+};
+
+}
+
+#endif
+
