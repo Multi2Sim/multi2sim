7aae9c11da4777b087a61613e673fd1efd57892a
tushar.swamy <tushar.swamy@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2015-06-24 08:31:30 +0000

    - Added last ABI calls to SI driver

--- trunk/runtime/opencl/si-kernel.h
+++ trunk/runtime/opencl/si-kernel.h
@@ -33,7 +33,7 @@ enum si_call_t
 {                                                                                
         SIInvalid = 0,                                                  
 	#define DEFCALL(name, code) SI##name = code,                    
-	#include "Driver.def"
+	#include "../../src/arch/southern-islands/driver/Driver.def"
 	#undef DEFCALL                                                           
 	SICallCount                                                    
 };  
