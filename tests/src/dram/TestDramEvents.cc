acae4a6092c11f1b28d7e2f927cc2f618e64c89a
david.english <david.english@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2015-09-03 20:54:59 +0000

    added DRAM timing tests

--- tests/src/dram/TestDramEvents.cc
+++ tests/src/dram/TestDramEvents.cc
@@ -70,7 +70,6 @@ TEST(TestSystemEvents, section_one_read)
 	std::array<std::string, 2>  results;
 
 	// Test body
-	std::string message;
 	try
 	{
 		// Submit a single read request
@@ -126,7 +125,6 @@ TEST(TestSystemEvents, section_one_write)
 	std::array<std::string, 2> results;
 
 	// Test body
-	std::string message;
 	try
 	{
 		// Submit a single write request
@@ -182,7 +180,6 @@ TEST(TestSystemEvents, section_two_reads_same_address)
 	std::array<std::string, 3>  results;
 
 	// Test body
-	std::string message;
 	try
 	{
 		// Submit two read requests
@@ -239,7 +236,6 @@ TEST(TestSystemEvents, section_two_writes_same_address)
 	std::array<std::string, 3>  results;
 
 	// Test body
-	std::string message;
 	try
 	{
 		// Submit two write requests
@@ -297,7 +293,6 @@ TEST(TestSystemEvents, section_two_reads_different_row)
 	std::array<std::string, 5>  results;
 
 	// Test body
-	std::string message;
 	try
 	{
 		// Submit two read requests one row apart
@@ -355,7 +350,6 @@ TEST(TestSystemEvents, section_two_writes_different_row)
 	std::array<std::string, 5>  results;
 
 	// Test body
-	std::string message;
 	try
 	{
 		// Submit two write requests one row apart
@@ -389,6 +383,241 @@ TEST(TestSystemEvents, section_two_writes_different_row)
 	
 	// Compare results to expected
 	EXPECT_TRUE(results == expected);
+}
+
+TEST(TestSystemEvents, section_two_reads_same_row_closed_page_policy)
+{
+	// cleanup singleton instance
+	Cleanup();
+
+	// Set up INI file
+	misc::IniFile ini_file;
+	std::string closed_page_config = 
+			default_config + "PagePolicy = Closed\n";
+	ini_file.LoadFromString(closed_page_config);
+
+	// Set up dram instance
+	System *dram_system = System::getInstance();
+	dram_system->ParseConfiguration(&ini_file);
+
+	// TODO: Verify that these are the expected commands 
+	// after two reads to the same row
+	std::array<std::string, 6> expected = { "Activate", "Read",
+		"Precharge", "Activate", "Read", "Precharge" };
+
+	// Initialze array to store test results
+	std::array<std::string, 6>  results;
+
+	// Test body
+	try
+	{
+		// Submit two read requests one row apart
+		dram_system->Read(0);
+		dram_system->Read(0);
+		esim::Engine *engine = esim::Engine::getInstance();
+		engine->ProcessEvents();
+
+		// Get relevent bank
+		Bank *bank = dram_system->getController(0)->
+				getChannel(0)->getRank(0)->getBank(0);
+
+		// Step through cycle by cycle
+		for (unsigned i = 0; i < results.size(); i++)
+		{
+			engine->ProcessEvents();
+
+			// Store Current Bank Command in results
+			results[i] = bank->getCommandInQueueType();
+		}
+
+		// Check that command queue is now empty
+		engine->ProcessEvents();
+		EXPECT_TRUE(bank->getNumCommandsInQueue() == 0);
+	}
+	catch (misc::Error &e)
+	{
+		e.Dump();
+		FAIL();
+	}
+	
+	// Compare results to expected
+	EXPECT_TRUE(results == expected);
+}
+
+TEST(TestSystemEvents, section_two_writes_same_row_closed_page_policy)
+{
+	// cleanup singleton instance
+	Cleanup();
+
+	// Set up INI file
+	misc::IniFile ini_file;
+	std::string closed_page_config = 
+			default_config + "PagePolicy = Closed\n";
+	ini_file.LoadFromString(closed_page_config);
+
+	// Set up dram instance
+	System *dram_system = System::getInstance();
+	dram_system->ParseConfiguration(&ini_file);
+
+	// TODO: Verify that these are the expected commands 
+	// after two writes to the same row
+	std::array<std::string, 6> expected = { "Activate", "Write",
+		"Precharge", "Activate", "Write", "Precharge" };
+
+	// Initialze array to store test results
+	std::array<std::string, 6>  results;
+
+	// Test body
+	try
+	{
+		// Submit two write requests one row apart
+		dram_system->Write(0);
+		dram_system->Write(0);
+		esim::Engine *engine = esim::Engine::getInstance();
+		engine->ProcessEvents();
+
+		// Get relevent bank
+		Bank *bank = dram_system->getController(0)->
+				getChannel(0)->getRank(0)->getBank(0);
+
+		// Step through cycle by cycle
+		for (unsigned i = 0; i < results.size(); i++)
+		{
+			engine->ProcessEvents();
+
+			// Store Current Bank Command in results
+			results[i] = bank->getCommandInQueueType();
+		}
+
+		// Check that command queue is now empty
+		engine->ProcessEvents();
+		EXPECT_TRUE(bank->getNumCommandsInQueue() == 0);
+	}
+	catch (misc::Error &e)
+	{
+		e.Dump();
+		FAIL();
+	}
+	
+	// Compare results to expected
+	EXPECT_TRUE(results == expected);
+}
+
+TEST(TestSystemEvents, section_test_precharge_timing)
+{
+	// cleanup singleton instance
+	Cleanup();
+
+	// Set up INI file
+	misc::IniFile ini_file;
+	std::string DDR3_1600_config = 
+			default_config + "Default = DDR3_1600\n";
+	ini_file.LoadFromString(DDR3_1600_config);
+
+	// Set up dram instance
+	System *dram_system = System::getInstance();
+	dram_system->ParseConfiguration(&ini_file);
+
+	// Test body
+	try
+	{
+		// Submit two read requests one row apart
+		// to trigger a precharge
+		dram_system->Read(0);
+		dram_system->Read(1024);
+		esim::Engine *engine = esim::Engine::getInstance();
+
+		// Three ProcessEvents to get the precharge scheduled
+		engine->ProcessEvents();
+		engine->ProcessEvents();
+		engine->ProcessEvents();
+
+		// Get relevent bank
+		Bank *bank = dram_system->getController(0)->
+				getChannel(0)->getRank(0)->getBank(0);
+
+		// Go to precharge command
+		while(System::frequency_domain->getCycle()
+			       	< bank->getFrontCommandTiming()){
+			engine->ProcessEvents();
+		}
+
+		// Ensure we are at the first cycle where prcharge in at front
+		EXPECT_TRUE(bank->getCommandInQueueType() != "Precharge");
+		engine->ProcessEvents();
+		EXPECT_TRUE(bank->getCommandInQueueType() == "Precharge");
+
+		// TODO: Verify Precharge should take 16 cycles
+		EXPECT_TRUE(bank->getFrontCommandTiming() - 
+			System::frequency_domain->getCycle() == 16);
+	}
+	catch (misc::Error &e)
+	{
+		e.Dump();
+		FAIL();
+	}
+}
+
+TEST(TestSystemEvents, section_test_activate_timing_same_bank)
+{
+	// cleanup singleton instance
+	Cleanup();
+
+	// Set up INI file
+	misc::IniFile ini_file;
+	std::string DDR3_1600_config = 
+			default_config + "Default = DDR3_1600\n";
+	ini_file.LoadFromString(DDR3_1600_config);
+
+	// Set up dram instance
+	System *dram_system = System::getInstance();
+	dram_system->ParseConfiguration(&ini_file);
+
+	// Test body
+	try
+	{
+		// Submit two read requests one row apart
+		// to trigger an activate
+		dram_system->Read(0);
+		dram_system->Read(1024);
+		esim::Engine *engine = esim::Engine::getInstance();
+
+		// Three ProcessEvents to get the activate scheduled
+		engine->ProcessEvents();
+		engine->ProcessEvents();
+		engine->ProcessEvents();
+
+		// Get relevent bank
+		Bank *bank = dram_system->getController(0)->
+				getChannel(0)->getRank(0)->getBank(0);
+
+		// Skip over precharge command
+		while(System::frequency_domain->getCycle()
+			       	< bank->getFrontCommandTiming()){
+			engine->ProcessEvents();
+		}
+		engine->ProcessEvents();
+
+		// Go to activate command
+		while(System::frequency_domain->getCycle()
+			       	< bank->getFrontCommandTiming()){
+			engine->ProcessEvents();
+		}
+
+		// Ensure we are at the first cycle where activate in at front
+		EXPECT_TRUE(bank->getCommandInQueueType() != "Activate");
+		engine->ProcessEvents();
+		EXPECT_TRUE(bank->getCommandInQueueType() == "Activate");
+
+		// TODO: Verify activate should take 20 cycles
+		EXPECT_TRUE(bank->getFrontCommandTiming() - 
+			System::frequency_domain->getCycle() == 20);
+	}
+	catch (misc::Error &e)
+	{
+		e.Dump();
+		FAIL();
+	}
 }
 
 }
