e443264ddb12122ffa7158f48a1557c738f03613
tushar.swamy <tushar.swamy@a6503f54-2359-11de-8ce0-bfd7a313a9c3>
2015-10-13 02:44:16 +0000

    - Fixed bugs in ScalarUnit and SimdUnit in SI timing
    git-svn-id: svn+ssh://multi2sim.org/multi2sim/trunk@3662 a6503f54-2359-11de-8ce0-bfd7a313a9c3

--- src/arch/southern-islands/timing/ScalarUnit.h
+++ src/arch/southern-islands/timing/ScalarUnit.h
@@ -33,9 +33,6 @@ class ComputeUnit;
 /// Class representing the scalar unit of a compute unit
 class ScalarUnit : public ExecutionUnit
 {
-	// Variable number of issued Uops
-	//std::deque<std::unique_ptr<Uop>> issue_buffer;
-
 	// Variable number of decoded Uops
 	std::deque<std::unique_ptr<Uop>> decode_buffer;
 
